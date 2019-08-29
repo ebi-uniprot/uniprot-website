d6c2595480a9da5955ac1c17236bb11667e6845d
Daniel Rice <dlrice@ebi.ac.uk>
2019-08-29 16:34:05 +0100

    Add getBEMClassName convenience function which is memoized using the mem package.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import mem from 'mem';
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const serializableDeepCopy = (x: object) =>
   JSON.parse(JSON.stringify(x));
@@ -36,3 +38,24 @@ export const moveItemInList = (
   result.splice(destIndex, 0, removed);
   return result;
 };
+
+// Memoizing because this function is going to be called a lot and most often
+// with the same arguments. Without doing this the app slows down when throlling
+// cpu: 6x slowdown 
+export const getBEMClassName = mem(({ b, e: elements = null, m: modifiers = null }) => {
+  let clasName = b;
+  if (!!elements) {
+    const e = Array.isArray(elements) ? elements : [elements];
+    clasName = [b, ...e].join("__");
+  }
+  if (!!modifiers) {
+    const m = Array.isArray(modifiers) ? modifiers : [modifiers];
+    clasName = m.reduce((accum, modifier) => {
+      if (!!modifier) {
+        accum += ` ${clasName}--${modifier}`;
+      }
+      return accum;
+    }, clasName);  
+  }
+  return clasName;
+})
