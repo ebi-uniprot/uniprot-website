2365a6627eff6530820925a9f304bb31cb9dd4e6
Daniel Rice <dlrice@ebi.ac.uk>
2019-09-17 19:45:01 +0100

    Refactor src/model/ColumnConfiguration.tsx to be a map with columnId as the key.

--- src/model/ColumnConfiguration.tsx
+++ src/model/ColumnConfiguration.tsx
@@ -7,44 +7,64 @@ import OrganismView from '../view/uniprotkb/components/OrganismView';
 import GeneNamesView from '../view/uniprotkb/components/GeneNamesView';
 import convertGeneNames from './uniprotkb/GeneNamesConverter';
 import { UniProtkbAPIModel } from './uniprotkb/UniProtkbConverter';
+import ColumnId from './types/columnIdTypes';
 
-const ColumnConfiguration: {
-  [index: string]: {
+export const ColumnConfiguration = new Map<
+  ColumnId,
+  {
     label: string;
+    sortable: boolean;
     render: (data: UniProtkbAPIModel) => JSX.Element | undefined;
-  };
-} = {
-  accession: {
-    label: 'Entry',
-    render: (data: { primaryAccession: string; entryType: string }) => (
-      <SimpleView
-        termValue={`${data.primaryAccession} (${data.entryType})`}
-        linkTo={`/uniprotkb/${data.primaryAccession}`}
-      />
+  }
+>();
+
+// sortable:
+// mnemonic = 'mnemonic',
+// name = 'name',
+// annotation_score = 'annotation_score',
+// gene = 'gene',
+// length = 'length',
+// mass = 'mass',
+
+ColumnConfiguration.set(ColumnId.accession, {
+  label: 'Entry',
+  sortable: true,
+  render: (data: { primaryAccession: string; entryType: string }) => (
+    <SimpleView
+      termValue={`${data.primaryAccession} (${data.entryType})`}
+      linkTo={`/uniprotkb/${data.primaryAccession}`}
+    />
+  ),
+});
+
+ColumnConfiguration.set(ColumnId.id, {
+  label: 'Entry Name',
+  sortable: true,
+  render: (data: { uniProtId: string }) => (
+    <SimpleView termValue={data.uniProtId} />
+  ),
+});
+
+ColumnConfiguration.set(ColumnId.proteinName, {
+  label: 'Protein names',
+  sortable: true,
+  render: data =>
+    data.proteinDescription && (
+      <ProteinNamesView {...convertProteinNames(data.proteinDescription)} />
     ),
-  },
-  id: {
-    label: 'Entry Name',
-    render: (data: { uniProtId: string }) => (
-      <SimpleView termValue={data.uniProtId} />
-    ),
-  },
-  protein_name: {
-    label: 'Protein names',
-    render: data =>
-      data.proteinDescription && (
-        <ProteinNamesView {...convertProteinNames(data.proteinDescription)} />
-      ),
-  },
-  gene_names: {
-    label: 'Gene Names',
-    render: data =>
-      data.genes && <GeneNamesView {...convertGeneNames(data.genes)} />,
-  },
-  organism: {
-    label: 'Organism',
-    render: data => data.organism && <OrganismView data={data.organism} />,
-  },
-};
+});
+
+ColumnConfiguration.set(ColumnId.geneNames, {
+  label: 'Gene Names',
+  sortable: true,
+  render: data =>
+    data.genes && <GeneNamesView {...convertGeneNames(data.genes)} />,
+});
+
+ColumnConfiguration.set(ColumnId.organism, {
+  label: 'Organism',
+  sortable: true,
+  render: data => data.organism && <OrganismView data={data.organism} />,
+});
 
 export default ColumnConfiguration;
