2823cecd51091c13d67d810bdc3f0f42852bceeb
Daniel Rice <dlrice@ebi.ac.uk>
2019-10-17 15:04:18 +0100

    Add src/results/CustomiseTableContainer.tsx. Provide onCancel callback.

--- src/results/CustomiseTableContainer.tsx
+++ src/results/CustomiseTableContainer.tsx
@@ -1,91 +1,21 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Dispatch, bindActionCreators } from 'redux';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-import { RootState, RootAction } from '../state/state-types';
-import * as resultsActions from './state/resultsActions';
-import ColumnSelect from './ColumnSelect';
-import defaultTableColumns from './state/resultsInitialState';
-import fieldsData from '../data/fields.json';
-import ColumnId from '../model/types/columnIdTypes';
+import React, { useState } from 'react';
+import CustomiseTableView from './CustomiseTableView';
 
-type CustomiseTableProps = {
-  tableColumns: ColumnId[];
-  // fetchFieldsIfNeeded: () => void;
-  // fieldsData: ;
-  namespace: Namespace;
-  dispatchFetchBatchOfResultsIfNeeded: (url: string | undefined) => void;
-  dispatchReset: () => void;
-  dispatchClearResults: () => void;
-  dispatchSwitchViewMode: () => void;
-  dispatchUpdateSummaryAccession: (accession: string) => void;
-  clauses?: Clause[];
-  tableColumns: string[];
-  cardColumns: string[];
-  results: UniProtkbAPIModel[];
-  facets: Facet[];
-  isFetching: boolean;
-  nextUrl: string;
-  totalNumberResults: number;
-  viewMode: ViewMode;
-  summaryAccession: string | null;
-} & RouteComponentProps;
+const CustomiseTableContainer = ({ columns }) => {
+  const [selectedColumns, setSelectedColumns] = useState(columns);
 
-const entryField = {
-  tabId: 'data',
-  accordionId: 'Names & Taxonomy',
-  itemId: ColumnId.accession,
-};
-
-export const removeFieldFromFieldsData = (
-  { tabId, accordionId, itemId },
-  fieldsData
-) => ({
-  ...fieldsData,
-  [tabId]: fieldsData[tabId].map(group =>
-    group.id === accordionId
-      ? { ...group, items: group.items.filter(({ id }) => id !== itemId) }
-      : group
-  ),
-});
-
-const CustomiseTable = ({ tableColumns, fetchFieldsIfNeeded, fieldsData }) => {
-  const handleChange = columns => {
-    console.log(columns);
+  const handleCancel = () => {
+    console.log('cancel');
   };
 
   return (
-    <ColumnSelect
-      tableColumns={tableColumns.filter(
-        tableColumn => tableColumn !== entryField.itemId
-      )}
-      fetchFieldsIfNeeded={fetchFieldsIfNeeded}
-      fieldsData={removeFieldFromFieldsData(entryField, fieldsData)}
-      defaultTableColumns={defaultTableColumns}
-      onChange={handleChange}
+    <CustomiseTableView
+      onColumnSelect={cols => setSelectedColumns(cols)}
+      selectedColumns={selectedColumns}
+      onSubmit={saveColumns} // dispatch submit update columns
+      onCancel={handleCancel}
     />
   );
 };
-const mapStateToProps = (state: RootState) => ({
-  tableColumns: state.results.tableColumns,
-  // fieldsData: state.results.fields.data,
-  fieldsData,
-  isFetching: state.results.fields.isFetching,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch<RootAction>) =>
-  bindActionCreators(
-    {
-      fetchFieldsIfNeeded: () => resultsActions.fetchFieldsIfNeeded(),
-    },
-    dispatch
-  );
-
-const CustomiseTableContainer = withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(CustomiseTable)
-);
 
 export default CustomiseTableContainer;
--- src/results/CustomiseTableView.tsx
+++ src/results/CustomiseTableView.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ColumnSelectContainer from './ColumnSelectContainer';
 
-const CustomiseTableView = ({ onColumnSelect, selectedColumns, onSubmit }) => (
+const CustomiseTableView = ({
+  onColumnSelect,
+  selectedColumns,
+  onSubmit,
+  onCancel,
+}) => (
   <form onSubmit={onSubmit}>
     <ColumnSelectContainer onColumnSelect={handleColumnSelect} />;
     <div className="button-group">
       <button className="button secondary" type="button">
         Cancel
       </button>
-      <button className="button" type="submit">
+      <button className="button" type="submit" onClick={onCancel}>
         Save
       </button>
     </div>
