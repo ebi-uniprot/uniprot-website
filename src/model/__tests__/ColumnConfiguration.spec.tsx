3dfcf2c228d7005d954cf72389f84e37e01752bc
Daniel Rice <dlrice@ebi.ac.uk>
2019-10-24 17:27:22 +0100

    Update existing tests and modify ColumnCofiguriation to use Map.get(column).

--- src/model/__tests__/ColumnConfiguration.spec.tsx
+++ src/model/__tests__/ColumnConfiguration.spec.tsx
@@ -3,32 +3,41 @@ import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ColumnConfiguration from '../ColumnConfiguration';
 import data from '../__mocks__/modelData.json';
+import { Column } from '../types/ColumnTypes';
 
 configure({ adapter: new Adapter() });
 
 describe('ColumnConfiguration component', () => {
   test('should render accession', () => {
-    const wrapper = shallow(ColumnConfiguration.accession.render(data));
+    const wrapper = shallow(
+      ColumnConfiguration.get(Column.accession).render(data)
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   test('should render id', () => {
-    const wrapper = shallow(ColumnConfiguration.id.render(data));
+    const wrapper = shallow(ColumnConfiguration.get(Column.id).render(data));
     expect(wrapper).toMatchSnapshot();
   });
 
   test('should render protein_name', () => {
-    const wrapper = shallow(ColumnConfiguration.protein_name.render(data));
+    const wrapper = shallow(
+      ColumnConfiguration.get(Column.proteinName).render(data)
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   test('should render gene_names', () => {
-    const wrapper = shallow(ColumnConfiguration.gene_names.render(data));
+    const wrapper = shallow(
+      ColumnConfiguration.get(Column.geneNames).render(data)
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   test('should render organism', () => {
-    const wrapper = shallow(ColumnConfiguration.organism.render(data));
+    const wrapper = shallow(
+      ColumnConfiguration.get(Column.organism).render(data)
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
