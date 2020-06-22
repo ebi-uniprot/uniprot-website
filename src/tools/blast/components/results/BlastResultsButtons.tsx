fd62509e478ef8211634f63f86d068dc91938bc8
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-06-22 11:47:15 +0200

    implement resubmit from server parameters

--- src/tools/blast/components/results/BlastResultsButtons.tsx
+++ src/tools/blast/components/results/BlastResultsButtons.tsx
@@ -1,11 +1,56 @@
 import React, { FC, lazy, useState, Suspense } from 'react';
-import { DownloadIcon, BasketIcon } from 'franklin-sites';
+import { useHistory } from 'react-router-dom';
+import { DownloadIcon, BasketIcon, ReSubmitIcon } from 'franklin-sites';
+
 import SidePanel from '../../../../shared/components/layouts/SidePanel';
 
-const BlastResultsButtons: FC<{
+import { serverParametersToFormParameters } from '../../adapters/BlastParametersAdapter';
+
+import { LocationToPath, Location } from '../../../../app/config/urls';
+
+import { PublicServerParameters } from '../../types/blastServerParameters';
+
+type ResubmitButtonProps = {
+  inputParamsData?: PublicServerParameters;
+};
+
+const ResubmitButton: FC<ResubmitButtonProps> = ({ inputParamsData }) => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    if (!inputParamsData) {
+      return;
+    }
+
+    const parameters = serverParametersToFormParameters(inputParamsData);
+
+    history.push(LocationToPath[Location.Blast], { parameters });
+  };
+
+  return (
+    <button
+      type="button"
+      className="button tertiary"
+      disabled={!inputParamsData}
+      onClick={handleClick}
+    >
+      <ReSubmitIcon />
+      Resubmit
+    </button>
+  );
+};
+
+type BlastResultsButtonsProps = {
   jobId: string;
   selectedEntries: string[];
-}> = ({ jobId, selectedEntries }) => {
+  inputParamsData?: PublicServerParameters;
+};
+
+const BlastResultsButtons: FC<BlastResultsButtonsProps> = ({
+  jobId,
+  selectedEntries,
+  inputParamsData,
+}) => {
   const BlastResultDownload = lazy(() =>
     import(/* webpackChunkName: "blast-download" */ './BlastResultDownload')
   );
@@ -57,6 +102,7 @@ const BlastResultsButtons: FC<{
           <BasketIcon />
           Add
         </button>
+        <ResubmitButton inputParamsData={inputParamsData} />
       </div>
     </>
   );
