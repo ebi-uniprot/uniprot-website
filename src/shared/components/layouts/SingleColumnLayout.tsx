5896ed1cf08180802bb8b29b6829d2ae15d71403
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-07-29 10:27:53 +0200

    basic set-up

--- src/shared/components/layouts/SingleColumnLayout.tsx
+++ src/shared/components/layouts/SingleColumnLayout.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, FC, Suspense } from 'react';
+import React, { lazy, FC, Suspense, ReactNode } from 'react';
 import { Loader } from 'franklin-sites';
 
 import ErrorBoundary from '../error-component/ErrorBoundary';
@@ -9,16 +9,20 @@ const UniProtFooter = lazy(() =>
   import(/* webpackChunkName: "footer" */ './UniProtFooter')
 );
 
-const SingleColumnLayout: FC = ({ children }) => (
+type Props = {
+  title?: ReactNode;
+};
+
+const SingleColumnLayout: FC<Props> = ({ children }) => (
   <div className="single-column-layout">
     <Suspense fallback={<Loader />}>
       <ErrorBoundary>
-        <main>{children}</main>
+        <main className="single-column-layout__main">{children}</main>
       </ErrorBoundary>
     </Suspense>
     <Suspense fallback={null}>
       <ErrorBoundary>
-        <UniProtFooter className="footer" />
+        <UniProtFooter className="single-column-layout__footer" />
       </ErrorBoundary>
     </Suspense>
   </div>
---
+++ src/tools/align/components/results/AlignResult.tsx
@@ -0,0 +1,249 @@
+import React, { useMemo, useEffect, useState, lazy, Suspense } from 'react';
+import { Link, useRouteMatch, useHistory, useLocation } from 'react-router-dom';
+import { Loader, PageIntro, Tabs, Tab } from 'franklin-sites';
+
+import SingleColumnLayout from '../../../../shared/components/layouts/SingleColumnLayout';
+import ErrorHandler from '../../../../shared/components/error-pages/ErrorHandler';
+// import BlastResultButtons from './BlastResultButtons';
+
+import useDataApi, {
+  UseDataAPIState,
+} from '../../../../shared/hooks/useDataApi';
+
+import inputParamsXMLToObject from '../../../blast/adapters/inputParamsXMLToObject';
+
+import { Location, LocationToPath } from '../../../../app/config/urls';
+import toolsURLs from '../../../config/urls';
+import { getAccessionsURL } from '../../../../uniprotkb/config/apiUrls';
+
+import { AlignResults } from '../../types/alignResults';
+import Response from '../../../../uniprotkb/types/responseTypes';
+import { JobTypes } from '../../../types/toolsJobTypes';
+import { PublicServerParameters } from '../../types/alignServerParameters';
+// what we import are types, even if they are in adapter file
+import { UniProtkbAPIModel } from '../../../../uniprotkb/adapters/uniProtkbConverter';
+import ErrorBoundary from '../../../../shared/components/error-component/ErrorBoundary';
+
+const alignUrls = toolsURLs(JobTypes.ALIGN);
+
+// const AlignResultOverview = lazy(() =>
+//   import(/* webpackChunkName: "blast-result-page" */ './BlastResultTable')
+// );
+// const BlastResultTaxonomy = lazy(() =>
+//   import(
+//     /* webpackChunkName: "blast-result-taxonomy" */ './BlastResultTaxonomy'
+//   )
+// );
+// const BlastResultTextOutput = lazy(() =>
+//   import(
+//     /* webpackChunkName: "blast-result-text-output" */ './BlastResultTextOutput'
+//   )
+// );
+const BlastResultToolInput = lazy(() =>
+  import(
+    /* webpackChunkName: "blast-result-tool-input" */ '../../../blast/components/results/BlastResultToolInput'
+  )
+);
+// const BlastResultHitDistribution = lazy(() =>
+//   import(
+//     /* webpackChunkName: "blast-result-hit-distribution" */ './BlastResultHitDistribution'
+//   )
+// );
+
+enum TabLocation {
+  Overview = 'overview',
+  Taxonomy = 'taxonomy',
+  HitDistribution = 'hit-distribution',
+  TextOutput = 'text-output',
+  ToolInput = 'tool-input',
+}
+
+type Match = {
+  params: {
+    id: string;
+    subPage?: string;
+  };
+};
+
+// custom hook to get data from the input parameters endpoint, input sequence
+// then parse it and merge it.
+// This is kinda 'faking' useDataApi for the kind of object it outputs
+const useParamsData = (
+  id: string
+): Partial<UseDataAPIState<PublicServerParameters>> => {
+  const [paramsData, setParamsData] = useState<
+    Partial<UseDataAPIState<PublicServerParameters>>
+  >({});
+
+  const paramsXMLData = useDataApi<string>(
+    alignUrls.resultUrl(id, 'submission')
+  );
+  const sequenceData = useDataApi<string>(alignUrls.resultUrl(id, 'sequence'));
+
+  useEffect(() => {
+    const loading = paramsXMLData.loading || sequenceData.loading;
+    const error = paramsXMLData.error || sequenceData.error;
+    const status = paramsXMLData.status || sequenceData.status;
+    if (loading) {
+      setParamsData({ loading });
+    } else if (error) {
+      setParamsData({ loading, error, status });
+    } else if (paramsXMLData.data && sequenceData.data) {
+      setParamsData({
+        loading,
+        data: inputParamsXMLToObject(paramsXMLData.data, sequenceData.data),
+      });
+    }
+  }, [paramsXMLData, sequenceData]);
+
+  if (paramsData.data) {
+    console.log('params data:', paramsData.data);
+  }
+
+  return paramsData;
+};
+
+const AlignResult = () => {
+  const history = useHistory();
+  const match = useRouteMatch(LocationToPath[Location.AlignResult]) as Match;
+
+  const [selectedEntries, setSelectedEntries] = useState<string[]>([]);
+
+  // if URL doesn't finish with "overview" redirect to /overview by default
+  useEffect(() => {
+    if (!match.params.subPage) {
+      history.replace(
+        history.createHref({
+          ...history.location,
+          pathname: `${history.location.pathname}/overview`,
+        })
+      );
+    }
+  }, [match.params.subPage, history]);
+
+  // get data from the blast endpoint
+  const { loading, data, error, status } = useDataApi<AlignResults>(
+    alignUrls.resultUrl(match.params.id, 'aln-clustal_num')
+  );
+
+  // get data from accessions endpoint with facets applied
+  // const { loading: accessionsLoading, data: accessionsData } = useDataApi<
+  //   Response['data']
+  // >(
+  //   useMemo(
+  //     () =>
+  //       getAccessionsURL(accessionsFilteredByLocalFacets, {
+  //         selectedFacets: urlParams.selectedFacets,
+  //         facets: [],
+  //       }),
+  //     [accessionsFilteredByLocalFacets, urlParams.selectedFacets]
+  //   )
+  // );
+
+  const inputParamsData = useParamsData(match.params.id);
+
+  // Note: this function is duplicated in ResultsContainer.tsx
+  const handleSelectedEntries = (rowId: string) => {
+    const filtered = selectedEntries.filter((id) => id !== rowId);
+    setSelectedEntries(
+      filtered.length === selectedEntries.length
+        ? [...selectedEntries, rowId]
+        : filtered
+    );
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error || !data) {
+    return <ErrorHandler status={status} />;
+  }
+
+  // const actionBar = (
+  //   <BlastResultButtons
+  //     jobId={match.params.id}
+  //     selectedEntries={selectedEntries}
+  //     inputParamsData={inputParamsData.data}
+  //     nHits={blastData.hits.length}
+  //     isTableResultsFiltered={
+  //       typeof data?.hits.length !== 'undefined' &&
+  //       data?.hits.length !== blastData.hits.length
+  //     }
+  //   />
+  // );
+  const actionBar = '<AlignResultButtons>';
+
+  return (
+    <SingleColumnLayout>
+      <PageIntro title="Align Results" />
+      <Tabs active={match.params.subPage}>
+        <Tab
+          id="overview"
+          title={
+            <Link
+              to={(location) => ({
+                ...location,
+                pathname: `/align/${match.params.id}/overview`,
+              })}
+            >
+              Overview
+            </Link>
+          }
+        >
+          {actionBar}
+          <Suspense fallback={<Loader />}>
+            {data}
+            {/* <BlastResultTable
+              loading={blastLoading || accessionsLoading}
+              data={{ ...blastData, hits: hitsFiltered }}
+              selectedEntries={selectedEntries}
+              handleSelectedEntries={handleSelectedEntries}
+            /> */}
+          </Suspense>
+        </Tab>
+        <Tab
+          id="text-output"
+          title={
+            <Link
+              to={(location) => ({
+                ...location,
+                pathname: `/align/${match.params.id}/text-output`,
+              })}
+            >
+              Text Output
+            </Link>
+          }
+        >
+          <Suspense fallback={<Loader />}>
+            {data}
+            {/* <BlastResultTextOutput id={match.params.id} /> */}
+          </Suspense>
+        </Tab>
+        <Tab
+          id="tool-input"
+          title={
+            <Link
+              to={(location) => ({
+                ...location,
+                pathname: `/align/${match.params.id}/tool-input`,
+              })}
+            >
+              Tool Input
+            </Link>
+          }
+        >
+          <Suspense fallback={<Loader />}>
+            <BlastResultToolInput
+              id={match.params.id}
+              jobType={JobTypes.ALIGN}
+              inputParamsData={inputParamsData}
+            />
+          </Suspense>
+        </Tab>
+      </Tabs>
+    </SingleColumnLayout>
+  );
+};
+
+export default AlignResult;
