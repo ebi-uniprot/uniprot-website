4621aafff6718b85c8b9e95ef3b9a4a4c3b857f8
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-07-30 16:47:34 +0200

    remove unused prop type

--- src/shared/components/layouts/SingleColumnLayout.tsx
+++ src/shared/components/layouts/SingleColumnLayout.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, FC, Suspense, ReactNode } from 'react';
+import React, { lazy, FC, Suspense } from 'react';
 import { Loader } from 'franklin-sites';
 import cn from 'classnames';
 
@@ -11,7 +11,6 @@ const UniProtFooter = lazy(() =>
 );
 
 type Props = {
-  title?: ReactNode;
   className?: string;
 };
 
