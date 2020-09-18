d02ced53262fb68e9bfbbf36b16d102e315251c5
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-09-18 15:16:38 +0100

    reset the main search text when needed, also, fix linting error

--- src/uniprotkb/components/search/SearchContainer.tsx
+++ src/uniprotkb/components/search/SearchContainer.tsx
@@ -1,19 +1,16 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import { MainSearch } from 'franklin-sites';
 
-import {
-  LocationToPath,
-  Location,
-  SearchResultsLocations,
-} from '../../../app/config/urls';
+import { SearchResultsLocations } from '../../../app/config/urls';
 import { Namespace, NamespaceLabels } from '../../types/searchTypes';
 
 import './styles/search-container.scss';
 
 const Search = () => {
   const history = useHistory();
+  const location = useLocation();
 
   // local state to hold the search value without modifying URL
   const [searchTerm, setSearchTerm] = useState(
@@ -45,6 +42,12 @@ const Search = () => {
   const setNamespace = (namespace: Namespace) => {
     setSelectedNamespace(namespace);
   };
+
+  // reset the text content when there is a navigation to reflect what is in the
+  // URL. That includes removing the text when browsing to a non-search page.
+  useEffect(() => {
+    setSearchTerm(queryString.parse(location.search, { decode: true }).query);
+  }, [location]);
 
   return (
     <MainSearch
