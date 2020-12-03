7c855c0728967df2c1546b104267f1e3b5c64ebb
Daniel Rice <dlrice@ebi.ac.uk>
2020-12-03 11:36:28 +0000

    More Download tests.

--- src/shared/components/download/__tests__/Download.spec.tsx
+++ src/shared/components/download/__tests__/Download.spec.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import Download, { getPreviewFileFormat } from '../Download';
-import renderWithRedux from '../../../__test-helpers__/RenderWithRedux';
-import initialState from '../../../../app/state/rootInitialState';
+import renderWithRouter from '../../../__test-helpers__/RenderWithRouter';
 import { FileFormat } from '../../../types/resultsDownload';
 import { UniProtKBColumn } from '../../../../uniprotkb/types/columnTypes';
 import { SearchResultsLocations } from '../../../../app/config/urls';
 import { Namespace } from '../../../types/namespaces';
 import mockFasta from '../../../../uniprotkb/components/__mocks__/fasta.json';
 import '../../../../uniprotkb/components/__mocks__/mockApi';
+import * as utils from '../../../utils/utils';
 
 // FIXME: Doing network call through ColumnSelect's logic and Redux
 // FIXME: logic. Might need to mock after removing Redux logic to load data.
@@ -25,21 +25,25 @@ describe('getPreviewFileFormat', () => {
 });
 
 describe('Download component', () => {
-  let renderedWithRedux;
+  let rendered;
   const namespace = Namespace.uniprotkb;
+  const query = 'nod2';
+  const selectedEntries = ['Q9HC29', 'O43353', 'Q3KP66'];
   const onCloseMock = jest.fn();
+  let downloadFileInNewTab;
 
   beforeEach(() => {
-    renderedWithRedux = renderWithRedux(
+    downloadFileInNewTab = jest.spyOn(utils, 'downloadFileInNewTab');
+    rendered = renderWithRouter(
       <Download
-        query="nod2"
+        query={query}
         selectedFacets={[]}
         selectedColumns={[
           UniProtKBColumn.accession,
           UniProtKBColumn.reviewed,
           UniProtKBColumn.id,
         ]}
-        selectedEntries={['Q9HC29', 'O43353', 'Q3KP66']}
+        selectedEntries={selectedEntries}
         totalNumberResults={10}
         onClose={onCloseMock}
       />,
@@ -49,22 +53,40 @@ describe('Download component', () => {
     );
   });
 
-  test('should go back and not call updateTableColumns action when cancel button is pressed', () => {
-    const { getByText } = renderedWithRedux;
+  afterEach(() => {
+    downloadFileInNewTab.mockRestore();
+  });
+
+  test('should call onClose when cancel button is clicked', () => {
+    const { getByText } = rendered;
     const cancelButton = getByText('Cancel');
     fireEvent.click(cancelButton);
     expect(onCloseMock).toHaveBeenCalled();
   });
 
-  test('should handle download button by opening new tab and then going back', () => {
-    const { getAllByText } = renderedWithRedux;
-    const downloadButton = getAllByText('Download')[1];
-    fireEvent.click(downloadButton);
+  test('should call onClose and downloadFileInNewTab with JSON format when format is selected and Download button is clicked', () => {
+    const { getAllByText, getByTestId } = rendered;
+    const formatSelect = getByTestId('file-format-select');
+    fireEvent.change(formatSelect, { target: { value: FileFormat.json } });
+    fireEvent.click(getAllByText('Download')[1]);
+    expect(downloadFileInNewTab).toHaveBeenCalledWith(
+      expect.stringContaining('format=json')
+    );
+    expect(onCloseMock).toHaveBeenCalled();
+  });
+
+  test('should call onClose and downloadFileInNewTab without compressed=true when selected false in the form and Download button is clicked', () => {
+    const { getAllByText, getByLabelText } = rendered;
+    fireEvent.click(getByLabelText('No'));
+    fireEvent.click(getAllByText('Download')[1]);
+    expect(downloadFileInNewTab).toHaveBeenCalledWith(
+      expect.not.stringContaining('compressed')
+    );
     expect(onCloseMock).toHaveBeenCalled();
   });
 
   test('should handle preview button click', async () => {
-    const { getByText, findByTestId } = renderedWithRedux;
+    const { getByText, findByTestId } = rendered;
     const previewButton = getByText('Preview 10');
     fireEvent.click(previewButton);
     const preview = await findByTestId('download-preview');
@@ -72,7 +94,7 @@ describe('Download component', () => {
   });
 
   test('should show column selection component when excel or tsv file type is selected and otherwise hide it', async () => {
-    const { getByTestId, findByText } = renderedWithRedux;
+    const { getByTestId, findByText } = rendered;
     const formatSelect = getByTestId('file-format-select');
     [
       [FileFormat.excel, true],
@@ -87,78 +109,12 @@ describe('Download component', () => {
         : expectCustomise.not.toBeInTheDocument();
     });
   });
-});
 
-describe.skip('DownloadView component', () => {
-  let props, renderedWithRedux;
-  beforeEach(() => {
-    props = {
-      onSubmit: jest.fn(),
-      onCancel: jest.fn(),
-      onPreview: jest.fn(),
-      selectedColumns: defaultTableColumns,
-      downloadAll: true,
-      fileFormat: FileFormat.tsv,
-      compressed: true,
-      preview: '',
-      loadingPreview: false,
-      onSelectedColumnsChange: jest.fn(),
-      onDownloadAllChange: jest.fn(),
-      onFileFormatChange: jest.fn(),
-      onCompressedChange: jest.fn(),
-      nSelectedEntries: 5,
-      totalNumberResults: 100,
-      nPreview: 10,
-    };
-    renderedWithRedux = renderWithRedux(<DownloadView {...props} />, {
-      initialState,
-      location: '/download',
-    });
-  });
-
-  test('should render', () => {
-    const { asFragment } = renderedWithRedux;
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('should call onSubmit when submit button is clicked', () => {
-    const { getByTestId } = renderedWithRedux;
-    const form = getByTestId('download-form');
-    fireEvent.submit(form);
-    expect(props.onSubmit).toHaveBeenCalled();
-  });
-
-  test('should call onCancel when cancel button is clicked', () => {
-    const { getByText } = renderedWithRedux;
-    const cancelButton = getByText('Cancel');
-    fireEvent.click(cancelButton);
-    expect(props.onCancel).toHaveBeenCalled();
-  });
-
-  test('should call onPreview when preview button is clicked', () => {
-    const { getByText } = renderedWithRedux;
-    const previewButton = getByText('Preview 10');
-    fireEvent.click(previewButton);
-    expect(props.onPreview).toHaveBeenCalled();
-  });
-  test('should call onDownloadAllChange when download selected radio is selected', () => {
-    const { getByLabelText } = renderedWithRedux;
-    const radio = getByLabelText(/Download selected/);
-    fireEvent.click(radio);
-    expect(props.onDownloadAllChange).toHaveBeenCalled();
-  });
-
-  test('should call onFileFormatChange when file format select is changed', () => {
-    const { getByTestId } = renderedWithRedux;
-    const formatSelect = getByTestId('file-format-select');
-    fireEvent.change(formatSelect, { target: { value: FileFormat.gff } });
-    expect(props.onFileFormatChange).toHaveBeenCalled();
-  });
-
-  test('should call onCompressedChange when no choice is clicked', () => {
-    const { getByText } = renderedWithRedux;
-    const radio = getByText(/No/);
-    fireEvent.click(radio);
-    expect(props.onCompressedChange).toHaveBeenCalled();
+  test('should change Preview button text when Download selected radio is selected', () => {
+    const { getByLabelText, getByText } = rendered;
+    fireEvent.click(
+      getByLabelText(`Download selected (${selectedEntries.length})`)
+    );
+    expect(getByText(`Preview ${selectedEntries.length}`)).toBeTruthy();
   });
 });
