9e9dff6bc22c90b5cd50ff4445f9df24c3424c92
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-11 19:00:59 +0000

    Deploy ebi-uniprot/uniprot-website to github.com/ebi-uniprot/uniprot-website.git:gh-pages

---
+++ text-output.9ad3f4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{860:function(a,e,t){"use strict";t.r(e);var n=t(0),l=t.n(n),r=t(280),u=t(903),s=t(294),c=t(287),o=t(71),d=t(4);e.default=({id:a,jobType:e})=>{const{loading:t,data:n,error:i,status:p}=Object(c.a)(Object(o.a)(e).resultUrl(a,e===d.a.ALIGN?"aln-clustal_num":"out"));return t?l.a.createElement(r.a,null):i||!n?l.a.createElement(s.a,{status:p}):l.a.createElement(u.a,null,n)}}}]);
+//# sourceMappingURL=text-output.9ad3f4.js.map
