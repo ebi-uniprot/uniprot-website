919eca9e29c94eb4e66131e68f4604d552945e8d
Aurélien Luciani <aurel-l@users.noreply.github.com>
2021-01-05 12:49:24 +0100

    use "curly" rule instead of custom one

--- eslint-custom-rules/index.js
+++ eslint-custom-rules/index.js
@@ -1,13 +1,11 @@
 module.exports = {
   rules: {
     'use-config-location': require('./rules/use-config-location'),
-    'return-on-new-line': require('./rules/return-on-new-line'),
   },
   configs: {
     recommended: {
       rules: {
         'uniprot-website/use-config-location': 'warn',
-        'uniprot-website/return-on-new-line': 'warn',
       },
     },
   },
--- eslint-custom-rules/rules/__tests__/config.js
+++ eslint-custom-rules/rules/__tests__/config.js
@@ -2,7 +2,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2019,
     sourceType: 'module',
-    // project: '../tsconfig.json',
   },
   parser: require.resolve('@typescript-eslint/parser'),
 };
