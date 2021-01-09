1a77807b301e864d1f04b52493f6a968a14f2f87
Aurélien Luciani <aurel-l@users.noreply.github.com>
2021-01-09 16:36:35 +0100

    improve tests

--- src/query-builder/components/__tests__/QueryBuilder.spec.tsx
+++ src/query-builder/components/__tests__/QueryBuilder.spec.tsx
@@ -1,43 +1,36 @@
 import { createMemoryHistory } from 'history';
-import { act, fireEvent, screen, getByText } from '@testing-library/react';
+import { fireEvent, screen, getByText, waitFor } from '@testing-library/react';
 
 import QueryBuilder from '../QueryBuilder';
 
 import { resetUuidV1 } from '../../../../__mocks__/uuid';
 import renderWithRedux from '../../../shared/__test-helpers__/RenderWithRedux';
-import apiUrls from '../../../shared/config/apiUrls';
-import { Namespace } from '../../../shared/types/namespaces';
 import searchTermData from './__mocks__/configure_search-term.json';
 
 import useDataApi from '../../../shared/hooks/useDataApi';
 jest.mock('../../../shared/hooks/useDataApi');
 
-// const mock = useDataApi;
-// mock
-//   .onGet(apiUrls.queryBuilderTerms(Namespace.uniprotkb))
-//   .reply(200, searchTermData);
-
 let rendered;
 
 const history = createMemoryHistory();
+let onCancel;
 
 describe('QueryBuilder', () => {
   beforeEach(async () => {
+    onCancel = jest.fn();
     resetUuidV1();
-    useDataApi.mockReturnValue({ data: searchTermData });
+    (useDataApi as jest.Mock).mockReturnValue({ data: searchTermData });
 
-    await act(async () => {
-      rendered = renderWithRedux(<QueryBuilder />, { history });
+    rendered = renderWithRedux(<QueryBuilder onCancel={onCancel} />, {
+      history,
     });
   });
 
   // only exception where we want different payload
   test('should render loading', async () => {
-    useDataApi.mockReturnValue({ loading: true });
+    (useDataApi as jest.Mock).mockReturnValue({ loading: true });
 
-    await act(async () => {
-      rendered = renderWithRedux(<QueryBuilder />, { history });
-    });
+    rendered = renderWithRedux(<QueryBuilder onCancel={onCancel} />);
 
     expect(rendered.asFragment()).toMatchSnapshot();
   });
@@ -54,11 +47,22 @@ describe('QueryBuilder', () => {
     expect(clauses.length).toBe(5);
   });
 
-  test('should remove a clause', () => {
-    const { getAllByTestId } = rendered;
-    fireEvent.click(getAllByTestId('clause-list-button-remove')[1]);
-    const clauses = getAllByTestId('search__clause');
+  test('should remove clauses', () => {
+    // Go through all the default clauses one by one and remove them
+    fireEvent.click(screen.getAllByTestId('clause-list-button-remove')[0]);
+    let clauses = screen.getAllByTestId('search__clause');
     expect(clauses.length).toBe(3);
+    fireEvent.click(screen.getAllByTestId('clause-list-button-remove')[0]);
+    clauses = screen.getAllByTestId('search__clause');
+    expect(clauses.length).toBe(2);
+    fireEvent.click(screen.getAllByTestId('clause-list-button-remove')[0]);
+    clauses = screen.getAllByTestId('search__clause');
+    expect(clauses.length).toBe(1);
+    // Check special case where only one clause left,
+    // a new empty clause should be added
+    fireEvent.click(screen.getAllByTestId('clause-list-button-remove')[0]);
+    clauses = screen.getAllByTestId('search__clause');
+    expect(clauses.length).toBe(1);
   });
 
   test('should select field', () => {
@@ -69,26 +73,28 @@ describe('QueryBuilder', () => {
     });
     const clause = dropdownButton.closest('[data-testid="search__clause"]');
     fireEvent.click(dropdownButton);
-    let entryNameFieldOption = getByText(clause, /Entry Name \[ID\]/);
+    let entryNameFieldOption = getByText(
+      clause as HTMLElement,
+      /Entry Name \[ID\]/
+    );
     fireEvent.click(entryNameFieldOption);
     entryNameField = screen.queryByPlaceholderText('P53_HUMAN');
     expect(entryNameField).toBeTruthy();
   });
 
-  test('should submit a simple query', () => {
-    const { getByTestId, getAllByTestId, getByPlaceholderText } = rendered;
-    const input = getByPlaceholderText(/ydj1/);
+  test('should submit a simple query', async () => {
+    const input = screen.getByPlaceholderText(/ydj1/);
     fireEvent.change(input, { target: { value: 'zen' } });
-    const all = getByPlaceholderText(/a4_human/);
+    const all = screen.getByPlaceholderText(/a4_human/);
     fireEvent.change(all, { target: { value: 'eve' } });
-    const reviewedLogic = getAllByTestId('query-builder-logic-select');
+    const reviewedLogic = screen.getAllByTestId('query-builder-logic-select');
     // Note this should be 1 as the first line shouldn't have one
     fireEvent.change(reviewedLogic[3], { target: { value: 'OR' } });
-    // FIXME: These next lines are the ones triggering the warning even though
-    // FIXME: we are indeed using act...
-    act(() => {
-      fireEvent.submit(getByTestId('query-builder-form'));
-    });
+    const search = screen.getByRole('button', { name: 'Search' });
+    fireEvent.click(search);
+
+    expect(onCancel).toHaveBeenCalledTimes(1);
+    expect(history.location.pathname).toBe('/uniprotkb');
     expect(history.location.search).toBe('?query=(gene:zen) OR eve');
   });
 });
