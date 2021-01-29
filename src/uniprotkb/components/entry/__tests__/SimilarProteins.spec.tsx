1c9a957dab645938f25b52672d1134c80946090d
Xavier Watkins <xavier.watkins@gmail.com>
2021-01-29 16:53:02 +0000

    Update test syntax

--- src/uniprotkb/components/entry/__tests__/SimilarProteins.spec.tsx
+++ src/uniprotkb/components/entry/__tests__/SimilarProteins.spec.tsx
@@ -1,5 +1,5 @@
 import { act } from 'react-dom/test-utils';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import renderWithRedux from '../../../../shared/__test-helpers__/RenderWithRedux';
 import SimilarProteins from '../similar-proteins/SimilarProteins';
 import similarProteinsData from './__mocks__/similarProteinsData.json';
@@ -12,28 +12,26 @@ var axiosMock = new MockAdapter(axios);
 axiosMock.onGet(/\/uniref\/search/).reply(200, similarProteinsData);
 axiosMock.onGet(/\/uniprotkb\/accessions/).reply(200, accessionsData);
 
-const getRendered = async () => {
-  let rendered;
-  await act(async () => {
-    rendered = renderWithRedux(
-      <SimilarProteins
-        primaryAccession="P05067"
-        isoforms={{ isoforms: ['P05067-4'] }}
-      />
-    );
-  });
-  return rendered;
-};
+let rendered;
 
 describe('SimilarProteins tests', () => {
+  beforeEach(async () => {
+    await act(async () => {
+      rendered = renderWithRedux(
+        <SimilarProteins
+          primaryAccession="P05067"
+          isoforms={{ isoforms: ['P05067-4'] }}
+        />
+      );
+    });
+  });
+
   it('should call useDataApi and render', async () => {
-    const { findAllByText } = await getRendered();
-    expect(await findAllByText(/0FGN2/)).toBeTruthy();
+    expect(await screen.findAllByText(/0FGN2/)).toBeTruthy();
   });
 
   it('should change tabs', async () => {
-    const { findByText } = await getRendered();
-    fireEvent.click(await findByText(/50% identity/));
-    expect(await findByText(/P12023/)).toBeTruthy();
+    fireEvent.click(await screen.findByRole('tab', { name: /50% identity/ }));
+    expect(await screen.findByText(/P12023/)).toBeTruthy();
   });
 });
