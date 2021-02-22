605186331fa9858f3b051dc4df2275c052f4110c
Daniel Rice <dlrice@ebi.ac.uk>
2021-02-22 15:26:47 +0000

    Convert proteomes entry model data to ts. Add redundantTo & isolate to proteomes model.

--- src/proteomes/adapters/proteomesConverter.ts
+++ src/proteomes/adapters/proteomesConverter.ts
@@ -14,7 +14,7 @@ export type Component = {
   proteinCount: number; // used in the entry for each component TODO: eventually will be supported by backend in 2021_02 - 2021_03
 };
 
-enum CpdStatus {
+export enum CpdStatus {
   CLOSE_TO_STANDARD = 'Close to Standard',
   STANDARD = 'Standard',
   OUTLIER = 'Outlier',
@@ -81,9 +81,11 @@ export type ProteomesAPIModel = {
   genomeAnnotation: GenomeAnnotation;
   taxonLineage: Lineage[];
   strain: string;
+  isolate?: string;
   panproteome: string;
   description: string;
   redundantProteomes: RedundantProteome[];
+  redundantTo: string;
   proteinCount: number; // use this in the results table - calculated sum of the components proteinCount: components.reduce((total, { proteinCount }) => proteinCount + total, 0)  TODO: eventually will be supported by backend in in 2021_02 - 2021_03
 };
 
