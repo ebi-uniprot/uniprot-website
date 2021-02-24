7f6069f808187c5e84d758a01c9a9ea8357887f6
Daniel Rice <dlrice@ebi.ac.uk>
2021-02-24 19:01:01 +0000

    Remove enableFetchMocks.

--- src/uniprotkb/components/entry/__tests__/Entry.spec.tsx
+++ src/uniprotkb/components/entry/__tests__/Entry.spec.tsx
@@ -1,5 +1,3 @@
-import { enableFetchMocks } from 'jest-fetch-mock';
-enableFetchMocks();
 import { Route } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
 import MockAdapter from 'axios-mock-adapter';
