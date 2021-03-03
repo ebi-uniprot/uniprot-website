f1595cb13ecf6989e7d845759b54273944a0b127
Xavier Watkins <xavier.watkins@gmail.com>
2021-03-03 17:08:03 +0000

    New component to reuse column renderers

---
+++ src/shared/components/results/RenderColumnInCard.tsx
@@ -0,0 +1,26 @@
+import { ColumnConfiguration } from '../../types/columnConfiguration';
+
+type RenderColumnInCardProps<T, R extends Record<string, unknown>> = {
+  type: T;
+  data: R;
+  columnConfig: ColumnConfiguration<T, R>;
+};
+
+const RenderColumnInCard = <T, R extends Record<string, unknown>>(
+  props: RenderColumnInCardProps<T, R>
+) => {
+  const { type, data, columnConfig } = props;
+  const rendered = columnConfig.get(type)?.render(data);
+  return rendered ? (
+    <>
+      <span className="result-card__info-bit">
+        {columnConfig.get(type)?.label}
+        {': '}
+        {rendered}
+      </span>{' '}
+      {' · '}
+    </>
+  ) : null;
+};
+
+export default RenderColumnInCard;
