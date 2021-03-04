ca4e5bf2061cef98661b112f8afc595908a11e5e
Xavier Watkins <xavier.watkins@gmail.com>
2021-03-04 17:17:04 +0000

    Temporary component holder

--- src/uniparc/components/entry/UniParcFeaturesView.tsx
+++ src/uniparc/components/entry/UniParcFeaturesView.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 import { SequenceFeature } from '../../adapters/uniParcConverter';
 
-const UniParcFeaturesView: FC<{ data: SequenceFeature[] }> = ({ data }) => null;
+const UniParcFeaturesView: FC<{ data: SequenceFeature[] }> = ({ data }) => {
+  console.log(data);
+  return null;
+};
 export default UniParcFeaturesView;
