9967e6344efe947504de0562c807735c9ebd56c4
Daniel Rice <dlrice@ebi.ac.uk>
2021-03-11 12:51:18 +0000

    Remove ununsed getEntrySectionNameAndId from entrySection types.

--- src/proteomes/types/entrySection.ts
+++ src/proteomes/types/entrySection.ts
@@ -4,33 +4,4 @@ export enum EntrySection {
   Publications = 'publications',
 }
 
-export type EntrySectionNameAndId = {
-  name: string;
-  id: EntrySection;
-};
-
-export const getEntrySectionNameAndId = (
-  section: EntrySection
-  // eslint-disable-next-line consistent-return
-): EntrySectionNameAndId => {
-  // eslint-disable-next-line default-case
-  switch (section) {
-    case EntrySection.Overview:
-      return {
-        name: 'Overview',
-        id: EntrySection.Overview,
-      };
-    case EntrySection.Components:
-      return {
-        name: 'Components',
-        id: EntrySection.Components,
-      };
-    case EntrySection.Publications:
-      return {
-        name: 'Publications',
-        id: EntrySection.Publications,
-      };
-  }
-};
-
 export default EntrySection;
