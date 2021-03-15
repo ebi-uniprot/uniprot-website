c3ab25cdccde588fac06180664832773687ad4dc
Daniel Rice <dlrice@ebi.ac.uk>
2021-03-15 09:04:16 +0000

    Add todo for handling multiple simultaneous items in useItemSelect.

--- src/shared/hooks/useItemSelect.ts
+++ src/shared/hooks/useItemSelect.ts
@@ -5,6 +5,7 @@ const useItemSelect = (
 ): [selectedItems: string[], handleSelectedItem: (id: string) => void] => {
   const [selectedItems, setSelectedItems] = useState(items);
 
+  // TODO: handle multiple simultaneous item selections rather just than a single item.
   const handleEntrySelection = useCallback((rowId: string) => {
     setSelectedItems((selectedEntries) => {
       const filtered = selectedEntries.filter((id) => id !== rowId);
