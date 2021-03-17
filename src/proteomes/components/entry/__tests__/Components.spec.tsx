1ec7e9184e0413fdea6465adc998cf004619bde7
Daniel Rice <dlrice@ebi.ac.uk>
2021-03-17 08:34:42 +0000

    Update EntryMain test and create Components snapshot test.

--- src/proteomes/components/entry/__tests__/Components.spec.tsx
+++ src/proteomes/components/entry/__tests__/Components.spec.tsx
@@ -1,14 +1,15 @@
 import { render } from '@testing-library/react';
 import { MemoryRouter as Router } from 'react-router-dom';
-import EntryMain from '../EntryMain';
+import Components from '../Components';
 import proteomesConverter from '../../../adapters/proteomesConverter';
 import mockData from '../../../__mocks__/proteomesEntryModelData';
 
-describe('EntryMain view', () => {
+describe('Components view', () => {
   it('should render', () => {
+    const data = proteomesConverter(mockData);
     const { asFragment } = render(
       <Router>
-        <EntryMain transformedData={proteomesConverter(mockData)} />
+        <Components components={data.components} id={data.id} />
       </Router>
     );
     expect(asFragment()).toMatchSnapshot();
