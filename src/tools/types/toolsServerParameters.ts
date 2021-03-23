2e64d2b2cdb4a80957088e4b5318b441ec85c8a6
Aurélien Luciani <aurel-l@users.noreply.github.com>
2021-03-23 10:43:12 +0000

    add forgotten file

--- src/tools/types/toolsServerParameters.ts
+++ src/tools/types/toolsServerParameters.ts
@@ -1,7 +1,13 @@
 import { JobTypes } from './toolsJobTypes';
 
-import { ServerParameters as AlignSP } from '../align/types/alignServerParameters';
-import { ServerParameters as BlastSP } from '../blast/types/blastServerParameters';
+import {
+  ServerParameters as AlignSP,
+  PublicServerParameters as PublicAlignSP,
+} from '../align/types/alignServerParameters';
+import {
+  ServerParameters as BlastSP,
+  PublicServerParameters as PublicBlastSP,
+} from '../blast/types/blastServerParameters';
 import { ServerParameters as PeptideSearchSP } from '../peptide-search/types/peptideSearchServerParameters';
 
 export type ServerParameters = {
@@ -12,8 +18,8 @@ export type ServerParameters = {
 };
 
 export type PublicServerParameters = {
-  [JobTypes.ALIGN]: Omit<AlignSP, 'email'>;
-  [JobTypes.BLAST]: Omit<BlastSP, 'email'>;
+  [JobTypes.ALIGN]: PublicAlignSP;
+  [JobTypes.BLAST]: PublicBlastSP;
   [JobTypes.ID_MAPPING]: never; // TODO
   [JobTypes.PEPTIDE_SEARCH]: PeptideSearchSP;
 };
