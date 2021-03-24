0b5409b0c9916ef74c0f4d1e6c4d993689457e61
Daniel Rice <dlrice@ebi.ac.uk>
2021-03-24 18:03:06 +0000

    Incorporate file drag and drop.

--- src/tools/id-mapping/components/IDMappingForm.tsx
+++ src/tools/id-mapping/components/IDMappingForm.tsx
@@ -1,9 +1,18 @@
-import { useRef, FormEvent, useState, ChangeEvent } from 'react';
+import { useRef, FormEvent, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v1 } from 'uuid';
 import { PageIntro, Message } from 'franklin-sites';
+
+import useTextFileInput from '../../../shared/hooks/useTextFileInput';
 
 import infoMappings from '../../../shared/config/InfoMappings';
 
 import { JobTypes } from '../../types/toolsJobTypes';
+import { addMessage } from '../../../messages/state/messagesActions';
+import {
+  MessageFormat,
+  MessageLevel,
+} from '../../../messages/types/messagesTypes';
 
 import '../../styles/ToolsForm.scss';
 import idMappingFormStyle from './style/id-mapping-form.module.scss';
@@ -13,20 +22,37 @@ const reWhitespace = /\s+/;
 const IDMappingForm = () => {
   const { name, links, info } = infoMappings[JobTypes.ID_MAPPING];
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [textIDs, setTextIDs] = useState<string>('');
   const [ids, setIDs] = useState<string[]>([]);
+  const dispatch = useDispatch();
 
   const submitIDMappingJob = (event: FormEvent | MouseEvent) => {
     event.preventDefault();
   };
 
-  const handleIDTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    const text = event.target.value;
+  const handleIDTextChange = (text: string) => {
+    setTextIDs(text);
     setIDs(text.split(reWhitespace).filter(Boolean));
   };
 
   const handleReset = (event: FormEvent) => {
     event.preventDefault();
   };
+
+  useTextFileInput({
+    input: fileInputRef.current,
+    onFileContent: (content) => handleIDTextChange(content),
+    onError: (error) =>
+      dispatch(
+        addMessage({
+          id: v1(),
+          content: error.message,
+          format: MessageFormat.POP_UP,
+          level: MessageLevel.FAILURE,
+        })
+      ),
+    dndOverlay: <span>Drop your input file anywhere on this page</span>,
+  });
 
   return (
     <>
@@ -54,7 +80,10 @@ const IDMappingForm = () => {
               spellCheck="false"
               placeholder="P31946 P62258 ALBU_HUMAN EFTU_ECOLI"
               className={idMappingFormStyle['id-text-input']}
-              onChange={handleIDTextChange}
+              value={textIDs}
+              onChange={(event) => {
+                handleIDTextChange(event.target.value);
+              }}
             />
             {ids.length > 0 && (
               <Message level="info">
