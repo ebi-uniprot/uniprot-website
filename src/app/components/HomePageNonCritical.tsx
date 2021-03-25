5188cb65af1ef973fd0011814a6fedf2e1838db8
Aurélien Luciani <aurel-l@users.noreply.github.com>
2021-03-25 15:35:53 +0000

    wip

--- src/app/components/HomePageNonCritical.tsx
+++ src/app/components/HomePageNonCritical.tsx
@@ -1,10 +1,12 @@
+import { memo } from 'react';
 import {
   Tile,
   HeroContainer,
   DecoratedListItem,
-  ExternalLink,
+  SwissProtIcon,
+  TremblIcon,
 } from 'franklin-sites';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import cn from 'classnames';
 
 import colors from '../../../node_modules/franklin-sites/src/styles/colours.json';
@@ -27,254 +29,281 @@ import ProgrammaticIllustration from '../../images/programmatic_illustration.svg
 import TechDocIllustration from '../../images/tech_doc_illustration.svg';
 
 import PlaceHolder from './PlaceHolder';
-import { Location, LocationToPath } from '../config/urls';
+
+import { jobTypeToPath, Location, LocationToPath } from '../config/urls';
+
+import { JobTypes } from '../../tools/types/toolsJobTypes';
 
 import styles from './styles/home-page-non-critical.module.scss';
 
-const HomePageNonCritical = () => {
-  const history = useHistory();
+const getNamespaceTo = (location: Location) => ({
+  pathname: LocationToPath[location],
+  search: `query=*`,
+});
 
-  const getNamespaceTo = (location: Location) => ({
-    pathname: LocationToPath[location],
-    search: `query=*`,
-  });
-
-  return (
-    <>
-      <section className="uniprot-grid uniprot-grid--centered">
-        <Tile
-          title="Proteins"
-          className="uniprot-grid-cell--span-3"
-          subtitle="UniProt Knowledgebase"
-          description={
-            <section>
-              <span>
-                <h6>Reviewed</h6>SwissProt
-              </span>
-              <span>
-                <h6>Unreviewed</h6>TrEMBL
-              </span>
-            </section>
-          }
-          backgroundImage={<UniProtKBIllustration />}
-          backgroundColor={colors.seaBlue}
-          gradient
-          onClick={() =>
-            history.push(getNamespaceTo(Location.UniProtKBResults))
-          }
-        />
-        <Tile
-          title="Species"
-          className="uniprot-grid-cell--span-3"
-          subtitle="Proteomes"
-          description="Protein sets for species with sequenced genomes from across the tree of life."
-          backgroundImage={<SpeciesIllustration />}
-          backgroundColor={colors.proteomes}
-          onClick={() =>
-            history.push(getNamespaceTo(Location.ProteomesResults))
-          }
-          gradient
-        />
-        <Tile
-          title="Protein Clusters"
-          className="uniprot-grid-cell--span-3"
-          subtitle="UniRef"
-          description="Clusters of protein sequences at 100%, 90% &amp; 50% identity."
-          backgroundImage={<ClusterIllustration />}
-          backgroundColor={colors.uniref}
-          onClick={() => history.push(getNamespaceTo(Location.UniRefResults))}
-          gradient
-        />
-        <Tile
-          title="Sequence Archive"
-          className="uniprot-grid-cell--span-3"
-          subtitle="UniParc"
-          description="Non-redundant archive of publicly available protein sequences seen across different databases."
-          backgroundImage={<ArchiveIllustration />}
-          backgroundColor={colors.uniparc}
-          onClick={() => history.push(getNamespaceTo(Location.UniParcResults))}
-          gradient
-        />
-      </section>
-
-      <HeroContainer
-        className={cn(
-          'uniprot-grid',
-          'uniprot-grid--centered',
-          'uniprot-grid--with-bleed'
-        )}
-        titleClassName="uniprot-grid-cell--span-12"
-        noSidePadding
+const HomePageNonCritical = () => (
+  <>
+    <section className="uniprot-grid uniprot-grid--centered">
+      <h2 className="visually-hidden">UniProt core data</h2>
+      <Tile
+        title="Proteins"
+        className="uniprot-grid-cell--span-3"
+        subtitle="UniProt Knowledgebase"
+        backgroundImage={<UniProtKBIllustration />}
+        backgroundColor={colors.seaBlue}
+        to={getNamespaceTo(Location.UniProtKBResults)}
+        gradient
       >
-        <div className="uniprot-grid-cell--span-3">
-          <h3 className={styles['supporting-data-section__header']}>
-            Supporting Data
-          </h3>
-        </div>
-        <div className="uniprot-grid-cell--span-3">
-          <DecoratedListItem compact altStyle>
-            <Link to={getNamespaceTo(Location.TaxonomyResults)}>Taxonomy</Link>
-          </DecoratedListItem>
-        </div>
-        <div className="uniprot-grid-cell--span-3">
-          <DecoratedListItem compact altStyle>
-            <Link to={getNamespaceTo(Location.LocationsResults)}>
-              Subcellular locations
-            </Link>
-          </DecoratedListItem>
-        </div>
-        <div className="uniprot-grid-cell--span-3">
-          <DecoratedListItem compact altStyle>
-            {/* TODO: update link */}
-            <a href="//www.uniprot.org/unirule">UniRule automatic annotation</a>
-          </DecoratedListItem>
-        </div>
-        <div
-          className="uniprot-grid-cell--span-3"
-          style={{ display: 'inline-flex' }}
-        >
-          <DecoratedListItem compact altStyle inline>
-            <Link to={getNamespaceTo(Location.DiseasesResults)}>Diseases</Link>
-          </DecoratedListItem>
-          <DecoratedListItem compact altStyle inline>
-            <Link to={getNamespaceTo(Location.KeywordsResults)}>Keywords</Link>
-          </DecoratedListItem>
-        </div>
-        <div className="uniprot-grid-cell--span-3">
-          <DecoratedListItem compact altStyle>
-            <Link to={getNamespaceTo(Location.CitationsResults)}>
-              Literature Citations
-            </Link>
-          </DecoratedListItem>
-        </div>
-        <div className="uniprot-grid-cell--span-3">
-          <DecoratedListItem compact altStyle>
-            <Link to={getNamespaceTo(Location.DatabaseResults)}>
-              Cross-referenced databases
-            </Link>
-          </DecoratedListItem>
-        </div>
-        <div className="uniprot-grid-cell--span-3">
-          <DecoratedListItem compact altStyle>
-            {/* TODO: update link */}
-            <a href="//www.uniprot.org/arba">ARBA automatic annotation</a>
-          </DecoratedListItem>
-        </div>
-      </HeroContainer>
+        <span className={styles['proteins-section']}>
+          <Link
+            to={{
+              pathname: LocationToPath[Location.UniProtKBResults],
+              search: `facets=reviewed:true&query=*`,
+            }}
+          >
+            <SwissProtIcon width="2.5em" />
+            <div className="proteins-section__status">Reviewed</div>
+            <small>Swiss-Prot</small>
+          </Link>
+          <Link
+            to={{
+              pathname: LocationToPath[Location.UniProtKBResults],
+              search: `facets=reviewed:false&query=*`,
+            }}
+          >
+            <TremblIcon width="2.5em" />
+            <div className="proteins-section__status">Unreviewed</div>
+            <small>TrEMBL</small>
+          </Link>
+        </span>
+      </Tile>
+      <Tile
+        title="Species"
+        className="uniprot-grid-cell--span-3"
+        subtitle="Proteomes"
+        backgroundImage={<SpeciesIllustration />}
+        backgroundColor={colors.proteomes}
+        to={getNamespaceTo(Location.ProteomesResults)}
+        gradient
+      >
+        Protein sets for species with sequenced genomes from across the tree of
+        life
+      </Tile>
+      <Tile
+        title="Protein Clusters"
+        className="uniprot-grid-cell--span-3"
+        subtitle="UniRef"
+        backgroundImage={<ClusterIllustration />}
+        backgroundColor={colors.uniref}
+        to={getNamespaceTo(Location.UniRefResults)}
+        gradient
+      >
+        Clusters of protein sequences at 100%, 90% &amp; 50% identity
+      </Tile>
+      <Tile
+        title="Sequence Archive"
+        className="uniprot-grid-cell--span-3"
+        subtitle="UniParc"
+        backgroundImage={<ArchiveIllustration />}
+        backgroundColor={colors.uniparc}
+        to={getNamespaceTo(Location.UniParcResults)}
+        gradient
+      >
+        Non-redundant archive of publicly available protein sequences seen
+        across different databases
+      </Tile>
+    </section>
 
-      <HeroContainer
-        title="News"
-        className="uniprot-grid uniprot-grid--centered uniprot-grid--with-bleed"
-        titleClassName="uniprot-grid-cell--span-12"
+    <HeroContainer
+      className={cn(
+        'uniprot-grid',
+        'uniprot-grid--centered',
+        'uniprot-grid--with-bleed'
+      )}
+      titleClassName="uniprot-grid-cell--span-12"
+      noSidePadding
+    >
+      <div className="uniprot-grid-cell--span-3">
+        <h3 className={styles['supporting-data-section__header']}>
+          Supporting Data
+        </h3>
+      </div>
+      <div className="uniprot-grid-cell--span-3">
+        <DecoratedListItem compact altStyle>
+          <Link to={getNamespaceTo(Location.TaxonomyResults)}>Taxonomy</Link>
+        </DecoratedListItem>
+      </div>
+      <div className="uniprot-grid-cell--span-3">
+        <DecoratedListItem compact altStyle>
+          <Link to={getNamespaceTo(Location.LocationsResults)}>
+            Subcellular locations
+          </Link>
+        </DecoratedListItem>
+      </div>
+      <div className="uniprot-grid-cell--span-3">
+        <DecoratedListItem compact altStyle>
+          {/* TODO: update link */}
+          <a href="//www.uniprot.org/unirule">UniRule automatic annotation</a>
+        </DecoratedListItem>
+      </div>
+      <div
+        className="uniprot-grid-cell--span-3"
+        style={{ display: 'inline-flex' }}
       >
-        <section className="uniprot-grid-cell--span-12">
-          <PlaceHolder />
-        </section>
-      </HeroContainer>
+        <DecoratedListItem compact altStyle inline>
+          <Link to={getNamespaceTo(Location.DiseasesResults)}>Diseases</Link>
+        </DecoratedListItem>
+        <DecoratedListItem compact altStyle inline>
+          <Link to={getNamespaceTo(Location.KeywordsResults)}>Keywords</Link>
+        </DecoratedListItem>
+      </div>
+      <div className="uniprot-grid-cell--span-3">
+        <DecoratedListItem compact altStyle>
+          <Link to={getNamespaceTo(Location.CitationsResults)}>
+            Literature Citations
+          </Link>
+        </DecoratedListItem>
+      </div>
+      <div className="uniprot-grid-cell--span-3">
+        <DecoratedListItem compact altStyle>
+          <Link to={getNamespaceTo(Location.DatabaseResults)}>
+            Cross-referenced databases
+          </Link>
+        </DecoratedListItem>
+      </div>
+      <div className="uniprot-grid-cell--span-3">
+        <DecoratedListItem compact altStyle>
+          {/* TODO: update link */}
+          <a href="//www.uniprot.org/arba">ARBA automatic annotation</a>
+        </DecoratedListItem>
+      </div>
+    </HeroContainer>
 
-      <HeroContainer
-        title="Analysis Tools"
-        className="uniprot-grid uniprot-grid--centered uniprot-grid--with-bleed"
-        titleClassName="uniprot-grid-cell--span-12"
-      >
-        <Tile
-          title="BLAST"
-          className="uniprot-grid-cell--span-3"
-          description="Search with a sequence to find homologs through pairwise sequence alignment."
-          backgroundImage={<BlastIllustration />}
-          backgroundColor={colors.blast}
-          onClick={() => history.push({ pathname: Location.Blast })}
-          gradient
-        />
-        <Tile
-          title="ALIGN"
-          className="uniprot-grid-cell--span-3"
-          description="Align two or more protein sequences with Clustal Omega to find conserved regions."
-          backgroundImage={<AlignIllustration />}
-          backgroundColor={colors.align}
-          onClick={() => history.push({ pathname: Location.Align })}
-          gradient
-        />
-        <Tile
-          title="Search with Lists Map IDs"
-          className="uniprot-grid-cell--span-3"
-          description="Find proteins with lists of UniProt IDs or convert from/to other database IDs."
-          backgroundImage={<UploadListIllustration />}
-          backgroundColor={colors.idMapping}
-          onClick={() => history.push({ pathname: Location.IDMapping })}
-          gradient
-        />
-        <Tile
-          title="Search Peptides"
-          className="uniprot-grid-cell--span-3"
-          description="Search with a peptide sequence to find all UniProt proteins that contain exact matches."
-          backgroundImage={<PeptideSearchIllustration />}
-          backgroundColor={colors.peptideSearch}
-          onClick={() => history.push({ pathname: Location.PeptideSearch })}
-          gradient
-        />
-      </HeroContainer>
-      <HeroContainer
-        title="Need Help?"
-        className="uniprot-grid uniprot-grid--centered uniprot-grid--with-bleed"
-        titleClassName="uniprot-grid-cell--span-12"
-      >
-        <PlaceHolder />
-      </HeroContainer>
-      <HeroContainer
-        title="Download and APIs"
-        className="uniprot-grid uniprot-grid--centered uniprot-grid--with-bleed"
-        titleClassName="uniprot-grid-cell--span-12"
-      >
-        <Tile
-          title="FTP Download"
-          className="uniprot-grid-cell--span-3"
-          description="Get UniProt data for the latest release."
-          backgroundImage={<FTPIllustration />}
-          backgroundColor={colors.independence}
-          // eslint-disable-next-line no-return-assign
-          onClick={() =>
-            (window.location.href =
-              'https://ftp.uniprot.org/pub/databases/uniprot/')
-          }
-          gradient
-        />
-        <Tile
-          title="Technical Documentation"
-          className="uniprot-grid-cell--span-3"
-          description="Manuals, schemas and ontology descriptions."
-          backgroundImage={<TechDocIllustration />}
-          backgroundColor={colors.independence}
-          // TODO: update link
-          // eslint-disable-next-line no-return-assign
-          onClick={() =>
-            (window.location.href = 'https://www.uniprot.org/help/technical')
-          }
-          gradient
-        />
-        <Tile
-          title="Programmatic access"
-          className="uniprot-grid-cell--span-3"
-          description="REST API, SPARQL API, JAVA API."
-          backgroundImage={<ProgrammaticIllustration />}
-          backgroundColor={colors.independence}
-          // TODO: update link
-          // eslint-disable-next-line no-return-assign
-          onClick={() =>
-            (window.location.href =
-              'https://www.uniprot.org/help/programmatic_access')
-          }
-          gradient
-        />
-        <section className="uniprot-grid-cell--span-3">
-          <ExternalLink url="https://groups.google.com/u/1/g/ebi-proteins-api">
-            Join our Google group{' '}
-          </ExternalLink>
-          and learn more.
-        </section>
-      </HeroContainer>
-    </>
-  );
-};
+    <HeroContainer
+      title="News"
+      className="uniprot-grid uniprot-grid--centered uniprot-grid--with-bleed"
+      titleClassName="uniprot-grid-cell--span-12"
+    >
+      <PlaceHolder />
+    </HeroContainer>
 
-export default HomePageNonCritical;
+    <HeroContainer
+      title="Analysis Tools"
+      className="uniprot-grid uniprot-grid--centered uniprot-grid--with-bleed"
+      titleClassName="uniprot-grid-cell--span-12"
+    >
+      <Tile
+        title="BLAST"
+        className="uniprot-grid-cell--span-3"
+        backgroundImage={<BlastIllustration />}
+        backgroundColor={colors.blast}
+        to={jobTypeToPath(JobTypes.BLAST)}
+        gradient
+      >
+        Search with a sequence to find homologs through pairwise sequence
+        alignment
+      </Tile>
+      <Tile
+        title="ALIGN"
+        className="uniprot-grid-cell--span-3"
+        backgroundImage={<AlignIllustration />}
+        backgroundColor={colors.align}
+        to={jobTypeToPath(JobTypes.ALIGN)}
+        gradient
+      >
+        Align two or more protein sequences with Clustal Omega to find conserved
+        regions
+      </Tile>
+      <Tile
+        title="Search with Lists Map IDs"
+        className="uniprot-grid-cell--span-3"
+        backgroundImage={<UploadListIllustration />}
+        backgroundColor={colors.idMapping}
+        to={jobTypeToPath(JobTypes.ID_MAPPING)}
+        gradient
+      >
+        Find proteins with lists of UniProt IDs or convert from/to other
+        database IDs
+      </Tile>
+      <Tile
+        title="Search Peptides"
+        className="uniprot-grid-cell--span-3"
+        backgroundImage={<PeptideSearchIllustration />}
+        backgroundColor={colors.peptideSearch}
+        to={jobTypeToPath(JobTypes.PEPTIDE_SEARCH)}
+        gradient
+      >
+        Search with a peptide sequence to find all UniProt proteins that contain
+        exact matches
+      </Tile>
+    </HeroContainer>
+    <HeroContainer
+      title="Need Help?"
+      className="uniprot-grid uniprot-grid--centered uniprot-grid--with-bleed"
+      titleClassName="uniprot-grid-cell--span-12"
+    >
+      <PlaceHolder />
+    </HeroContainer>
+    <HeroContainer
+      title="UniProt data"
+      className="uniprot-grid uniprot-grid--centered uniprot-grid--with-bleed"
+      titleClassName="uniprot-grid-cell--span-12"
+    >
+      <Tile
+        title="FTP Download"
+        className="uniprot-grid-cell--span-3"
+        backgroundImage={<FTPIllustration />}
+        backgroundColor={colors.independence}
+        url="https://ftp.uniprot.org/pub/databases/uniprot/"
+        gradient
+      >
+        Download UniProt release data
+      </Tile>
+      <Tile
+        title={
+          <>
+            Technical
+            <span className={styles['forced-break']}>documentation</span>
+          </>
+        }
+        className="uniprot-grid-cell--span-3"
+        backgroundImage={<TechDocIllustration />}
+        backgroundColor={colors.independence}
+        // TODO: update link
+        url="https://www.uniprot.org/help/technical"
+        gradient
+      >
+        Manuals, schemas and ontology descriptions
+      </Tile>
+      <Tile
+        title={
+          <>
+            Programmatic
+            <span className={styles['forced-break']}>access</span>
+          </>
+        }
+        className="uniprot-grid-cell--span-3"
+        backgroundImage={<ProgrammaticIllustration />}
+        backgroundColor={colors.independence}
+        // TODO: update link
+        url="https://www.uniprot.org/help/programmatic_access"
+        gradient
+      >
+        Query UniProt data using APIs providing REST, SPARQL and Java services
+      </Tile>
+      <Tile
+        title="Submit data"
+        className="uniprot-grid-cell--span-3"
+        backgroundImage={<ProgrammaticIllustration />}
+        backgroundColor={colors.independence}
+        // TODO: update link
+        url="https://www.uniprot.org/help/submissions"
+        gradient
+      >
+        Submit your sequences, publications and annotation updates
+      </Tile>
+    </HeroContainer>
+  </>
+);
+
+export default memo(HomePageNonCritical);
