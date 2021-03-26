1639df0bf7cc04f482a8e7a02a7f8248802ee6b7
Xavier Watkins <xavier.watkins@gmail.com>
2021-03-26 16:23:18 +0000

    Uncessecary import

--- src/shared/components/download/__tests__/Download.spec.tsx
+++ src/shared/components/download/__tests__/Download.spec.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen, waitFor } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 
 import customRender from '../../../__test-helpers__/customRender';
 
