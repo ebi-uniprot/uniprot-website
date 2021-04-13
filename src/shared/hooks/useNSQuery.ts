9d21b3fc9300cec3342b26e00b6eaa713fcba004
Xavier Watkins <xavier.watkins@gmail.com>
2021-04-13 10:01:05 +0100

    Add hook for query

---
+++ src/shared/hooks/useNSQuery.ts
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { getParamsFromURL } from '../../uniprotkb/utils/resultsUtils';
+import { getAPIQueryUrl } from '../config/apiUrls';
+import { Column } from '../config/columns';
+
+import useNS from './useNS';
+
+const useNSQuery = ({
+  queryColumns,
+  facets,
+  size,
+}: {
+  queryColumns?: Column[];
+  facets?: string[];
+  size?: number;
+}) => {
+  const namespace = useNS();
+  const location = useLocation();
+  const [url, setUrl] = useState<string>();
+
+  const { search: queryParamFromUrl } = location;
+  const {
+    query,
+    selectedFacets,
+    sortColumn,
+    sortDirection,
+    direct,
+  } = getParamsFromURL(queryParamFromUrl);
+
+  useEffect(() => {
+    setUrl(
+      getAPIQueryUrl({
+        namespace,
+        query,
+        columns: queryColumns,
+        selectedFacets,
+        facets,
+        sortColumn,
+        sortDirection,
+        size,
+      })
+    );
+  }, [
+    namespace,
+    query,
+    queryColumns,
+    selectedFacets,
+    facets,
+    sortColumn,
+    sortDirection,
+    size,
+  ]);
+
+  return { url, sortColumn, sortDirection, direct };
+};
+
+export default useNSQuery;
