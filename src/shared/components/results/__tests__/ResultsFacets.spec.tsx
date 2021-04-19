94fcd4a8a138578e99b89e007738d510bc4160c8
Daniel Rice <dlrice@ebi.ac.uk>
2021-04-19 17:40:23 +0100

    Update ResultsFacets file.

--- src/shared/components/results/__tests__/ResultsFacets.spec.tsx
+++ src/shared/components/results/__tests__/ResultsFacets.spec.tsx
@@ -4,17 +4,37 @@ import ResultsFacets from '../ResultsFacets';
 
 import customRender from '../../../__test-helpers__/customRender';
 
-import '../../../../uniprotkb/components/__mocks__/mockApi';
+import response, {
+  url,
+} from '../../../../uniprotkb/components/__mocks__/response';
+
+const dataApiObject = {
+  ...response,
+  loading: false,
+  progress: 1,
+  status: 200,
+  statusText: '',
+  url,
+};
 
 describe('ResultsFacets', () => {
-  test('should select a facet', async () => {
-    const { history } = customRender(<ResultsFacets />, {
-      route: '/uniprotkb?query=blah',
-    });
-    expect(history.location.search).toEqual('?query=blah');
-    const unreviewedButton = await screen.findByText(
-      'Unreviewed (TrEMBL) (455)'
+  const resultsFacets = (route: string) =>
+    customRender(
+      <ResultsFacets
+        dataApiObject={dataApiObject}
+        total={+response.headers['x-totalrecords']}
+      />,
+      {
+        route,
+      }
     );
+
+  const regexUnreviewed = /Unreviewed \(TrEMBL\) \(\d+\)/;
+
+  test('should select a facet', async () => {
+    const { history } = resultsFacets('/uniprotkb?query=blah');
+    expect(history.location.search).toEqual('?query=blah');
+    const unreviewedButton = await screen.findByText(regexUnreviewed);
     fireEvent.click(unreviewedButton);
     await waitFor(() => {
       expect(history.location.search).toEqual(
@@ -24,12 +44,11 @@ describe('ResultsFacets', () => {
   });
 
   it('should deselect a facet', async () => {
-    const { history } = customRender(<ResultsFacets />, {
-      route: '/uniprotkb?query=blah&facets=reviewed:false',
-    });
-    const unreviewedButton = await screen.findByText(
-      'Unreviewed (TrEMBL) (455)'
+    const { history } = resultsFacets(
+      '/uniprotkb?query=blah&facets=reviewed:false'
     );
+
+    const unreviewedButton = await screen.findByText(regexUnreviewed);
     fireEvent.click(unreviewedButton);
     await waitFor(() => {
       expect(history.location.search).toEqual('?query=blah');
