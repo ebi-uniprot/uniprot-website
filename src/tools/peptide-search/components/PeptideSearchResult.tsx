be28a3aac8a4dcf94f59d884aee46ae8fb74a75c
Daniel Rice <dlrice@ebi.ac.uk>
2021-04-22 16:29:59 +0100

    If no accessions in the job results, correctly parse as empty array.

--- src/tools/peptide-search/components/PeptideSearchResult.tsx
+++ src/tools/peptide-search/components/PeptideSearchResult.tsx
@@ -40,7 +40,7 @@ const PeptideSearchResult: FC = () => {
   } = useDataApi<PeptideSearchResults>(
     urls.resultUrl(match?.params.id || '', {})
   );
-  const accessions = jobData?.split(',');
+  const accessions = jobData?.split(',').filter(Boolean);
 
   // Query for facets
   const { url: initialApiFacetUrl } = useNSQuery({
@@ -52,7 +52,6 @@ const PeptideSearchResult: FC = () => {
   const facetApiObject = useDataApiWithStale<Response['data']>(
     initialApiFacetUrl
   );
-
   const {
     loading: facetInititialLoading,
     headers: facetHeaders,
