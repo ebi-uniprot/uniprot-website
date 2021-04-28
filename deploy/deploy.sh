8cca58f3f35a5da53ddd4b73736d48b8be7fe6ee
Daniel Rice <dlrice@ebi.ac.uk>
2021-04-28 18:50:30 +0100

    Debugging deployment.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,14 +7,16 @@ fi
 
 echo "--- Deploying ${DC} ---"
 pwd
+env
 
 K8S_CERTIFICATE=${DC}_K8S_CERTIFICATE
 K8S_URL=${DC}_K8S_URL
 K8S_USER_TOKEN=${DC}_K8S_USER_TOKEN
 
 mkdir -p ~/.kube
-sed "s~__K8S_CERTIFICATE__~${K8S_CERTIFICATE}~; s~__K8S_URL__~${K8S_URL}~; s~__K8S_USER_TOKEN__~${K8S_USER_TOKEN}~" deploy/wp-config-template.yml > ~/.kube/config
+sed "s~__K8S_CERTIFICATE__~${!K8S_CERTIFICATE}~; s~__K8S_URL__~${!K8S_URL}~; s~__K8S_USER_TOKEN__~${!K8S_USER_TOKEN}~" deploy/wp-config-template.yml > ~/.kube/config
 chmod 700 ~/.kube/config
+cat ~/.kube/config
 kubectl config use-context team-admin-wp-webadmin-02
 kubectl config set-context --current --namespace=uniprot-front-end
 printf "$(kubectl create secret docker-registry gitlab-registry --docker-server=$CI_REGISTRY --docker-username=$CI_DEPLOY_USER --docker-password=$CI_DEPLOY_PASSWORD --docker-email=$GITLAB_USER_EMAIL -o yaml --dry-run=client)" | kubectl apply -f -
