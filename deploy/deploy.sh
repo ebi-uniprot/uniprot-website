926df543e2d16139807fb17f10dd055cdc899478
Daniel Rice <dlrice@ebi.ac.uk>
2021-04-28 11:55:22 +0100

    Wait for deployment to run before reporting status. Deploy HX for testing purposes.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -18,10 +18,21 @@ kubectl config use-context team-admin-wp-webadmin-02
 kubectl config set-context --current --namespace=uniprot-front-end
 printf "$(kubectl create secret docker-registry gitlab-registry --docker-server=$CI_REGISTRY --docker-username=$CI_DEPLOY_USER --docker-password=$CI_DEPLOY_PASSWORD --docker-email=$GITLAB_USER_EMAIL -o yaml --dry-run=client)" | kubectl apply -f -
 kubectl apply -f k8s_deploy.yml
-kubectl get all
-kubectl get nodes
 
-if [ $(kubectl get pods -o=jsonpath="{.items[*].status.phase}") != "Running" ]; then
-    echo "Error: deployment of ${DC} unsuccessful, exiting"
+MAX_ATTEMPTS=20
+COUNTER=0
+while [[ (( $COUNTER < $MAX_ATTEMPTS )) && $(kubectl get pods -o=jsonpath="{.items[0].status.phase}") != "Running" ]]
+do
+    echo "waiting for pod" && sleep 1;
+    COUNTER=$(( $COUNTER + 1 ))
+done
+if (( $COUNTER == $MAX_ATTEMPTS ))
+then
+    echo "Deployment of ${DC} unsuccessful. Exiting."
     exit 1
 fi
+
+kubectl get all
+NODE_PORT=$(kubectl get services -o=jsonpath='{.items[0].spec.ports[0].nodePort}')
+NODE_HOST_NAME=$(kubectl get nodes -o=jsonpath='{.items[0].status.addresses[?(@.type=="Hostname")].address}')
+echo $'\n'${DC} deployed. Internally avaialable at: http://$NODE_HOST_NAME:$NODE_PORT
