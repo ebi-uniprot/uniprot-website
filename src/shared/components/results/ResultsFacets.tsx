854f8e0537354ae4d690b1ffb02057bbe8a92fc0
Xavier Watkins <xavier.watkins@gmail.com>
2021-05-13 14:54:47 +0100

    Handle proteome valut too

--- src/shared/components/results/ResultsFacets.tsx
+++ src/shared/components/results/ResultsFacets.tsx
@@ -5,6 +5,7 @@ import {
   Loader,
   SwissProtIcon,
   TremblIcon,
+  ReferenceProteomeIcon,
 } from 'franklin-sites';
 
 import useNS from '../../hooks/useNS';
@@ -21,21 +22,36 @@ import './styles/results-data.scss';
 
 const ICON_HEIGHT = '1em';
 
-const getDecoratedFacetValue = (facetValue: FacetValue) => ({
-  ...facetValue,
-  label:
-    facetValue.value === 'true' ? (
-      <>
-        <SwissProtIcon height={ICON_HEIGHT} className="icon--reviewed" />
-        {facetValue.label}
-      </>
-    ) : (
-      <>
-        <TremblIcon height={ICON_HEIGHT} className="icon--unreviewed" />
-        {facetValue.label}
-      </>
-    ),
-});
+const getDecoratedFacetLabel = (facetValue: FacetValue) => {
+  switch (facetValue.value) {
+    case 'true':
+      return (
+        <>
+          <SwissProtIcon height={ICON_HEIGHT} className="icon--reviewed" />
+          {facetValue.label}
+        </>
+      );
+    case 'false':
+      return (
+        <>
+          <TremblIcon height={ICON_HEIGHT} className="icon--unreviewed" />
+          {facetValue.label}
+        </>
+      );
+    case '1':
+      return (
+        <>
+          <ReferenceProteomeIcon
+            height={ICON_HEIGHT}
+            className="icon--reference-proteome"
+          />
+          {facetValue.label}
+        </>
+      );
+    default:
+      return facetValue.label;
+  }
+};
 
 const ResultsFacets: FC<{
   dataApiObject: UseDataAPIWithStaleState<Response['data']>;
@@ -57,12 +73,13 @@ const ResultsFacets: FC<{
 
   // Add relevant icons
   const facetsWithIcons = facets.map((facet) =>
-    facet.name === 'reviewed'
+    facet.name === 'reviewed' || facet.name === 'proteome_type'
       ? {
           ...facet,
-          values: facet.values?.map((facetValue) =>
-            getDecoratedFacetValue(facetValue)
-          ),
+          values: facet.values?.map((facetValue) => ({
+            ...facetValue,
+            label: getDecoratedFacetLabel(facetValue),
+          })),
         }
       : facet
   );
