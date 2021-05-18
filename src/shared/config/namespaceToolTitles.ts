9abc571dabf4df10aaa7f07bfcb1c88f4e7079cc
Xavier Watkins <xavier.watkins@gmail.com>
2021-05-18 10:46:20 +0100

    Nicer

--- src/shared/config/namespaceToolTitles.ts
+++ src/shared/config/namespaceToolTitles.ts
@@ -1,7 +1,7 @@
-import { Namespace } from '../types/namespaces';
+import { Namespace, SearchableNamespace } from '../types/namespaces';
 import { JobTypes } from '../../tools/types/toolsJobTypes';
 
-const titles: Record<Namespace | JobTypes, string> = {
+const titles: Record<SearchableNamespace | JobTypes, string> = {
   // Main data
   [Namespace.uniprotkb]: 'UniProtKB',
   [Namespace.uniref]: 'UniRef',
@@ -15,7 +15,6 @@ const titles: Record<Namespace | JobTypes, string> = {
   [Namespace.database]: 'Cross-referenced databases',
   [Namespace.locations]: 'Subcellular locations',
   // Tools
-  [Namespace.idmapping]: 'Retrieve/ID mapping',
   [JobTypes.ID_MAPPING]: 'Retrieve/ID mapping',
   [JobTypes.ALIGN]: 'Align',
   [JobTypes.BLAST]: 'BLAST',
