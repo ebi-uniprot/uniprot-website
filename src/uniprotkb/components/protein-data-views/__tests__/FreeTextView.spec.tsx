35cdb15f64cb8c698bebe0ac3b4d13b9e0e46f8c
Aurélien Luciani <aurel-l@users.noreply.github.com>
2021-05-26 09:27:46 +0100

    improve tests

--- src/uniprotkb/components/protein-data-views/__tests__/FreeTextView.spec.tsx
+++ src/uniprotkb/components/protein-data-views/__tests__/FreeTextView.spec.tsx
@@ -1,12 +1,23 @@
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import customRender from '../../../../shared/__test-helpers__/customRender';
 
 import FreeTextView from '../FreeTextView';
 
-import FreeTextUIData from './__mocks__/freeTextUIData';
+import freeTextUIData from './__mocks__/freeTextUIData';
 
 describe('FreeText component', () => {
   test('should render free text CC', () => {
-    const { asFragment } = render(<FreeTextView comments={FreeTextUIData} />);
+    const { asFragment } = customRender(
+      <FreeTextView comments={freeTextUIData} />
+    );
     expect(asFragment()).toMatchSnapshot();
+
+    // PubMed links
+    expect(screen.getAllByRole('link')).toHaveLength(2);
+
+    // Publication button / evidence tag
+    expect(
+      screen.getByRole('button', { name: '1 publication' })
+    ).toBeInTheDocument();
   });
 });
