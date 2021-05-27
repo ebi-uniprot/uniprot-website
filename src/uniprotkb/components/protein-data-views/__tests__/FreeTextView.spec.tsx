b4753bba8286db235fec96d178cac0682cb39933
Aurélien Luciani <aurel-l@users.noreply.github.com>
2021-05-27 12:54:45 +0100

    more edge cases

--- src/uniprotkb/components/protein-data-views/__tests__/FreeTextView.spec.tsx
+++ src/uniprotkb/components/protein-data-views/__tests__/FreeTextView.spec.tsx
@@ -6,9 +6,24 @@ import FreeTextView from '../FreeTextView';
 import freeTextUIData from './__mocks__/freeTextUIData';
 
 describe('FreeText component', () => {
-  test('should render free text CC', () => {
+  it('should render free text CC', () => {
     const { asFragment } = customRender(
-      <FreeTextView comments={freeTextUIData} />
+      <FreeTextView comments={[freeTextUIData[0]]} />
+    );
+    expect(asFragment()).toMatchSnapshot();
+
+    // PubMed links
+    expect(screen.queryAllByRole('link')).toHaveLength(0);
+
+    // Publication button / evidence tag
+    expect(
+      screen.getByRole('button', { name: '1 publication' })
+    ).toBeInTheDocument();
+  });
+
+  it('should render free text CC with inlined pubmeds', () => {
+    const { asFragment } = customRender(
+      <FreeTextView comments={[freeTextUIData[1]]} />
     );
     expect(asFragment()).toMatchSnapshot();
 
@@ -19,5 +34,18 @@ describe('FreeText component', () => {
     expect(
       screen.getByRole('button', { name: '1 publication' })
     ).toBeInTheDocument();
+  });
+
+  it('should render free text CC, without evidences nor inlined pubmeds', () => {
+    const { asFragment } = customRender(
+      <FreeTextView comments={[freeTextUIData[0]]} noEvidence />
+    );
+    expect(asFragment()).toMatchSnapshot();
+
+    // PubMed links
+    expect(screen.queryAllByRole('link')).toHaveLength(0);
+
+    // Publication button / evidence tag
+    expect(screen.queryByRole('button')).not.toBeInTheDocument();
   });
 });
--- src/uniprotkb/components/protein-data-views/__tests__/__mocks__/freeTextUIData.ts
+++ src/uniprotkb/components/protein-data-views/__tests__/__mocks__/freeTextUIData.ts
@@ -6,6 +6,22 @@ const mock: FreeTextComment[] = [
     texts: [
       {
         value:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        evidences: [
+          {
+            evidenceCode: 'ECO:0000269',
+            source: 'PubMed',
+            id: '31182795',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    commentType: 'FUNCTION',
+    texts: [
+      {
+        value:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua (PubMed:1234567, PubMed:12345678). Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         evidences: [
           {
