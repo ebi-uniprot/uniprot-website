7e1e9b26b3de0040c500bde18370ff9918ebe8c4
Xavier Watkins <xavier.watkins@gmail.com>
2021-06-22 12:00:00 +0100

    Start to display basket items

--- src/shared/components/basket/Basket.tsx
+++ src/shared/components/basket/Basket.tsx
@@ -1,15 +1,43 @@
 import { Tabs, Tab } from 'franklin-sites';
 
+import useBasket from '../../hooks/useBasket';
+import { Namespace } from '../../types/namespaces';
+
 import styles from './styles/basket.module.scss';
 
-const Basket = () => (
-  <div className={styles.basket}>
-    <Tabs>
-      <Tab title="UniProtKB">UniProtKB basket contents</Tab>
-      <Tab title="UniRef">UniRef basket contents</Tab>
-      <Tab title="UniParc">UniParc basket contents</Tab>
-    </Tabs>
-  </div>
-);
+const Basket = () => {
+  const [basket] = useBasket();
+
+  const uniprotkbIds = basket.get(Namespace.uniprotkb);
+  const unirefIds = basket.get(Namespace.uniref);
+  const uniparcIds = basket.get(Namespace.uniparc);
+  // TODO
+  // Create query for the accessions endpoint
+  // Wire in datatables
+  // Wire in buttons
+  return (
+    <div className={styles.basket}>
+      <Tabs>
+        {/* TODO: support "disabled" attribute in Tab */}
+        <Tab title="UniProtKB">
+          <ul>
+            {uniprotkbIds &&
+              Array.from(uniprotkbIds).map((id) => <li>{id}</li>)}
+          </ul>
+        </Tab>
+        <Tab title="UniRef">
+          <ul>
+            {unirefIds && Array.from(unirefIds).map((id) => <li>{id}</li>)}
+          </ul>
+        </Tab>
+        <Tab title="UniParc">
+          <ul>
+            {uniparcIds && Array.from(uniparcIds).map((id) => <li>{id}</li>)}
+          </ul>
+        </Tab>
+      </Tabs>
+    </div>
+  );
+};
 
 export default Basket;
