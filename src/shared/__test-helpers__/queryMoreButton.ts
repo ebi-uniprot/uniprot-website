fc76c22c639d94f75bd7d2b2e4861fd6fb804221
Daniel Rice <dlrice@ebi.ac.uk>
2021-07-08 17:09:51 +0100

    Remove unused test helper.

--- src/shared/__test-helpers__/queryMoreButton.ts
+++
@@ -1,7 +0,0 @@
-import { screen } from '@testing-library/react';
-
-const queryShowMore = () =>
-  screen.queryByTitle('Show more') ||
-  screen.queryByTestId('expandable-message');
-
-export default queryShowMore;
