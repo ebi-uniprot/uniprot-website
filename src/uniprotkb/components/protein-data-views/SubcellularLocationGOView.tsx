27583256c7cf8bf07cfb0a19aad4a604f33a3e68
Daniel Rice <dlrice@ebi.ac.uk>
2021-07-27 12:33:29 +0100

    Initial GO sub cell view.

---
+++ src/uniprotkb/components/protein-data-views/SubcellularLocationGOView.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import { ExternalLink } from 'franklin-sites';
+
+// import UniProtKBEvidenceTag from './UniProtKBEvidenceTag';
+
+import externalUrls from '../../../shared/config/externalUrls';
+import { GoXref } from '../../adapters/subcellularLocationConverter';
+
+const getSwissBioPicLocationId = (id: string) => id.replace(':', '');
+
+const SubcellularLocationGOView: FC<{
+  goXrefs: GoXref[];
+}> = ({ goXrefs }) => {
+  if (!goXrefs?.length) {
+    return null;
+  }
+  return (
+    <>
+      {goXrefs.map(({ id, properties }) => (
+        <div>
+          <ExternalLink
+            // id is used in the case that this component is used in conjunction
+            // with @swissprot/swissbiopics-visualizer
+            className={getSwissBioPicLocationId(id)}
+            key={id}
+            url={externalUrls.QuickGO(id)}
+          >
+            {properties.GoTerm}
+          </ExternalLink>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default SubcellularLocationGOView;
