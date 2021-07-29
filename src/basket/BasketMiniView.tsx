953f5c27ccbcf6fcde979db2fa49351c6cd8092a
Xavier Watkins <xavier.watkins@gmail.com>
2021-07-29 13:30:50 +0100

    Tidy imports

--- src/basket/BasketMiniView.tsx
+++ src/basket/BasketMiniView.tsx
@@ -1,5 +1,5 @@
-import { useMemo, useRef, useEffect, Dispatch, SetStateAction } from 'react';
-import { generatePath, Link, useLocation } from 'react-router-dom';
+import { useMemo, Dispatch, SetStateAction } from 'react';
+import { generatePath, Link } from 'react-router-dom';
 import cn from 'classnames';
 import { Tabs, Tab, BinIcon, Button, FullViewIcon } from 'franklin-sites';
 
--- src/tools/dashboard/components/Dashboard.tsx
+++ src/tools/dashboard/components/Dashboard.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
   Card,
   Button,
