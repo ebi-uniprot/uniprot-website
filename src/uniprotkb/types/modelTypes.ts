1a6807dd16ca4be6b8c93c1db3840464d3ad8fcb
Daniel Rice <dlrice@ebi.ac.uk>
2021-08-17 11:52:57 +0100

    Add GoEvidenceCodes to model types.

--- src/uniprotkb/types/modelTypes.ts
+++ src/uniprotkb/types/modelTypes.ts
@@ -43,7 +43,41 @@ export enum PropertyKey {
   Type = 'Type',
 }
 
-export type Property = {
-  key?: PropertyKey;
-  value?: string;
-};
+export type Property =
+  | {
+      key?: PropertyKey;
+      value?: string;
+    }
+  | {
+      key: PropertyKey.GoEvidenceType;
+      value: `${GoEvidenceCodes}:${string}`; // eg IDA:ARUK-UCL, TAS:Reactome
+    };
+
+// Source: https://github.com/ebi-uniprot/uniprot-core/blob/master/core-domain/src/main/java/org/uniprot/core/cv/go/GoEvidenceType.java
+export type GoEvidenceCodes =
+  | 'EXP' // Inferred from experiment
+  | 'HDA' // high throughput direct assay evidence used in manual assertion
+  | 'HEP' // high throughput expression pattern evidence used in manual assertion
+  | 'HGI' // high throughput genetic interaction evidence used in manual assertion
+  | 'HMP' // high throughput mutant phenotype evidence used in manual assertion
+  | 'HTP' // high throughput evidence used in manual assertion
+  | 'IBA' // inferred from Biological aspect of Ancestor
+  | 'IBD' // inferred from Biological aspect of Descendant
+  | 'IC' // inferred by curator
+  | 'IDA' // inferred from direct assay
+  | 'IEA' // inferred from electronic annotation
+  | 'IEP' // inferred from expression pattern
+  | 'IGC' // inferred from Genomic Context
+  | 'IGI' // inferred from genetic interaction
+  | 'IKR' // inferred from Key Residues
+  | 'IMP' // inferred from mutant phenotype
+  | 'IPI' // inferred from physical interaction
+  | 'IRD' // inferred from Rapid Divergence
+  | 'ISA' // inferred from Sequence Alignment
+  | 'ISM' // inferred from Sequence Model
+  | 'ISO' // inferred from Sequence Orthology
+  | 'ISS' // inferred from sequence or structural similarity
+  | 'NAS' // non-traceable author statement
+  | 'ND' // no biological data available
+  | 'RCA' // inferred from reviewed computational analysis
+  | 'TAS'; // traceable author statement
