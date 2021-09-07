b55d5cebfda9d0c3e2644eb679dd1324f2a754f7
Daniel Rice <dlrice@ebi.ac.uk>
2021-09-07 11:43:31 +0100

    Refactor loading logic in help autocomplete.

--- src/help/components/landing/HelpAutocomplete.tsx
+++ src/help/components/landing/HelpAutocomplete.tsx
@@ -1,32 +1,70 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
+import { generatePath, Link, useHistory, useLocation } from 'react-router-dom';
 import { Card, InfoList, SearchInput } from 'franklin-sites';
-
-import useDataApi from '../../../shared/hooks/useDataApi';
+import qs from 'query-string';
+import { debounce } from 'lodash-es';
 
 import CleanHighlightMarkDown from '../../utils/CleanHighlightMarkDown';
 
+import useDataApiWithStale from '../../../shared/hooks/useDataApiWithStale';
+
 import { help as helpURL } from '../../../shared/config/apiUrls';
+import { LocationToPath, Location } from '../../../app/config/urls';
 
 import { HelpSearchResponse } from '../../adapters/helpConverter';
 
 import styles from './styles/help-autocomplete.module.scss';
 
 const HelpAutocomplete = () => {
-  const [searchValue, setSearchValue] = useState<string>('');
-  const dataObject = useDataApi<HelpSearchResponse>(
-    searchValue && helpURL.search({ query: searchValue })
+  const location = useLocation();
+  const history = useHistory();
+
+  const [searchValue, setSearchValue] = useState<string>(() => {
+    const { query } = qs.parse(location.search);
+    const searchValue = Array.isArray(query) ? query.join(' ') : query;
+    return searchValue || '';
+  });
+
+  const parsed = location.search && qs.parse(location.search);
+  const dataObject = useDataApiWithStale<HelpSearchResponse>(
+    parsed && helpURL.search(parsed)
   );
 
-  const articles = dataObject?.data?.results.map((article) => {
-    const titleMatch = article.matches?.title?.[0];
-    const contentMatch = article.matches?.content?.[0];
+  const replaceLocation = useMemo(
+    () =>
+      debounce((searchValue: string) => {
+        history.replace({
+          pathname: LocationToPath[Location.HelpLanding],
+          search: qs.stringify({
+            ...qs.parse(history.location.search),
+            query: searchValue || undefined,
+          }),
+        });
+      }, 500),
+    [history]
+  );
+
+  useEffect(() => {
+    replaceLocation(searchValue);
+
+    return replaceLocation.cancel;
+  }, [replaceLocation, searchValue]);
+
+  const articles = dataObject?.data?.results.map(({ matches, title, id }) => {
+    const titleMatch = matches?.title?.[0];
+    const contentMatch = matches?.content?.[0];
+    const to = generatePath(LocationToPath[Location.HelpEntry], {
+      accession: id,
+    });
     return {
-      title: titleMatch ? (
-        <CleanHighlightMarkDown md={titleMatch} />
-      ) : (
-        article.title
+      title: (
+        <Link to={to}>
+          {' '}
+          {titleMatch ? <CleanHighlightMarkDown md={titleMatch} /> : title}
+        </Link>
       ),
       content: contentMatch && <CleanHighlightMarkDown md={contentMatch} />,
+      to,
     };
   });
 
@@ -41,7 +79,7 @@ const HelpAutocomplete = () => {
         value={searchValue}
         autoFocus
       />
-      {!!articles?.length && (
+      {!!articles?.length && searchValue && (
         <Card>
           <InfoList infoData={articles} />
         </Card>
