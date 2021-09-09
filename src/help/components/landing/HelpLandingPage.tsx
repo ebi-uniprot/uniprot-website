faa936a9fd950261cb8be930acf3f7cdde754cd8
Daniel Rice <dlrice@ebi.ac.uk>
2021-09-09 16:40:09 +0100

    Initial layout.

--- src/help/components/landing/HelpLandingPage.tsx
+++ src/help/components/landing/HelpLandingPage.tsx
@@ -1,9 +1,24 @@
 import HelpQuickSearch from './HelpQuickSearch';
 
 const HelpLandingPage = () => (
-  <div className="uniprot-grid--centered">
-    <h1>Help center</h1>
-    <HelpQuickSearch />
+  <div className="uniprot-grid uniprot-grid--centered">
+    <h2 className="uniprot-grid-cell--span-12">Help center</h2>
+    <div className="uniprot-grid-cell--span-9">
+      <HelpQuickSearch />
+    </div>
+    <div className="uniprot-grid-cell--span-3">
+      <h4>Suggest FAQ</h4>
+      <ul className="no-bullet">
+        <li>How can I get all the proteins involved in a given disease?</li>
+        <li>What is the canonical sequence? </li>
+        <li>What are reference proteomes?</li>
+      </ul>
+      <p>contact icons</p>
+    </div>
+    <div className="uniprot-grid-cell--span-9">Tiles</div>
+    <div className="uniprot-grid-cell--span-3">
+      <h4>Help videos</h4>
+    </div>
   </div>
 );
 
