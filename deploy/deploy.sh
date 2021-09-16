5854b6a633732c5c6d19d559390c5f16bca451e0
Daniel Rice <dlrice@ebi.ac.uk>
2021-09-16 16:32:41 +0100

    Check if cluster is there before proceeding.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -16,6 +16,9 @@ K8S_USER_TOKEN=${DC}_K8S_USER_TOKEN
 mkdir -p ~/.kube
 sed "s~__K8S_CERTIFICATE__~${!K8S_CERTIFICATE}~; s~__K8S_URL__~${!K8S_URL}~; s~__K8S_USER_TOKEN__~${!K8S_USER_TOKEN}~" deploy/wp-config-template.yml > ~/.kube/config
 chmod 700 ~/.kube/config
+
+kubectl cluster-info || { echo 'Cannot reach cluster' ; exit 1; } 
+
 kubectl config use-context team-admin-wp-webadmin-02
 kubectl config set-context --current --namespace=uniprot-front-end
 printf "$(kubectl create secret docker-registry gitlab-registry --docker-server=$CI_REGISTRY --docker-username=$CI_DEPLOY_USER --docker-password=$CI_DEPLOY_PASSWORD --docker-email=$GITLAB_USER_EMAIL -o yaml --dry-run=client)" | kubectl apply -f -
