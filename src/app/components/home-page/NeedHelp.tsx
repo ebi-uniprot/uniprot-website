835a4c7353324a2c02f8e9a1520f7470f3952d0d
Aurélien Luciani <aurel-l@users.noreply.github.com>
2021-12-03 17:24:31 +0000

    use YouTubeEmbed on help page too

--- src/app/components/home-page/NeedHelp.tsx
+++ src/app/components/home-page/NeedHelp.tsx
@@ -11,9 +11,10 @@ import {
 } from 'franklin-sites';
 import cn from 'classnames';
 
+import YouTubeEmbed from '../../../shared/components/YouTubeEmbed';
+
 import useDataApi from '../../../shared/hooks/useDataApi';
 import useStructuredData from '../../../shared/hooks/useStructuredData';
-import useCustomElement from '../../../shared/hooks/useCustomElement';
 
 import parseDate from '../../../shared/utils/parseDate';
 import cleanText, {
@@ -87,40 +88,6 @@ const fallback: PayloadEBISearch['entries'][0] = {
       value: 'https://www.ebi.ac.uk/training/online/courses/uniprot-quick-tour',
     },
   ],
-};
-
-const YouTubeEmbed = ({ id, title }: { id: string; title: string }) => {
-  const liteYouTube = useCustomElement(
-    () =>
-      import(/* webpackChunkName: "lite-youtube-embed" */ 'lite-youtube-embed'),
-    'lite-youtube'
-  );
-
-  /* NOTE: privacy issue? When clicking, we embed YouTube in the website,
-  with all the related tracking, might need to link to YouTube instead */
-  return (
-    <liteYouTube.name
-      videoid={id}
-      playlabel={title}
-      style={{
-        backgroundImage: `url('https://i.ytimg.com/vi/${id}/hqdefault.jpg')`,
-      }}
-    >
-      {liteYouTube.defined ? (
-        <button className="lty-playbtn" type="button">
-          <span className="visually-hidden">{title}</span>
-        </button>
-      ) : (
-        <ExternalLink
-          className="lty-playbtn"
-          url={`https://www.youtube.com/watch?v=${id}`}
-          noIcon
-        >
-          <span className="visually-hidden">{title}</span>
-        </ExternalLink>
-      )}
-    </liteYouTube.name>
-  );
 };
 
 const NeedHelp = () => {
--- src/help/components/landing/HelpLandingPage.tsx
+++ src/help/components/landing/HelpLandingPage.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import { CommunityAnnotationIcon } from 'franklin-sites';
 import cn from 'classnames';
-import 'lite-youtube-embed';
 
 import HelpQuickSearch from './HelpQuickSearch';
 import Contact from '../../../shared/components/layouts/Contact';
@@ -26,6 +25,7 @@ import Terminal from './svgs/terminal.svg';
 import Reader from './svgs/reader.svg';
 
 import landing from './styles/help-landing-page.module.scss';
+import YouTubeEmbed from '../../../shared/components/YouTubeEmbed';
 
 const getHelpEntryPath = getLocationEntryPathFor(Location.HelpEntry);
 
@@ -206,10 +206,7 @@ const HelpLandingPage = () => (
       <section className={landing['help-landing__videos']}>
         <h4>Help videos</h4>
         <div className={landing['help-landing__videos__list']}>
-          <lite-youtube
-            videoid="yp1O1gDK8oA"
-            playlabel="How to search UniProtKB"
-          />
+          <YouTubeEmbed id="yp1O1gDK8oA" title="How to search UniProtKB" />
           {/* TODO: include more videos as they become available */}
         </div>
       </section>
