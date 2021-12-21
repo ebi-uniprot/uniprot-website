ccf6c5aac60837010f74016c672a1168e56f192d
Daniel Rice <dlrice@ebi.ac.uk>
2021-12-21 19:22:56 +0000

    Put EC number in catalytic activity view on own line. Use fullstop at end of direction sentence.

--- src/uniprotkb/components/protein-data-views/CatalyticActivityView.tsx
+++ src/uniprotkb/components/protein-data-views/CatalyticActivityView.tsx
@@ -202,11 +202,11 @@ export const ReactionDirection = ({
             <span data-testid="direction-text">
               {physiologicalReactionDirectionToString.get(directionType)}
             </span>
-            {physiologicalReactions.length === 1 && ' direction '}
+            {physiologicalReactions.length === 1 && ' direction.'}
             {!noEvidence && <UniProtKBEvidenceTag evidences={evidences} />}
             {physiologicalReactions.length === 2 &&
               index === 1 &&
-              ' directions '}
+              ' directions.'}
           </Fragment>
         ))}
     </div>
@@ -272,7 +272,9 @@ const CatalyticActivityView = ({
             )}
 
             {reaction.ecNumber && (
-              <ECNumbersView ecNumbers={[{ value: reaction.ecNumber }]} />
+              <div>
+                <ECNumbersView ecNumbers={[{ value: reaction.ecNumber }]} />
+              </div>
             )}
             {!!rheaId && (
               <>
