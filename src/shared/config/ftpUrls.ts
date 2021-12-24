6db35bf73f0de9d53898a6e7d3f463aef4cde327
Daniel Rice <dlrice@ebi.ac.uk>
2021-12-24 16:54:52 +0000

    Create and use uniprot ftp directory in ftpUrls.

--- src/shared/config/ftpUrls.ts
+++ src/shared/config/ftpUrls.ts
@@ -1,20 +1,21 @@
 import { capitalize } from 'lodash-es';
 import joinUrl from 'url-join';
 
-const ftpPrefix = 'https://ftp.uniprot.org';
+const ftpUniProt = 'https://ftp.uniprot.org/pub/databases/uniprot/';
 
 const ftpUrls = {
+  uniprot: ftpUniProt,
   referenceProteomes: (id: string, superkingdom: string, taxonId: number) =>
     joinUrl(
-      ftpPrefix,
-      `/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/${capitalize(
+      ftpUniProt,
+      `/current_release/knowledgebase/reference_proteomes/${capitalize(
         superkingdom
       )}/${id}/${id}_${taxonId}.fasta.gz`
     ),
   panProteomes: (id: string) =>
     joinUrl(
-      ftpPrefix,
-      `/pub/databases/uniprot/current_release/knowledgebase/pan_proteomes/${id}.fasta.gz`
+      ftpUniProt,
+      `/current_release/knowledgebase/pan_proteomes/${id}.fasta.gz`
     ),
 };
 
