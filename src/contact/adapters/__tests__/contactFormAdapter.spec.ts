8857528c24dea9cc04a7fe0a57e966d62da3fd17
Xavier Watkins <xavier.watkins@gmail.com>
2022-01-10 17:19:28 +0000

    Mock navigator.userAgent

--- src/contact/adapters/__tests__/contactFormAdapter.spec.ts
+++ src/contact/adapters/__tests__/contactFormAdapter.spec.ts
@@ -1,7 +1,14 @@
 import { generateForm } from '../contactFormAdapter';
 
+let userAgentGetter: jest.SpyInstance;
+
 describe('Test contact form adapter', () => {
+  beforeEach(() => {
+    userAgentGetter = jest.spyOn(window.navigator, 'userAgent', 'get');
+  });
+
   test('it should parse the inputs and generate a generic form', () => {
+    userAgentGetter.mockReturnValue('Browser and OS info');
     const form = generateForm(
       {
         email: 'pat@pencaster.co.uk',
