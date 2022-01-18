9ea16b2cf7491b89b1313a8322a9891a7633aae9
Xavier Watkins <xavier.watkins@gmail.com>
2022-01-18 17:55:52 +0000

    Only show on relevant pages

--- src/help/components/contextual/ContextualHelpHandler.tsx
+++ src/help/components/contextual/ContextualHelpHandler.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import { Location, LocationToPath } from '../../../app/config/urls';
 
 import './styles/contextual-help.scss';
 
@@ -10,12 +12,22 @@ const eventHandler = (event: MouseEvent) => {
 };
 
 const ContextualHelpHandler = () => {
+  const isHelpResults = useRouteMatch(LocationToPath[Location.HelpResults]);
+  const isHelpEntry = useRouteMatch(LocationToPath[Location.HelpEntry]);
+  const isContact = useRouteMatch(LocationToPath[Location.Contact]);
+
+  const shouldBeVisible = !isHelpResults && !isHelpEntry && !isContact;
+
   useEffect(() => {
     document.addEventListener('click', eventHandler);
     return () => {
       document.removeEventListener('click', eventHandler);
     };
   }, []);
+
+  if (!shouldBeVisible) {
+    return null;
+  }
 
   // TODO: return button and panel
   return null;
