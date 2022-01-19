b619ee016f0b7290c4bd4af70830ba873505dfa5
Xavier Watkins <xavier.watkins@gmail.com>
2022-01-19 14:13:29 +0000

    Use component functionality

--- src/help/components/contextual/ContextualHelpContainer.tsx
+++ src/help/components/contextual/ContextualHelpContainer.tsx
@@ -1,15 +1,13 @@
 import { Loader, SlidingPanel } from 'franklin-sites';
 import ErrorHandler from '../../../shared/components/error-pages/ErrorHandler';
 import { help as helpUrl } from '../../../shared/config/apiUrls';
-import useDataApiWithStale from '../../../shared/hooks/useDataApiWithStale';
+import useDataApi from '../../../shared/hooks/useDataApi';
 import { HelpEntryResponse } from '../../adapters/helpConverter';
 import { HelpEntryContent } from '../entry/Entry';
 
-import styles from './styles/contextual-help.module.scss';
-
 const ContextualHelpContainer = ({ articleId }: { articleId: string }) => {
   const { data, loading, error, status, progress } =
-    useDataApiWithStale<HelpEntryResponse>(helpUrl.accession(articleId));
+    useDataApi<HelpEntryResponse>(helpUrl.accession(articleId));
 
   if (loading && !data) {
     return <Loader progress={progress} />;
@@ -21,21 +19,21 @@ const ContextualHelpContainer = ({ articleId }: { articleId: string }) => {
 
   return (
     <SlidingPanel
+      title="Help"
       onClose={() => {
         console.log('do something');
       }}
-      className={styles['contextual-help-panel']}
+      className="contextual-help-panel"
       position="right"
     >
-      <h1 className="big">Help</h1>
-      <div className={styles['contextual-help-content']}>
+      <>
         <h2 className="medium">{data.title}</h2>
         <HelpEntryContent
           data={data}
           handleClick={() => console.log('click!')}
           upperHeadingLevel="h3"
         />
-      </div>
+      </>
     </SlidingPanel>
   );
 };
