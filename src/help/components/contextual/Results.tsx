3963b455736560ad2de489689f1864e3f1c72af3
Aurélien Luciani <aurel-l@users.noreply.github.com>
2022-01-27 16:46:43 +0000

    change search behaviour when no result or when loading

--- src/help/components/contextual/Results.tsx
+++ src/help/components/contextual/Results.tsx
@@ -1,7 +1,9 @@
 import { Link, RouteChildrenProps } from 'react-router-dom';
-import { InfoList } from 'franklin-sites';
+import { InfoList, Loader } from 'franklin-sites';
 import qs from 'query-string';
 
+import ErrorHandler from '../../../shared/components/error-pages/ErrorHandler';
+import NoResultsPage from '../../../shared/components/error-pages/NoResultsPage';
 import CleanHighlightMarkDown from '../results/CleanHighlightMarkDown';
 
 import useDataApiWithStale from '../../../shared/hooks/useDataApiWithStale';
@@ -13,12 +15,10 @@ import { Location, getLocationEntryPath } from '../../../app/config/urls';
 
 const Results = ({ location }: RouteChildrenProps) => {
   const { query } = qs.parse(location.search);
-  const dataObject = useDataApiWithStale<HelpSearchResponse>(
-    query && helpURL.search({ query })
-  );
+  const { data, loading, error, status, progress } =
+    useDataApiWithStale<HelpSearchResponse>(query && helpURL.search({ query }));
 
-  const allArticles = dataObject?.data?.results;
-  const infoData = allArticles?.map(({ matches, title, id }) => {
+  const infoData = data?.results?.map(({ matches, title, id }) => {
     const titleMatch = matches?.title?.[0];
     const contentMatch = matches?.content?.[0];
     const to = getLocationEntryPath(Location.HelpEntry, id);
@@ -37,11 +37,19 @@ const Results = ({ location }: RouteChildrenProps) => {
     };
   });
 
-  if (!!allArticles?.length && !!infoData?.length && query) {
+  if (error || (!loading && !data)) {
+    return <ErrorHandler status={status} />;
+  }
+
+  if (!data) {
+    return <Loader progress={progress} />;
+  }
+
+  if (infoData?.length && query) {
     return <InfoList infoData={infoData} />;
   }
 
-  return null;
+  return <NoResultsPage />;
 };
 
 export default Results;
