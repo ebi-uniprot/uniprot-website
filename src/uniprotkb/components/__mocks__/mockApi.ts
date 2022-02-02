dfd3f065602034016d38880712153d87766c3754
Aurélien Luciani <aurel-l@users.noreply.github.com>
2022-02-02 15:13:17 +0000

    fix expected content types in order to fix preview

--- src/uniprotkb/components/__mocks__/mockApi.ts
+++ src/uniprotkb/components/__mocks__/mockApi.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
-import results from './results';
-import noResults from './noResults.json';
-import entry from './swissprotEntry';
+import { ContentType } from '../../../shared/types/resultsDownload';
+
 import resultFields from '../../__mocks__/resultFields';
+import results from './results';
+import entry from './swissprotEntry';
+
+import noResults from './noResults.json';
 import mockFasta from './fasta.json';
 
 const mock = new MockAdapter(axios);
@@ -12,13 +15,13 @@ mock
   .onGet(/.+noresult/)
   .reply(200, noResults, { 'x-total-records': 0 })
   .onGet(/\/uniprotkb\/search.*&format=fasta.*/)
-  .reply(200, mockFasta, { 'content-type': 'text/fasta' })
+  .reply(200, mockFasta, { 'content-type': ContentType.fasta })
   .onGet(/\/uniprotkb\/search/)
   .reply(200, results, { 'x-total-records': 25 })
   .onGet(/\/uniprotkb\/result-fields/)
   .reply(200, resultFields)
   .onGet(/\/uniprotkb\/stream/)
-  .reply(200, mockFasta, { 'content-type': 'text/fasta' })
+  .reply(200, mockFasta, { 'content-type': ContentType.fasta })
   .onGet(/\/uniprotkb\//)
   .reply(200, entry);
 
