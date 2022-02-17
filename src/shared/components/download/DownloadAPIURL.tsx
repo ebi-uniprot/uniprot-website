a5d6277796494cf862a7241dfc685249b6771e61
Daniel Rice <dlrice@ebi.ac.uk>
2022-02-17 13:49:48 +0000

    Create copySuccessMessage and copyFailureMessage actions.

--- src/shared/components/download/DownloadAPIURL.tsx
+++ src/shared/components/download/DownloadAPIURL.tsx
@@ -1,11 +1,10 @@
 import { Button, CodeBlock, CopyIcon } from 'franklin-sites';
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { addMessage } from '../../../messages/state/messagesActions';
 import {
-  MessageFormat,
-  MessageLevel,
-} from '../../../messages/types/messagesTypes';
+  copyFailureMessage,
+  copySuccessMessage,
+} from '../../../messages/state/messagesActions';
 
 import style from './styles/download-api-url.module.scss';
 
@@ -15,26 +14,10 @@ const DownloadAPIURL = ({ apiURL }: { apiURL: string }) => {
     try {
       await navigator.clipboard.writeText(apiURL);
       // Success with Clipboard API, display message
-      dispatch(
-        addMessage({
-          id: 'copy link api',
-          content: `Link copied to clipboard`,
-          format: MessageFormat.POP_UP,
-          level: MessageLevel.SUCCESS,
-          displayTime: 5_000,
-        })
-      );
+      dispatch(copySuccessMessage());
     } catch {
       // Issue with Clipboard API too, bail with error message
-      dispatch(
-        addMessage({
-          id: 'copy link api',
-          content: `There was an issue while copying to clipboard`,
-          format: MessageFormat.POP_UP,
-          level: MessageLevel.FAILURE,
-          displayTime: 15_000,
-        })
-      );
+      dispatch(copyFailureMessage());
     }
   }, [apiURL, dispatch]);
 
