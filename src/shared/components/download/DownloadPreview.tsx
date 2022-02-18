7d7ecbb328cf0619ca255ce20f23f3c74ee15384
Aurélien Luciani <aurel-l@users.noreply.github.com>
2022-02-18 08:37:05 +0000

    remove extra logic

--- src/shared/components/download/DownloadPreview.tsx
+++ src/shared/components/download/DownloadPreview.tsx
@@ -5,9 +5,8 @@ import { JsonObject } from 'type-fest';
 import useDataApi from '../../hooks/useDataApi';
 
 import { fileFormatToContentType } from '../../config/resultsDownload';
-import urlsAreEqual from '../../utils/url';
 
-import { ContentType, FileFormat } from '../../types/resultsDownload';
+import { FileFormat } from '../../types/resultsDownload';
 
 import styles from './styles/download-preview.module.scss';
 
@@ -27,18 +26,10 @@ const DownloadPreview = ({
     return { headers };
   }, [previewFileFormat]);
 
-  const { data, headers, url, loading } = useDataApi<JsonObject | string>(
+  const { data, loading } = useDataApi<JsonObject | string>(
     previewUrl,
     options
   );
-
-  const previewContent =
-    urlsAreEqual(url || '', previewUrl, ['compressed']) &&
-    data &&
-    (headers?.['content-type'] as ContentType) ===
-      fileFormatToContentType[previewFileFormat]
-      ? data
-      : '';
 
   if (loading) {
     return <Loader />;
@@ -48,9 +39,7 @@ const DownloadPreview = ({
     <div className={styles.preview}>
       <h4>Preview</h4>
       <CodeBlock lightMode data-testid="download-preview">
-        {typeof data === 'string'
-          ? data
-          : JSON.stringify(previewContent, null, 2)}
+        {typeof data === 'string' ? data : JSON.stringify(data, null, 2)}
       </CodeBlock>
     </div>
   );
