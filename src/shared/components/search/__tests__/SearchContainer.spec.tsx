f6913444dc4cf21705303e4e25679a730d86ef60
Daniel Rice <dlrice@ebi.ac.uk>
2022-03-03 14:16:10 +0000

    Test align results specific behavior.

--- src/shared/components/search/__tests__/SearchContainer.spec.tsx
+++ src/shared/components/search/__tests__/SearchContainer.spec.tsx
@@ -6,7 +6,7 @@ import SearchContainer from '../SearchContainer';
 
 import { Namespace } from '../../../types/namespaces';
 
-describe('Search shallow components on home page', () => {
+describe('Search component on home page', () => {
   let rendered: ReturnType<typeof customRender>;
   beforeEach(() => {
     rendered = customRender(
@@ -30,8 +30,8 @@ describe('Search shallow components on home page', () => {
   });
 });
 
-describe('Search shallow components on results page', () => {
-  const getSearchQueryWithRoute = (route: string) => {
+describe('Search component on results page', () => {
+  const renderWithRoute = (route: string) => {
     customRender(
       <SearchContainer
         isOnHomePage={false}
@@ -40,16 +40,29 @@ describe('Search shallow components on results page', () => {
       />,
       { route }
     );
-    return screen.getByLabelText('Text query in uniprotkb');
   };
   it('should display the job id when on a tools results page', () => {
-    const input = getSearchQueryWithRoute('/id-mapping/uniparc/job123');
-    expect(input).toHaveValue('job:job123');
+    renderWithRoute('/id-mapping/uniparc/job123');
+    expect(screen.getByLabelText('Text query in uniprotkb')).toHaveValue(
+      'job:job123'
+    );
   });
   it('should display the job id and query when on a tools results page', () => {
-    const input = getSearchQueryWithRoute(
-      '/id-mapping/uniparc/job123?query=foo'
+    renderWithRoute('/id-mapping/uniparc/job123?query=foo');
+    expect(screen.getByLabelText('Text query in uniprotkb')).toHaveValue(
+      'job:job123 AND foo'
     );
-    expect(input).toHaveValue('job:job123 AND foo');
+    expect(
+      screen.getByRole('button', { name: 'Advanced' })
+    ).toBeInTheDocument();
+  });
+  it('should only display the job id when on an align tool results page', () => {
+    renderWithRoute('/align/job123?query=foo');
+    expect(screen.getByLabelText('Text query in uniprotkb')).toHaveValue(
+      'job:job123'
+    );
+    expect(
+      screen.queryByRole('button', { name: 'Advanced' })
+    ).not.toBeInTheDocument();
   });
 });
