a12c385774d5a206e2edfad56b8bc0241461b468
Daniel Rice <dlrice@ebi.ac.uk>
2022-03-16 12:03:28 +0000

    Create parseQueryString wrapper around query-string's parse.

--- src/shared/utils/url.ts
+++ src/shared/utils/url.ts
@@ -1,4 +1,5 @@
 import { matchPath } from 'react-router-dom';
+import queryString from 'query-string';
 
 import { LocationToPath } from '../../app/config/urls';
 
@@ -7,4 +8,23 @@ export const getLocationForPathname = (pathname: string) => {
     matchPath(pathname, { path, exact: path === '/' })
   );
   return found?.[0];
+};
+
+/**
+Wrapper around query-string's parse function which takes the output of this and
+returns only the first value of a parameter if it is an array.
+
+@param query - The query string to parse.
+@param options - Options to pass query-string's parse 
+*/
+export const parseQueryString = (
+  query: string,
+  options?: { parseNumbers?: true } & queryString.ParseOptions
+) => {
+  const parsed = queryString.parse(query, options);
+  const parsedWithoutArrayValues: { [key: string]: string | null } = {};
+  for (const [key, value] of Object.entries(parsed)) {
+    parsedWithoutArrayValues[key] = Array.isArray(value) ? value[0] : value;
+  }
+  return parsedWithoutArrayValues;
 };
