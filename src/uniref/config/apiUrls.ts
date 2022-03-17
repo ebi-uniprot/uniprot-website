55aa87928203cfe7aa1188fbb20fa6c8b903ab0c
Daniel Rice <dlrice@ebi.ac.uk>
2022-03-17 09:57:12 +0000

    Update members api url to join facet filters with AND, add file format.

--- src/uniref/config/apiUrls.ts
+++ src/uniref/config/apiUrls.ts
@@ -2,6 +2,8 @@ import qs from 'query-string';
 import joinUrl from '../../shared/config/testingApiUrls'; // TODO: revert import to: import joinUrl from 'url-join'
 
 import { apiPrefix } from '../../shared/config/apiUrls';
+import { FileFormat } from '../../shared/types/resultsDownload';
+import { fileFormatToUrlParameter } from '../../shared/config/resultsDownload';
 
 const apiUrls = {
   members: (
@@ -10,18 +12,20 @@ const apiUrls = {
       facets?: Readonly<string[]>;
       selectedFacets?: string[];
       size?: number;
+      format?: FileFormat.json | FileFormat.list; // TODO: add when supported: | FileFormat.tsv | FileFormat.xml
     } = {}
-  ) => {
-    const querystring = qs.stringify({
-      size: options.size,
-      facets: options.facets?.join(',') || undefined,
-      facetFilter: options.selectedFacets?.join(',') || undefined,
-    });
-    return (
-      joinUrl(apiPrefix, 'uniref', id, 'members') +
-      (querystring ? `?${querystring}` : '')
-    );
-  },
+  ) =>
+    qs.stringifyUrl({
+      url: joinUrl(apiPrefix, 'uniref', id, 'members'),
+      query: {
+        size: options.size,
+        facets: options.facets?.join(',') || undefined,
+        facetFilter: options.selectedFacets?.join(' AND ') || undefined,
+        format: options.format
+          ? fileFormatToUrlParameter[options.format]
+          : undefined,
+      },
+    }),
 };
 
 export default apiUrls;
