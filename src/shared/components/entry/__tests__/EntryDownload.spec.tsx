971e09cdc86838255551746e9c1a2d7c8e4e40ee
Daniel Rice <dlrice@ebi.ac.uk>
2022-03-17 10:20:40 +0000

    Test for presence of uniref list download.

--- src/shared/components/entry/__tests__/EntryDownload.spec.tsx
+++ src/shared/components/entry/__tests__/EntryDownload.spec.tsx
@@ -16,4 +16,17 @@ describe('EntryDownload', () => {
       expect.stringContaining('/uniparc/UPI0000000001/databases?format=tsv')
     );
   });
+  it('should link to uniref list download endpoint', () => {
+    customRender(<EntryDownload />, {
+      route: '/uniref/UniRef100_A0A009E088',
+    });
+    const dropdownButton = screen.getByText('Download');
+    fireEvent.click(dropdownButton);
+    const tsv = screen.getByRole<HTMLAnchorElement>('link', { name: 'List' });
+    expect(tsv.href).toEqual(
+      expect.stringContaining(
+        '/uniref/UniRef100_A0A009E088/members?format=list'
+      )
+    );
+  });
 });
