39cce02bd873ccc5dbbc0a3ea3b41f50a06c13df
Daniel Rice <dlrice@ebi.ac.uk>
2022-03-23 11:30:02 +0000

    Use axios.interceptors to handle parsing of JSON.

--- src/shared/utils/fetchData.ts
+++ src/shared/utils/fetchData.ts
@@ -1,4 +1,19 @@
 import axios, { AxiosPromise, AxiosRequestConfig, CancelToken } from 'axios';
+import { keysToLowerCase } from './utils';
+
+axios.interceptors.response.use((response) => {
+  if (
+    response.data &&
+    response.status >= 200 &&
+    response.status < 300 &&
+    typeof response.data === 'string' &&
+    // Need to lowercase headers as axios doesn't do this before the interceptor.
+    keysToLowerCase(response?.headers)['content-type']?.includes('json')
+  ) {
+    response.data = JSON.parse(response.data);
+  }
+  return response;
+});
 
 export default function fetchData<T>(
   url: string,
@@ -16,9 +31,9 @@ export default function fetchData<T>(
       ...headers,
     },
     cancelToken,
-    transitional: {
-      silentJSONParsing: false,
-    },
+    // transitional: {
+    //   silentJSONParsing: false,
+    // },
     ...axiosOptions,
   });
 }
