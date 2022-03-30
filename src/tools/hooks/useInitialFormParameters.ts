d82a97caa23d8638c3a2e6993fa74f199c6d4120
Daniel Rice <dlrice@ebi.ac.uk>
2022-03-30 10:36:07 +0100

    Try to get params from url then state then default.

--- src/tools/hooks/useInitialFormParameters.ts
+++ src/tools/hooks/useInitialFormParameters.ts
@@ -1,6 +1,5 @@
 import { useEffect, useMemo } from 'react';
 import { useHistory } from 'react-router-dom';
-import { cloneDeep } from 'lodash-es';
 
 import { sequenceProcessor } from 'franklin-sites';
 import useDataApi from '../../shared/hooks/useDataApi';
@@ -56,42 +55,43 @@ function useInitialFormParameters<
   }>(url);
 
   // Discard 'search' part of url to avoid url state issues.
-  useEffect(() => {
-    if (history.location?.search) {
-      // eslint-disable-next-line uniprot-website/use-config-location
-      history.replace({
-        pathname: history.location.pathname,
-      });
-    }
-  }, [history]);
+  // useEffect(() => {
+  //   if (history.location?.search) {
+  //     // eslint-disable-next-line uniprot-website/use-config-location
+  //     history.replace({
+  //       pathname: history.location.pathname,
+  //     });
+  //   }
+  // }, [history]);
 
   const initialFormValues = useMemo(() => {
     if (accessionsLoading) {
       return null;
     }
 
-    // NOTE: we should use a similar logic to pre-fill fields based on querystring
     const parametersFromHistoryState = (
       history.location?.state as CustomLocationState<FormParameters>
     )?.parameters;
 
-    // Might want to use structuredClone eventually, but it's really new and we
-    // need to make sure that it's polyfilled by core-js first.
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/structuredClone
-    const formValues: FormValues<Fields> = cloneDeep(defaultFormValues);
-    // Parameters from state
-    if (parametersFromHistoryState) {
+    const parametersFromSearch = new URLSearchParams(history.location?.search);
+    // This will eventually be filled in
+    const formValues: Partial<FormValues<Fields>> = {};
+    if (parametersFromHistoryState || parametersFromSearch) {
       const defaultValuesEntries = Object.entries<FormValue>(defaultFormValues);
-      // for every field of the form, get its value from the history state if
-      // present, otherwise leave as the default value
       for (const [key, field] of defaultValuesEntries) {
         const fieldName = field.fieldName as keyof FormParameters;
-        if (fieldName in parametersFromHistoryState) {
-          formValues[key as Fields] = Object.freeze({
-            ...field,
-            selected: parametersFromHistoryState[fieldName],
-          } as FormValue);
-        }
+        formValues[key as Fields] = Object.freeze({
+          ...field,
+          selected:
+            // url params
+            (parametersFromSearch &&
+              parametersFromSearch.get(fieldName.toString())) ||
+            // history state
+            (parametersFromHistoryState &&
+              parametersFromHistoryState[fieldName]) ||
+            // default
+            field.selected,
+        } as FormValue);
       }
     }
 
@@ -132,10 +132,11 @@ function useInitialFormParameters<
       });
     }
 
-    return Object.freeze(formValues);
+    return Object.freeze(formValues as FormValues<Fields>);
   }, [
     accessionsLoading,
     history.location?.state,
+    history.location?.search,
     defaultFormValues,
     accessionsData?.results,
     idsMaybeWithRange,
