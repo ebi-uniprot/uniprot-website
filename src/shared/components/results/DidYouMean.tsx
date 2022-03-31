4a7d109c08fc22090bf381736e883f5f0ac80b5a
Daniel Rice <dlrice@ebi.ac.uk>
2022-03-31 12:59:59 +0100

    Extract and use SuggestionsSentence function.

--- src/shared/components/results/DidYouMean.tsx
+++ src/shared/components/results/DidYouMean.tsx
@@ -12,32 +12,46 @@ import {
 // TODO: delete line and file if not needed
 // import styles from './styles/did-you-mean.module.scss';
 
-type Props = {
+type SuggestionSentenceProps = {
+  suggestions: Suggestion[];
+  namespace: SearchableNamespace;
+};
+
+const SuggestionsSentence = ({
+  suggestions,
+  namespace,
+}: SuggestionSentenceProps) => (
+  <>
+    {suggestions.map(({ query }, i, a) => [
+      (i > 0 && i < a.length - 1 && ', ') || (i === a.length - 1 && ' or '),
+      <a href="#" key={query}>
+        {query}
+      </a>,
+    ])}
+    {` in ${searchableNamespaceLabels[namespace]}?`}
+  </>
+);
+
+type DidYouMeanProps = {
   suggestions: Suggestion[];
 };
 
-const DidYouMean = ({ suggestions }: Props) => {
+const DidYouMean = ({ suggestions }: DidYouMeanProps) => {
   const namespace = useNS();
   if (!namespace) {
     return null;
   }
-  // TODO: fix type
-  const namespaceLabel =
-    searchableNamespaceLabels[namespace as SearchableNamespace];
+
   const suggestionsSortedByHits = orderBy(suggestions, ['hits'], ['desc']);
   return (
     <Message level="info">
       Did you mean to search for:
       <ul>
         <li>
-          {suggestionsSortedByHits.map(({ query }, i, a) => [
-            (i > 0 && i < a.length - 1 && ', ') ||
-              (i === a.length - 1 && ' or '),
-            <a href="#" key={query}>
-              {query}
-            </a>,
-          ])}
-          {` in ${namespaceLabel}?`}
+          <SuggestionsSentence
+            suggestions={suggestionsSortedByHits}
+            namespace={namespace as SearchableNamespace}
+          />
         </li>
       </ul>
     </Message>
