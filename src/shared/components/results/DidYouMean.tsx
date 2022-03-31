68df722a268369e2c6bc2ef61350e936ee20c0a3
Daniel Rice <dlrice@ebi.ac.uk>
2022-03-31 09:34:41 +0100

    Create comma separated lists of suggestions for main namespace.

--- src/shared/components/results/DidYouMean.tsx
+++ src/shared/components/results/DidYouMean.tsx
@@ -1,7 +1,13 @@
 import { orderBy } from 'lodash-es';
 import { Message } from 'franklin-sites';
 
+import useNS from '../../hooks/useNS';
+
 import { Suggestion } from '../../types/results';
+import {
+  SearchableNamespace,
+  searchableNamespaceLabels,
+} from '../../types/namespaces';
 
 // TODO: delete line and file if not needed
 // import styles from './styles/did-you-mean.module.scss';
@@ -11,14 +17,28 @@ type Props = {
 };
 
 const DidYouMean = ({ suggestions }: Props) => {
-  console.log(suggestions);
+  const namespace = useNS();
+  if (!namespace) {
+    return null;
+  }
+  // TODO: fix type
+  const namespaceLabel =
+    searchableNamespaceLabels[namespace as SearchableNamespace];
   const suggestionsSortedByHits = orderBy(suggestions, ['hits'], ['desc']);
   return (
     <Message level="info">
+      Did you mean to search for:
       <ul>
-        {suggestionsSortedByHits.map(({ query }) => (
-          <li key={query}>{query}</li>
-        ))}
+        <li>
+          {suggestionsSortedByHits.map(({ query }, i, a) => [
+            (i > 0 && i < a.length - 1 && ', ') ||
+              (i === a.length - 1 && ' or '),
+            <a href="#" key={query}>
+              {query}
+            </a>,
+          ])}
+          {` in ${namespaceLabel}?`}
+        </li>
       </ul>
     </Message>
   );
