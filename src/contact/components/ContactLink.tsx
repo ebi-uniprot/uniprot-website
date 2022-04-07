1a0b6d1ab872293fd6fc27bd32a3851889994f2e
Daniel Rice <dlrice@ebi.ac.uk>
2022-04-07 16:34:40 +0100

    Expect that ContactLink's to prop must be an LocationDescriptorObject.

--- src/contact/components/ContactLink.tsx
+++ src/contact/components/ContactLink.tsx
@@ -1,59 +1,28 @@
-import { Link, useLocation } from 'react-router-dom';
-
-import { LocationDescriptor } from 'history';
-import { SetOptional } from 'type-fest';
-import { ContactLocationState } from './ContactForm';
+import { ComponentProps } from 'react';
+import { Link } from 'react-router-dom';
+import { Except } from 'type-fest';
+import { LocationDescriptorObject } from 'history';
 
 import { LocationToPath, Location } from '../../app/config/urls';
+import { ContactLocationState } from './ContactForm';
 
-const ContactLink = ({
-  to: initialTo,
-  children,
-  className,
-}: SetOptional<React.ComponentProps<Link<ContactLocationState>>, 'to'>) => {
-  const location = useLocation<ContactLocationState>();
-  const defaultTo = {
-    pathname: LocationToPath[Location.ContactGeneric],
-    state: {
-      referrer: location.state?.referrer,
-    },
-  };
-  let to: LocationDescriptor<ContactLocationState> | null = null;
-  if (typeof initialTo === 'undefined') {
-    to = defaultTo;
-  } else if (typeof initialTo === 'string') {
-    to = {
-      ...defaultTo,
-      pathname: initialTo,
-    };
-  } else if (typeof initialTo === 'object') {
-    to = {
-      ...defaultTo,
-      ...initialTo,
-    };
-  } else {
-    const computedTo = initialTo(location);
-    if (typeof computedTo === 'string') {
-      to = {
-        ...defaultTo,
-        pathname: computedTo,
-      };
-    } else {
-      to = {
-        ...defaultTo,
-        ...computedTo,
-      };
-    }
-  }
-  return (
-    <Link<ContactLocationState>
-      className={className}
-      // eslint-disable-next-line uniprot-website/use-config-location
-      to={to}
-    >
-      {children}
-    </Link>
-  );
+type Props = Except<ComponentProps<Link<ContactLocationState>>, 'to'> & {
+  to?: LocationDescriptorObject<ContactLocationState>;
 };
+
+const ContactLink = ({ to, children, className }: Props) => (
+  <Link<ContactLocationState>
+    className={className}
+    to={(location) => ({
+      pathname: LocationToPath[Location.ContactGeneric],
+      state: {
+        referrer: location.state?.referrer,
+      },
+      ...to,
+    })}
+  >
+    {children}
+  </Link>
+);
 
 export default ContactLink;
