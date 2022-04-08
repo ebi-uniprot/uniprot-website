ff842f94e48fb800eabcaf060cb1fac3fdb79863
Aurélien Luciani <aurel-l@users.noreply.github.com>
2022-04-08 09:25:09 +0200

    refactor DidYouMean to use a map instead of an array

--- src/shared/components/results/DidYouMean.tsx
+++ src/shared/components/results/DidYouMean.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef } from 'react';
+import { Fragment, useEffect, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import { orderBy } from 'lodash-es';
 import { Loader, Message } from 'franklin-sites';
+import { sleep } from 'timing-functions';
 
 import ContactLink from '../../../contact/components/ContactLink';
 
@@ -10,6 +11,7 @@ import useNS from '../../hooks/useNS';
 import useSafeState from '../../hooks/useSafeState';
 
 import fetchData from '../../utils/fetchData';
+import listFormat from '../../utils/listFormat';
 import { parseQueryString } from '../../utils/url';
 
 import { searchLocations } from '../../../app/config/urls';
@@ -25,6 +27,10 @@ import { APIModel } from '../../types/apiModel';
 
 import styles from './styles/did-you-mean.module.scss';
 
+// example input: '( abc )' -> capturing group: 'abc'
+// not matching '( id:abc )' or '( abc OR def )'
+const cleanUpRE = /^\( ([^: ]+) \)$/;
+
 type QuerySuggestionListItemProps = {
   suggestions: Suggestion[];
   namespace: SearchableNamespace;
@@ -35,20 +41,24 @@ const QuerySuggestionListItem = ({
   namespace,
 }: QuerySuggestionListItemProps) => (
   <li>
-    {suggestions.map(({ query }, i, a) => [
-      (i > 0 && i < a.length - 1 && ', ') ||
-        (a.length > 1 && i === a.length - 1 && ' or '),
-      <Link
-        to={{
-          pathname: searchLocations[namespace],
-          search: queryString.stringify({ query }),
-        }}
-        key={query}
-        className={styles['query-suggestion-link']}
-      >
-        {query}
-      </Link>,
-    ])}
+    {suggestions.map(({ query }, i, a) => {
+      const cleanedQuery = query.replace(cleanUpRE, '$1');
+      return (
+        <Fragment key={query}>
+          {listFormat(i, a, 'or')}
+          <Link
+            to={{
+              pathname: searchLocations[namespace],
+              search: queryString.stringify({ query: cleanedQuery }),
+            }}
+            key={query}
+            className={styles['query-suggestion-link']}
+          >
+            {cleanedQuery}
+          </Link>
+        </Fragment>
+      );
+    })}
     {` in ${searchableNamespaceLabels[namespace]}`}
   </li>
 );
@@ -60,10 +70,7 @@ const didYouMeanNamespaces: SearchableNamespace[] = [
   Namespace.proteomes,
 ];
 
-type NamespaceSuggestions = {
-  namespace: SearchableNamespace;
-  suggestions: Suggestion[];
-};
+type NamespaceSuggestions = Map<SearchableNamespace, Suggestion[]>;
 
 const TIMEOUT = 3_000;
 
@@ -76,9 +83,14 @@ const DidYouMean = ({ suggestions }: DidYouMeanProps) => {
   const location = useLocation();
   // Blocks a render until we have all network results, or we have timed out
   const [renderContent, setRenderContent] = useSafeState(false);
-  const otherNamespaceSuggestions = useRef<NamespaceSuggestions[]>([]);
+  const otherNamespaceSuggestions = useRef<NamespaceSuggestions>(new Map());
 
   const { query } = parseQueryString(location.search);
+
+  // Clear the map if new namespace or new query
+  useEffect(() => {
+    otherNamespaceSuggestions.current.clear();
+  }, [currentNamespace, query]);
 
   useEffect(() => {
     if (!query || !currentNamespace) {
@@ -90,9 +102,7 @@ const DidYouMean = ({ suggestions }: DidYouMeanProps) => {
     );
 
     const promises = otherNamespaces.map((ns) =>
-      fetchData<{
-        results: APIModel[];
-      }>(
+      fetchData<{ results: APIModel[] }>(
         queryString.stringifyUrl({
           url: apiUrls.search(ns),
           query: { query, size: 0 },
@@ -100,66 +110,55 @@ const DidYouMean = ({ suggestions }: DidYouMeanProps) => {
       ).then((response) => {
         const hits = +(response?.headers?.['x-total-records'] || 0);
         if (hits) {
-          otherNamespaceSuggestions.current.push({
-            namespace: ns,
-            suggestions: [{ query, hits }],
-          });
+          otherNamespaceSuggestions.current.set(ns, [{ query, hits }]);
         }
       })
     );
 
-    // If all of the queries have finished, trigger a render even before timeout
-    Promise.all(promises).then(() => {
+    // Race between all of the queries having finished or the timeout triggering
+    Promise.race([Promise.all(promises), sleep(TIMEOUT)]).then(() => {
       setRenderContent(true);
     });
   }, [currentNamespace, query, setRenderContent]);
-
-  // Trigger the render after a set timeout
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setRenderContent(true);
-    }, TIMEOUT);
-    return () => clearTimeout(timeout);
-  }, [setRenderContent]);
 
   if (!renderContent) {
     return <Loader />;
   }
 
-  const suggestionsSortedByHits = orderBy(suggestions, ['hits'], ['desc']);
-  const querySuggestions: NamespaceSuggestions[] = [
-    ...(suggestionsSortedByHits.length && currentNamespace
-      ? [
-          {
-            suggestions: suggestionsSortedByHits,
-            namespace: currentNamespace as SearchableNamespace,
-          },
-        ]
-      : []),
-    ...orderBy(
-      otherNamespaceSuggestions.current,
-      ({ suggestions }) => suggestions[0].hits,
-      ['desc']
-    ),
-  ];
+  const suggestionsSortedByHits = orderBy(suggestions, 'hits', 'desc');
 
   return (
     <Message level="info" className={styles['did-you-mean-message']}>
       <h1 className="small">Sorry, no results were found!</h1>
-      {!!querySuggestions.length && (
+      {suggestionsSortedByHits.length ||
+      otherNamespaceSuggestions.current.size ? (
         <div className={styles.suggestions}>
           Did you mean to search for:
           <ul className={styles['suggestions-list']}>
-            {querySuggestions.map(({ namespace, suggestions }) => (
+            {/* current namespace */}
+            {suggestionsSortedByHits.length && currentNamespace ? (
               <QuerySuggestionListItem
-                suggestions={suggestions}
-                namespace={namespace}
-                key={namespace}
+                suggestions={suggestionsSortedByHits}
+                namespace={currentNamespace as SearchableNamespace}
               />
-            ))}
+            ) : null}
+            {/* other namespaces */}
+            {otherNamespaceSuggestions.current.size
+              ? orderBy(
+                  Array.from(otherNamespaceSuggestions.current.entries()),
+                  ([, suggestions]) => suggestions[0].hits,
+                  'desc'
+                ).map(([namespace, suggestions]) => (
+                  <QuerySuggestionListItem
+                    suggestions={suggestions}
+                    namespace={namespace}
+                    key={namespace}
+                  />
+                ))
+              : null}
           </ul>
         </div>
-      )}
+      ) : null}
       Can&apos;t find what you are looking for? Please{' '}
       <ContactLink>contact us</ContactLink>
     </Message>
