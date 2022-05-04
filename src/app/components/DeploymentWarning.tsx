2f9d8759401d23e85f8416069c27a582ad59fe4a
Daniel Rice <dlrice@ebi.ac.uk>
2022-05-04 12:51:39 +0100

    Make DeploymentWarning dismissable.

--- src/app/components/DeploymentWarning.tsx
+++ src/app/components/DeploymentWarning.tsx
@@ -1,16 +1,26 @@
 import { Message } from 'franklin-sites';
+import { useState } from 'react';
 import style from './styles/deployment-warning.module.scss';
 
 const reUniProtOrg = /^https?:\/\/www\.uniprot\.org/;
 
-const DeploymentWarning = () =>
-  !window.location.href.match(reUniProtOrg) &&
-  !LIVE_RELOAD && (
-    <Message className={style['deployment-warning']} level="warning">
-      {`This is a development version of `}
-      <a href="https://www.uniprot.org">www.uniprot.org</a>
-      {` |  git branch: ${GIT_BRANCH} |  API: ${API_PREFIX}`}
-    </Message>
+const DeploymentWarning = () => {
+  const [dismissed, setDismissed] = useState(false);
+  return (
+    !window.location.href.match(reUniProtOrg) &&
+    !LIVE_RELOAD &&
+    !dismissed && (
+      <Message
+        className={style['deployment-warning']}
+        level="warning"
+        onDismiss={() => setDismissed(true)}
+      >
+        {`This is a development version of `}
+        <a href="https://www.uniprot.org">www.uniprot.org</a>
+        {` |  git branch: ${GIT_BRANCH} |  API: ${API_PREFIX}`}
+      </Message>
+    )
   );
+};
 
 export default DeploymentWarning;
