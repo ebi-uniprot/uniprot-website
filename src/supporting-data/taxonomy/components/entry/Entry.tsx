467d7bdcd64c325c529f584ddfc3c5965f766adb
Aurélien Luciani <aurel-l@users.noreply.github.com>
2022-05-27 11:25:40 +0100

    small fix for links to proteome searches from taxonomy entries

--- src/supporting-data/taxonomy/components/entry/Entry.tsx
+++ src/supporting-data/taxonomy/components/entry/Entry.tsx
@@ -123,7 +123,11 @@ const TaxonomyEntry = (props: RouteChildrenProps<{ accession: string }>) => {
         <div className="button-group">
           <EntryDownload />
           <MapToDropdown statistics={proteinStatistics} />
-          <MapToDropdown statistics={proteomeStatistics}>
+          <MapToDropdown
+            statistics={proteomeStatistics}
+            // NOTE: inconsistent field search compared with the protein searches
+            fieldNameOverride="organism_id"
+          >
             View proteomes
           </MapToDropdown>
         </div>
