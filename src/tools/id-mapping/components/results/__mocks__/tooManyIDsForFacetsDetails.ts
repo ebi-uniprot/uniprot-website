325e787bb0e23f423299dbc604d4753365073251
Daniel Rice <dlrice@ebi.ac.uk>
2022-07-11 12:46:15 +0100

    Add warning/error mock files.

---
+++ src/tools/id-mapping/components/results/__mocks__/tooManyIDsForFacetsDetails.ts
@@ -0,0 +1,6 @@
+export default {
+  from: 'UniProtKB_AC-ID',
+  to: 'UniProtKB',
+  ids: 'A0A024B7W1,A0A024RBG1,and many more',
+  redirectURL: 'idmapping/uniprotkb/results/tooManyIDsForFacets',
+};
