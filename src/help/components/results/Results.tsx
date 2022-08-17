62ee1b1d0be14bda03b25892dc5140d0e9b59127
Aurélien Luciani <aurel-l@users.noreply.github.com>
2022-08-17 11:54:41 +0100

    TypeScript magic with generics

--- src/help/components/results/Results.tsx
+++ src/help/components/results/Results.tsx
@@ -25,27 +25,23 @@ import {
 import { parseQueryString } from '../../../shared/utils/url';
 import { LocationToPath, Location } from '../../../app/config/urls';
 
-import { APIModel } from '../../../shared/types/apiModel';
 import { HelpAPIModel, HelpUIModel } from '../../adapters/helpConverter';
 
 import styles from './styles/results.module.scss';
 
 import HelperImage from './svgs/helper.img.svg';
 
-const dataRenderer = (article: APIModel) => {
-  const content = { ...article };
-  return (
-    <HelpCard
-      id={(content as HelpAPIModel).id}
-      title={(content as HelpAPIModel).title}
-      titleMatch={(content as HelpAPIModel).matches?.title?.[0]}
-      contentMatch={(content as HelpAPIModel).matches?.content?.[0]}
-      releaseDate={(content as HelpAPIModel).releaseDate}
-    />
-  );
-};
+const dataRenderer = (article: HelpAPIModel) => (
+  <HelpCard
+    id={article.id}
+    title={article.title}
+    titleMatch={article.matches?.title?.[0]}
+    contentMatch={article.matches?.content?.[0]}
+    releaseDate={article.releaseDate}
+  />
+);
 
-const getIdKey = (article: APIModel) => (article as HelpAPIModel).id;
+const getIdKey = (article: HelpAPIModel) => article.id;
 
 type Props = {
   inPanel?: boolean;
@@ -140,7 +136,7 @@ const Results = ({
     main = <Loader progress={progress} />;
   } else {
     main = (
-      <DataListWithLoader
+      <DataListWithLoader<HelpUIModel>
         getIdKey={getIdKey}
         data={allResults}
         loading={initialLoading}
