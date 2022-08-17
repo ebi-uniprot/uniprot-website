c6bd591dd9d5694baf67bbcf96ccec71a91e2f72
Daniel Rice <dlrice@ebi.ac.uk>
2022-08-17 11:47:19 +0100

    Add ProteomicsPtm type and fetch from proteins API.

--- src/uniprotkb/components/entry/ProteinProcessingSection.tsx
+++ src/uniprotkb/components/entry/ProteinProcessingSection.tsx
@@ -1,4 +1,4 @@
-import { Card } from 'franklin-sites';
+import { Card, Loader } from 'franklin-sites';
 
 import EntrySection, {
   getEntrySectionNameAndId,
@@ -8,10 +8,73 @@ import KeywordView from '../protein-data-views/KeywordView';
 import XRefView from '../protein-data-views/XRefView';
 import FreeTextView from '../protein-data-views/FreeTextView';
 
+import useDataApi from '../../../shared/hooks/useDataApi';
+
 import { hasContent } from '../../../shared/utils/utils';
+import { proteinsApi } from '../../../shared/config/apiUrls';
 
 import { FreeTextComment } from '../../types/commentTypes';
 import { UIModel } from '../../adapters/sectionConverter';
+
+type ProteomicsPtm = {
+  accession: string;
+  entryName: string;
+  sequence: string;
+  sequenceChecksum: string;
+  taxid: number;
+  features: Feature[];
+};
+
+type Feature = {
+  type: string;
+  begin: string;
+  end: string;
+  xrefs: Xref[];
+  evidences: Evidence[];
+  peptide: string;
+  unique: boolean;
+  ptms: Ptm[];
+};
+
+type Evidence = {
+  code: string;
+  source: Source;
+};
+
+type Source = {
+  id: string;
+  url: string;
+};
+
+type Ptm = {
+  name: string;
+  position: number;
+  sources: string[];
+  dbReferences: DBReference[];
+};
+
+type DBReference = {
+  id: string;
+  properties: Properties;
+};
+
+type Properties = {
+  'Pubmed ID': string;
+  'PSM Score': string;
+  'Dataset ID': string;
+  'Organism part': string;
+  'Binomial final adjusted q_value': string;
+  'Universal Spectrum Id': string;
+  'PSM Count': string;
+  'Final adjusted site probability': string;
+  'Site probability': string;
+};
+
+type Xref = {
+  name: string;
+  id: string;
+  url: string;
+};
 
 type Props = {
   data: UIModel;
@@ -24,6 +87,15 @@ const ProteinProcessingSection = ({
   sequence,
   primaryAccession,
 }: Props) => {
+  const { loading: proteomicsPtmLoading, data: proteomicsPtmData } =
+    useDataApi<ProteomicsPtm>(proteinsApi.proteomicsPtm(primaryAccession));
+
+  if (proteomicsPtmLoading) {
+    return <Loader />;
+  }
+
+  console.log(proteomicsPtmData);
+
   if (!hasContent(data)) {
     return null;
   }
