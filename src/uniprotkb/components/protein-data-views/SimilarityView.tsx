2909a831a6fd6d2ac07a6ed07a69a271616f0b74
Swaathik <swaathik@gmail.com>
2022-09-29 13:23:37 +0100

    Fix lint errors

--- src/uniprotkb/components/protein-data-views/SimilarityView.tsx
+++ src/uniprotkb/components/protein-data-views/SimilarityView.tsx
@@ -20,21 +20,19 @@ const SimilarityView = ({ children }: { children?: string }) => {
   );
   return (
     <>
-      {children?.split(familyRegEx).map((part, index, { length }) => {
-        return part.split(familyExtractor).map((familyPart) => {
+      {children?.split(familyRegEx).map((part, index, { length }) =>
+        part.split(familyExtractor).map((familyPart) => {
           if (familyPart.endsWith('family')) {
             // Separate subfamily and sub-sub family if present
             if (subFamilyRegEx.test(familyPart)) {
               const subFamilies = familyPart.split(subFamilyRegEx);
               const families = [];
-              for (let i = 0; i < subFamilies.length; i = i + 2) {
+              for (let i = 0; i < subFamilies.length; i += 2) {
                 if (subFamilies[i] && subFamilies[i + 1]) {
                   families.push(`${subFamilies[i]}${subFamilies[i + 1]}`);
                 }
               }
-              return families.map((name) => {
-                return familyLink(name);
-              });
+              return families.map((name) => familyLink(name));
             }
             return familyLink(familyPart);
           }
@@ -42,8 +40,8 @@ const SimilarityView = ({ children }: { children?: string }) => {
             return `${part}.`;
           }
           return familyPart;
-        });
-      })}
+        })
+      )}
     </>
   );
 };
