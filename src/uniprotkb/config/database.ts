4846dfcdbd755e71c4777b47f6bb544aac697508
Aurélien Luciani <aurel-l@users.noreply.github.com>
2022-12-15 17:58:01 +0100

    revert adding AGR to entry page

--- src/uniprotkb/config/database.ts
+++ src/uniprotkb/config/database.ts
@@ -100,7 +100,6 @@ export const getEntrySectionToDatabaseNames = (
     })
   );
   entrySectionToDatabaseNames.set(EntrySection.NamesAndTaxonomy, [
-    'AGR',
     'ArachnoServer',
     'Araport',
     'CGD',
