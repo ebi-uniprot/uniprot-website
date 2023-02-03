50790446d4afdbc2aa6e426f900d391543921841
Daniel Rice <dlrice@ebi.ac.uk>
2023-02-03 16:35:08 +0000

    Refactor closing/cancelling logic for column selection.

--- src/shared/components/action-buttons/CustomiseButton.tsx
+++ src/shared/components/action-buttons/CustomiseButton.tsx
@@ -1,5 +1,7 @@
-import { Suspense, useState } from 'react';
+import { FormEvent, Suspense, useState } from 'react';
+import { useRouteMatch } from 'react-router-dom';
 import { Button, EditIcon, SlidingPanel } from 'franklin-sites';
+import { frame } from 'timing-functions';
 
 import ErrorBoundary from '../error-component/ErrorBoundary';
 
@@ -8,6 +10,9 @@ import lazy from '../../utils/lazy';
 import { Namespace } from '../../types/namespaces';
 
 import styles from './styles/customise-button.module.scss';
+import { allEntryPages } from '../../../app/config/urls';
+import { nsToDefaultColumns } from '../../config/columns';
+import useLocalStorage from '../../hooks/useLocalStorage';
 
 const CustomiseTable = lazy(
   () =>
@@ -18,6 +23,40 @@ const CustomiseTable = lazy(
 
 const CustomiseButton = ({ namespace }: { namespace: Namespace }) => {
   const [displayCustomisePanel, setDisplayCustomisePanel] = useState(false);
+  const isEntryPage = Boolean(useRouteMatch(allEntryPages));
+  const defaultColumns = nsToDefaultColumns(namespace, isEntryPage);
+  const [localStorageColumns, setLocalStorageColumns] = useLocalStorage(
+    `table columns for ${namespace}${
+      isEntryPage ? ' entry page' : ''
+    }` as const,
+    defaultColumns
+  );
+  const [columns, setColumns] = useState(localStorageColumns);
+
+  const close = () => setDisplayCustomisePanel(false);
+
+  const save = () => {
+    setLocalStorageColumns(columns);
+    // Have to delay closing otherwise sometimes it doesn't save
+    frame().then(close);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    save();
+  };
+
+  const handleClose = (reason: string) => {
+    if (reason === 'outside') {
+      save();
+    }
+    close();
+  };
+
+  const handleReset = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setColumns(defaultColumns);
+  };
 
   return (
     <>
@@ -26,13 +65,17 @@ const CustomiseButton = ({ namespace }: { namespace: Namespace }) => {
           <SlidingPanel
             title="Customize columns"
             position="left"
-            onClose={() => setDisplayCustomisePanel(false)}
+            onClose={handleClose}
             className={styles['customise-table-panel']}
           >
             <ErrorBoundary>
               <CustomiseTable
-                onClose={() => setDisplayCustomisePanel(false)}
+                isEntryPage={isEntryPage}
                 namespace={namespace}
+                columns={columns}
+                onChange={setColumns}
+                onReset={handleReset}
+                onSubmit={handleSubmit}
               />
             </ErrorBoundary>
           </SlidingPanel>
--- src/shared/components/customise-table/CustomiseTable.tsx
+++ src/shared/components/customise-table/CustomiseTable.tsx
@@ -1,78 +1,58 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent } from 'react';
 import { Button } from 'franklin-sites';
-import { useRouteMatch } from 'react-router-dom';
 import cn from 'classnames';
-import { frame } from 'timing-functions';
 
 import ColumnSelect from '../column-select/ColumnSelect';
-
-import useLocalStorage from '../../hooks/useLocalStorage';
-
-import { nsToDefaultColumns } from '../../config/columns';
-import { allEntryPages } from '../../../app/config/urls';
 
 import { Namespace } from '../../types/namespaces';
 
 import sticky from '../../styles/sticky.module.scss';
+import { Column } from '../../config/columns';
 
 type CustomiseTableProps = {
-  onClose: () => void;
+  isEntryPage: boolean;
   namespace: Namespace;
+  columns: Column[];
+  onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+  onReset: (e: FormEvent<HTMLFormElement>) => void;
+  onChange: (columns: Column[]) => void;
 };
 
-const CustomiseTable = ({ onClose, namespace }: CustomiseTableProps) => {
-  const isEntryPage = Boolean(useRouteMatch(allEntryPages));
-  const defaultColumns = nsToDefaultColumns(namespace, isEntryPage);
-  const [localStorageColumns, setLocalStorageColumns] = useLocalStorage(
-    `table columns for ${namespace}${
-      isEntryPage ? ' entry page' : ''
-    }` as const,
-    defaultColumns
-  );
-
-  const [columns, setColumns] = useState(localStorageColumns);
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLocalStorageColumns(columns);
-    // Have to delay closing otherwise sometimes it doesn't save
-    frame().then(onClose);
-  };
-
-  const handleReset = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setColumns(defaultColumns);
-  };
-
-  return (
-    <form
-      onSubmit={handleSubmit}
-      onReset={handleReset}
-      aria-label={`Customize ${namespace}${
-        isEntryPage ? '' : ' result'
-      } table columns form`}
+const CustomiseTable = ({
+  isEntryPage,
+  namespace,
+  columns,
+  onSubmit,
+  onReset,
+  onChange,
+}: CustomiseTableProps) => (
+  <form
+    onSubmit={onSubmit}
+    onReset={onReset}
+    aria-label={`Customize ${namespace}${
+      isEntryPage ? '' : ' result'
+    } table columns form`}
+  >
+    <ColumnSelect
+      onChange={onChange}
+      selectedColumns={columns}
+      namespace={namespace}
+      isEntryPage={isEntryPage}
     >
-      <ColumnSelect
-        onChange={setColumns}
-        selectedColumns={columns}
-        namespace={namespace}
-        isEntryPage={isEntryPage}
+      <div
+        className={cn(
+          'button-group',
+          'sliding-panel__button-row',
+          sticky['sticky-bottom-right']
+        )}
       >
-        <div
-          className={cn(
-            'button-group',
-            'sliding-panel__button-row',
-            sticky['sticky-bottom-right']
-          )}
-        >
-          <Button variant="secondary" type="reset">
-            Reset to default
-          </Button>
-          <Button type="submit">Close</Button>
-        </div>
-      </ColumnSelect>
-    </form>
-  );
-};
+        <Button variant="secondary" type="reset">
+          Reset to default
+        </Button>
+        <Button type="submit">Save</Button>
+      </div>
+    </ColumnSelect>
+  </form>
+);
 
 export default CustomiseTable;
