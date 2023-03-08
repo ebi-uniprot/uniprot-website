879644a233016be2f8325c4e4a393000a588240a
Daniel Rice <dlrice@ebi.ac.uk>
2023-03-08 14:35:42 +0000

    Async download job status check now works.

--- src/tools/state/getCheckAsyncDownloadJobStatus.ts
+++ src/tools/state/getCheckAsyncDownloadJobStatus.ts
@@ -1,6 +1,5 @@
 import { Dispatch, MutableRefObject } from 'react';
 
-import fetchData from '../../shared/utils/fetchData';
 import {
   getStatusFromResponse,
   getJobMessage,
@@ -36,94 +35,77 @@ const getCheckAsyncDownloadJobStatus =
       (job.parameters as FormParameters[JobTypes.ASYNC_DOWNLOAD]).namespace
     );
     try {
-      /*
-      Access to fetch at 'http://hx-rke-wp-webadmin-35-worker-9.caas.ebi.ac.uk:31210/uniprotkb/download/results/f9049c38631176ac76741e92c432aaf982f293db' from origin 'http://localhost:8080' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
-      */
-      const response = await window
-        // .fetch(urlConfig.statusUrl(job.remoteID), {
-        .fetch(
-          'http://hx-rke-wp-webadmin-35-worker-9.caas.ebi.ac.uk:31210/uniprotkb/download/results/f9049c38631176ac76741e92c432aaf982f293db',
-          {
-            headers: {
-              Accept: 'text/plain,application/json',
-            },
-            method: 'HEAD',
-            // Return a network error when a request is met with a redirect.
-            // We don't want to fetch async download payloads as they will be huge.
-            // redirect: 'error',
-          }
-        )
-        .catch((reason) => {
-          console.log(reason);
-        });
+      const response = await window.fetch(urlConfig.statusUrl(job.remoteID), {
+        headers: {
+          Accept: 'text/plain,application/json',
+        },
+      });
 
-      console.log(response);
-      // const [status] = await getStatusFromResponse(job.type, response);
+      const [status] = await getStatusFromResponse(job.type, response);
+      checkForResponseError(response, status);
 
-      // checkForResponseError(response, status);
+      let currentStateOfJob = getCurrentStateOfJob(job, stateRef);
+      if (!currentStateOfJob) {
+        return;
+      }
 
-      // const currentStateOfJob = getCurrentStateOfJob(job, stateRef);
-      // if (!currentStateOfJob) {
-      //   return;
-      // }
+      if (isJobAlreadyFinished(status, currentStateOfJob)) {
+        return;
+      }
 
-      // if (isJobAlreadyFinished(status, currentStateOfJob)) {
-      //   return;
-      // }
+      if (status === Status.FAILURE) {
+        const errorResponse: { jobStatus: Status; errors?: MappingError[] } =
+          await response.json();
+        if (errorResponse.errors?.[0]) {
+          const error = errorResponse.errors[0];
+          dispatch(
+            updateJob(job.internalID, {
+              timeLastUpdate: Date.now(),
+              status,
+              errorDescription: error.message,
+            })
+          );
+        }
+      }
+      if (isJobIncomplete(status)) {
+        dispatch(
+          updateJob(job.internalID, {
+            timeLastUpdate: Date.now(),
+            status,
+          })
+        );
+        return;
+      }
 
-      // if (job.type === JobTypes.ID_MAPPING && status === Status.FAILURE) {
-      //   const errorResponse: { jobStatus: Status; errors?: MappingError[] } =
-      //     await response.json();
-      //   if (errorResponse.errors?.[0]) {
-      //     const error = errorResponse.errors[0];
-      //     dispatch(
-      //       updateJob(job.internalID, {
-      //         timeLastUpdate: Date.now(),
-      //         status,
-      //         errorDescription: error.message,
-      //       })
-      //     );
+      // TODO: get size of resulting download
+      // const resultsResponse = await fetchData(
+      //   urlConfig.resultsUrl(job.remoteID),
+      //   undefined,
+      //   {
+      //     method: 'HEAD',
       //   }
-      // }
-
-      // if (isJobIncomplete(status)) {
-      //   dispatch(
-      //     updateJob(job.internalID, {
-      //       timeLastUpdate: Date.now(),
-      //       status,
-      //     })
-      //   );
-      //   return;
-      // }
-
-      // // only ID Mapping jobs
-      // const resultsResponse = await fetchData(idMappingResultsUrl, undefined, {
-      //   method: 'HEAD',
-      // });
-
-      // // get a new reference to the job
-      // currentStateOfJob = stateRef.current[job.internalID];
-      // // check that the job is still in the state (it might have been removed)
-      // if (!currentStateOfJob) {
-      //   return;
-      // }
-
-      // const now = Date.now();
-
-      // const hits: string = resultsResponse.headers['x-total-results'] || '0';
-
-      // dispatch(
-      //   updateJob(job.internalID, {
-      //     timeLastUpdate: now,
-      //     timeFinished: now,
-      //     seen: false,
-      //     status,
-      //     data: { hits: +hits },
-      //   })
       // );
-      // messagesDispatch(
-      //   addMessage(getJobMessage({ job: currentStateOfJob, nHits: +hits }))
-      // );
+      // const bytes: string = resultsResponse.headers['Content-Length'] || '0';
+
+      // get a new reference to the job
+      currentStateOfJob = getCurrentStateOfJob(job, stateRef);
+      if (!currentStateOfJob) {
+        return;
+      }
+
+      const now = Date.now();
+      dispatch(
+        updateJob(job.internalID, {
+          timeLastUpdate: now,
+          timeFinished: now,
+          seen: false,
+          status,
+          // data: { fileSize: .... }, // TODO: add this
+        })
+      );
+      messagesDispatch(
+        addMessage(getJobMessage({ job: currentStateOfJob })) // TODO: add fileSize
+      );
     } catch (error) {
       if (error instanceof Error || typeof error === 'string') {
         logging.error(error);
