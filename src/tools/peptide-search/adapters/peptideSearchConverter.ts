76752875793156ea0a37898c768a585f3dacf3c8
Aurélien Luciani <aurel-l@users.noreply.github.com>
2023-03-09 08:06:26 +0000

    fix specific peptide search results issue

--- src/tools/peptide-search/adapters/peptideSearchConverter.ts
+++ src/tools/peptide-search/adapters/peptideSearchConverter.ts
@@ -32,7 +32,7 @@ const peptideSearchConverter = (
     return results;
   }
 
-  const querySequences = peptides.split('\n');
+  const querySequences = peptides.split(/\s+/).filter(Boolean);
 
   return results.map((result) => {
     const sequence = 'sequence' in result && result.sequence.value;
