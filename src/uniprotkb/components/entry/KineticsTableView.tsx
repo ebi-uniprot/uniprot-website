557ad901257b505e5358fdc2005b4520f7c0bcc7
Swaathik <swaathik@gmail.com>
2023-03-21 10:16:32 +0000

    use RichText for constant as it delas correctly

--- src/uniprotkb/components/entry/KineticsTableView.tsx
+++ src/uniprotkb/components/entry/KineticsTableView.tsx
@@ -2,7 +2,6 @@ import useCustomElement from '../../../shared/hooks/useCustomElement';
 
 import { RichText, TextView } from '../protein-data-views/FreeTextView';
 import UniProtKBEvidenceTag from '../protein-data-views/UniProtKBEvidenceTag';
-import { needTextProcessingRE } from '../../utils';
 
 import { KineticParameters } from '../../adapters/functionConverter';
 import { Evidence } from '../../types/modelTypes';
@@ -15,7 +14,6 @@ const tempRegEx = /(([0-9]*[.])?[0-9]+)\sdegrees\scelsius/i;
 const muRegEx = /^u/;
 const captureWordsInParanthesis = /\(((.+)(?: \((.+)\))?)\)/;
 const removeLeadingTrailingComma = /(^,)|(,$)/g;
-const kineticsConstRegEx = /\(\d?[+-]\)|\(-\d\)|\(\d+\)/g;
 
 type KinecticsTableRow = {
   key: string;
@@ -62,19 +60,7 @@ const KineticsTable = ({
             {data.map((value) => (
               <tr key={value.key}>
                 <td className={helper['no-wrap']}>
-                  {value.constant
-                    ?.split(needTextProcessingRE)
-                    .map((part, index) =>
-                      // Support right formatting for scientific notations present in kinetic specific constants
-                      kineticsConstRegEx.test(part) ? (
-                        // eslint-disable-next-line react/no-array-index-key
-                        <sup key={index}>
-                          {part.substring(1, part.length - 1)}
-                        </sup>
-                      ) : (
-                        part
-                      )
-                    )}
+                  <RichText>{value.constant}</RichText>
                 </td>
                 {hasSubstrate && (
                   <td>
@@ -121,8 +107,7 @@ const excludePhTemp = (str: string) => {
 export const extractFromFreeText = (data: KineticParameters) => {
   let km: KinecticsTableRow[] = [];
   let vmax: KinecticsTableRow[] = [];
-  const kcats: KinecticsTableRow[] = [];
-  const additionalNotes: string[] = [];
+  const notes: string[] = [];
 
   if (data.michaelisConstants) {
     km = data.michaelisConstants.map((km) => {
@@ -195,113 +180,24 @@ export const extractFromFreeText = (data: KineticParameters) => {
   }
 
   if (data.note?.texts) {
-    const kcatRegEx = /\.\s/;
-    // From the curation manual: kcat is expressed per unit of time, in sec(-1), min(-1) or h(-1).
-    const kcatConstantRegEx =
-      /([0-9]*[.])?[0-9]+([x*]10\(\d+\))?\s?[sec|min|h]+\s?\(-1\)/gi;
-
     data.note?.texts.forEach((text) => {
-      // if (text.value.includes('kcat')) {
-      //   const kcatValues = text.value.split(kcatRegEx);
-      //   const evidencesForWhole = text.evidences;
-      //   const kcatsFreeText: string[] = [];
-
-      //   kcatValues.forEach((value) => {
-      //     const constants = value.match(kcatConstantRegEx);
-      //     if (constants && constants?.length > 1) {
-      //       const [pubMed] = value.match(/\(pubmed:\d+\)/i) || [null];
-
-      //       // Exceptional case like P45470, B0F481
-      //       if (
-      //         value.includes('respectively') ||
-      //         (value.match(/and/g)?.length || 0) > 1
-      //       ) {
-      //         additionalNotes.push(value);
-      //       } else {
-      //         const substrates = value.split(/and/);
-      //         substrates?.forEach((v) => {
-      //           kcatsFreeText.push(pubMed ? `${v}${pubMed}` : v);
-      //         });
-      //       }
-      //     } else {
-      //       kcatsFreeText.push(value);
-      //     }
-      //   });
-
-      //   kcatsFreeText.forEach((value) => {
-      //     const [constant] = value.match(kcatConstantRegEx) || [''];
-
-      //     if (constant.length > 0) {
-      //       const brokenSentence = value.split(kcatConstantRegEx);
-      //       let substrateInfo = '';
-      //       brokenSentence.forEach((s) => {
-      //         if (!s?.includes('kcat') || !kcatConstantRegEx.test(s)) {
-      //           substrateInfo = s;
-      //         }
-      //       });
-
-      //       const [info, pubMed] = substrateInfo.split('(PubMed:');
-
-      //       let substrateNotes = info.split('(at')?.[0];
-      //       const phTempNotes = info.split('(at')?.[1];
-
-      //       const evidences: Evidence[] = [];
-      //       if (evidencesForWhole && pubMed) {
-      //         evidencesForWhole.forEach((e: Evidence) => {
-      //           if (e.id && pubMed.includes(e.id)) {
-      //             evidences.push(e);
-      //           }
-      //         });
-      //       }
-      //       const ph = phTempNotes?.match(pHRegEx)?.[1];
-      //       const temp = phTempNotes?.match(tempRegEx)?.[1];
-
-      //       if (phTempNotes) {
-      //         const match =
-      //           `(${phTempNotes}`.match(captureWordsInParanthesis)?.[1] ||
-      //           phTempNotes;
-      //         if (['pH', 'degrees'].some((e) => match.includes(e))) {
-      //           substrateNotes += excludePhTemp(match);
-      //         } else {
-      //           // Add the additional info to the Notes column
-      //           substrateNotes += match;
-      //         }
-      //       }
-
-      //       kcats.push({
-      //         key: `kcat${constant}`,
-      //         constant,
-      //         notes: substrateNotes !== '.' ? substrateNotes.trim() : undefined,
-      //         ph,
-      //         temp,
-      //         evidences,
-      //       });
-      //     } else {
-      //       additionalNotes.push(value);
-      //     }
-      //   });
-      // }
-      // if (!text.value.includes('kcat')) {
-      //   additionalNotes.push(text.value);
-      // }
-      additionalNotes.push(text.value);
+      notes.push(text.value);
     });
   }
 
-  return { km, vmax, kcats, additionalNotes };
+  return { km, vmax, notes };
 };
 
 export const KineticsTableView = ({ data }: { data: KineticParameters }) => {
-  const { km, vmax, kcats, additionalNotes } = extractFromFreeText(data);
+  const { km, vmax, notes } = extractFromFreeText(data);
   const columns = ['pH', 'TEMPERATURE[C]', 'NOTES', 'EVIDENCE'];
 
   return (
     <>
       <KineticsTable columns={['KM', 'SUBSTRATE', ...columns]} data={km} />
       <KineticsTable columns={['Vmax', ...columns]} data={vmax} />
-      {/* <KineticsTable columns={['kcat', ...columns]} data={kcats} /> */}
 
-      {additionalNotes.map((note) => (
+      {notes.map((note) => (
         <TextView key={note} comments={[{ value: note }]} />
       ))}
     </>
