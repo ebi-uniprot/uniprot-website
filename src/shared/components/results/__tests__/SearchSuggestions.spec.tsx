a072c6ac164ca2f4067c6a9ae7ee0529110003ab
Swaathik <swaathik@gmail.com>
2023-04-11 15:40:23 +0100

    Add more tests

--- src/shared/components/results/__tests__/SearchSuggestions.spec.tsx
+++ src/shared/components/results/__tests__/SearchSuggestions.spec.tsx
@@ -32,6 +32,33 @@ mock
   .onGet(/\/configure\/uniprotkb\/search-fields/)
   .reply(200, configureSearchTerms);
 
+mock.onGet(/\/uniprotkb\/search\?query=organism_id%3A9606/).reply(200, {
+  results: [
+    {
+      organism: {
+        scientificName: 'Homo sapiens',
+        commonName: 'Human',
+        taxonId: 9606,
+      },
+    },
+  ],
+});
+
+mock.onGet(/\/uniprotkb\/search\?organism_id%3A2/).reply(200, {
+  results: [],
+});
+
+mock
+  .onGet(/\/proteomes\/search\?fields=upid&query=organism_id%3A9606/)
+  .reply(200, {
+    results: [
+      {
+        id: 'UP000005640',
+        proteomeType: 'Reference and representative proteome',
+      },
+    ],
+  });
+
 describe('SearchSuggestions', () => {
   describe('No render', () => {
     it('should not render anything on complex queries', () => {
@@ -101,6 +128,51 @@ describe('SearchSuggestions', () => {
     expect(screen.getByRole('link', { name: 'app' })).toHaveAttribute(
       'href',
       '/?query=(gene_exact:app)'
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render suggestions for fields that has different taxon level', async () => {
+    const { asFragment } = customRender(
+      <SearchSuggestions
+        query="taxonomy_id:9606"
+        namespace={Namespace.uniprotkb}
+        total={100}
+      />
+    );
+
+    await screen.findByText('or restrict search to', { exact: false });
+    expect(
+      screen.getByRole('link', { name: 'exclude lower taxonomic ranks' })
+    ).toHaveAttribute('href', '/?query=(organism_id:9606)');
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should not render suggestions if there is no organism with the given taxon id', async () => {
+    const { container } = customRender(
+      <SearchSuggestions
+        query="taxonomy_id:2"
+        namespace={Namespace.uniprotkb}
+        total={100}
+      />
+    );
+
+    expect(container.querySelector('small')).toHaveTextContent('');
+  });
+
+  it('should render proteome suggestions given an organism id', async () => {
+    const { asFragment } = customRender(
+      <SearchSuggestions
+        query="organism_id:9606"
+        namespace={Namespace.uniprotkb}
+        total={100}
+      />
+    );
+
+    await screen.findByText('or expand search to', { exact: false });
+    expect(screen.getByRole('link', { name: 'UP000005640' })).toHaveAttribute(
+      'href',
+      '/?query=organism_id:9606 AND (proteome:UP000005640)'
     );
     expect(asFragment()).toMatchSnapshot();
   });
