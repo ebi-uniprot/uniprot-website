97c5482c33df31c832bced608715d7db258fb4ef
Daniel Rice <dlrice@ebi.ac.uk>
2023-04-14 11:57:29 +0100

    Create useDatabaseInfoMaps.

---
+++ src/shared/hooks/useDatabaseInfoMaps.ts
@@ -0,0 +1,5 @@
+import getContextHook from '../contexts/getContextHook';
+
+import { DatabaseInfoMapsContext } from '../contexts/UniProtData';
+
+export default getContextHook(DatabaseInfoMapsContext);
