874991063c2d6525aabb939ba49f26e21c38c851
Daniel Rice <dlrice@ebi.ac.uk>
2023-05-19 16:52:17 +0100

    Track when fasta sequence is copied.

--- src/shared/components/entry/SequenceView.tsx
+++ src/shared/components/entry/SequenceView.tsx
@@ -13,6 +13,7 @@ import LazyComponent from '../LazyComponent';
 import useDataApi from '../../hooks/useDataApi';
 
 import { pluralise } from '../../utils/utils';
+import { sendGtagEventCopyFastaClick } from '../../utils/gtagEvents';
 
 import externalUrls from '../../config/externalUrls';
 import apiUrls from '../../config/apiUrls';
@@ -118,6 +119,7 @@ export const SequenceInfo = ({
         addToBasketButton={<AddToBasketButton selectedEntries={isoformId} />}
         isCollapsible={!openByDefault}
         isLoading={loading}
+        onCopy={() => sendGtagEventCopyFastaClick(isoformId)}
       />
     </LazyComponent>
   );
