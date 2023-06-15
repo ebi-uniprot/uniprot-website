76aeb6fa5f80c2fd4d5112d53c6bc59720361160
Swaathik <swaathik@gmail.com>
2023-06-15 09:54:13 +0100

    Add survey banner and disable the hotjar/feedback temporarily

--- src/help/components/contextual/SideButtons.tsx
+++ src/help/components/contextual/SideButtons.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState, MouseEventHandler } from 'react';
+import { useEffect, MouseEventHandler } from 'react';
 import { Link } from 'react-router-dom';
 import cn from 'classnames';
-import { sleep } from 'timing-functions';
+// import { sleep } from 'timing-functions';
 
 import { LocationToPath, Location } from '../../../app/config/urls';
 
@@ -14,7 +14,8 @@ type Props = {
 };
 
 const SideButtons = ({ displayHelp, onClick }: Props) => {
-  const [displayFeedback, setDisplayFeedback] = useState(false);
+  // const [displayFeedback, setDisplayFeedback] = useState(false);
+  const displayFeedback = false;
 
   useEffect(() => {
     // Checking if there is a scroll bar
@@ -40,24 +41,24 @@ const SideButtons = ({ displayHelp, onClick }: Props) => {
         sideButtonHelp.style.right = `${scrollBarWidth.toString()}px`;
       }
     }
+    // Important: restore it once the survey is over
+    // sleep(3000).then(() => {
+    //   // If there's already Hotjar's feedback, don't do anything
+    //   if (document.querySelector('._hj_feedback_container')) {
+    //     if (scrollBarWidth) {
+    //       const hjButton = document.querySelector<HTMLElement>(
+    //         '#_hj_feedback_container div button'
+    //       );
 
-    sleep(3000).then(() => {
-      // If there's already Hotjar's feedback, don't do anything
-      if (document.querySelector('._hj_feedback_container')) {
-        if (scrollBarWidth) {
-          const hjButton = document.querySelector<HTMLElement>(
-            '#_hj_feedback_container div button'
-          );
+    //       if (hjButton) {
+    //         hjButton.style.right = `${scrollBarWidth.toString()}px`;
+    //       }
+    //     }
 
-          if (hjButton) {
-            hjButton.style.right = `${scrollBarWidth.toString()}px`;
-          }
-        }
-
-        return;
-      }
-      setDisplayFeedback(true);
-    });
+    //     return;
+    //   }
+    //   setDisplayFeedback(true);
+    // });
   }, []);
 
   return (
