e25f764e4aa7b9bd80d9e315640c815e9a5bc7ff
Daniel Rice <dlrice@ebi.ac.uk>
2023-07-05 13:25:07 +0100

    Add other groupby facets.

--- src/uniprotkb/components/results/UniProtKBGroupByFacet.tsx
+++ src/uniprotkb/components/results/UniProtKBGroupByFacet.tsx
@@ -3,18 +3,19 @@ import { useMemo } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import cn from 'classnames';
+import { GroupBy } from '../../config/apiUrls';
 
 const UniProtKBGroupByFacet = () => {
   const location = useLocation();
   const searchParams = Object.fromEntries(new URLSearchParams(location.search));
 
   const getTo = useMemo(
-    () => (how: 'taxonomy') =>
+    () => (groupBy: GroupBy) =>
       queryString.stringifyUrl({
         url: location.pathname,
         query: {
           ...searchParams,
-          groupBy: searchParams.groupBy ? undefined : how,
+          groupBy: searchParams.groupBy ? undefined : groupBy,
           parent: undefined,
         },
       }),
@@ -27,10 +28,38 @@ const UniProtKBGroupByFacet = () => {
       <ul className="expandable-list no-bullet">
         <li>
           <Link
-            className={cn({ 'facet-active': searchParams.groupBy })}
+            className={cn({
+              'facet-active': searchParams.groupBy === 'taxonomy',
+            })}
             to={getTo('taxonomy')}
           >
             Taxonomy
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={cn({
+              'facet-active': searchParams.groupBy === 'keyword',
+            })}
+            to={getTo('keyword')}
+          >
+            Keywords
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={cn({ 'facet-active': searchParams.groupBy === 'go' })}
+            to={getTo('go')}
+          >
+            Gene Ontology
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={cn({ 'facet-active': searchParams.groupBy === 'ec' })}
+            to={getTo('ec')}
+          >
+            Enzyme Class
           </Link>
         </li>
       </ul>
