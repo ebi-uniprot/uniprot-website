7b9232599ee0f23cb23f2dccd6441e23ee3fd1bc
Daniel Rice <dlrice@ebi.ac.uk>
2023-07-24 11:07:07 +0100

    Snapshots for taxonomy and EC groups.

--- src/uniprotkb/components/results/__tests__/UniProtKBGroupByResults.spec.tsx
+++ src/uniprotkb/components/results/__tests__/UniProtKBGroupByResults.spec.tsx
@@ -6,16 +6,23 @@ import UniProtKBGroupByResults from '../UniProtKBGroupBy';
 
 import customRender from '../../../../shared/__test-helpers__/customRender';
 
-import { noParent } from '../../../__mocks__/uniProtKBGroupByData';
+import {
+  taxonomyRoot,
+  ecNonRoot,
+} from '../../../__mocks__/uniProtKBGroupByData';
 
 let rendered: ReturnType<typeof customRender>;
 
 const mock = new MockAdapter(axios);
 
-mock.onGet(/uniprotkb\?groupBy=taxonomy&query=%2A/).reply(200, noParent);
+mock
+  .onGet(/\/uniprotkb\/groups\/taxonomy\?query=%28%2A%29/)
+  .reply(200, taxonomyRoot);
 
-// TODO: update as soon as API is stable
-describe.skip('UniProtKBGroupByResults component', () => {
+// \?parent=3.-.-.-&query=%28shadab%29
+mock.onGet(/\/uniprotkb\/groups\/ec/).reply(200, ecNonRoot);
+
+describe('UniProtKBGroupByResults component with taxonomy groups', () => {
   beforeEach(async () => {
     rendered = customRender(<UniProtKBGroupByResults total={123} />, {
       route: '/uniprotkb?groupBy=taxonomy&query=%2A',
@@ -23,7 +30,21 @@ describe.skip('UniProtKBGroupByResults component', () => {
     expect(await screen.findByText(/cellular/)).toBeVisible();
   });
 
-  test('should render', () => {
+  it('should render', () => {
+    const { asFragment } = rendered;
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe('UniProtKBGroupByResults component with EC groups', () => {
+  beforeEach(async () => {
+    rendered = customRender(<UniProtKBGroupByResults total={123} />, {
+      route: 'uniprotkb?groupBy=ec&parent=3.-.-.-&query=shadab',
+    });
+    expect(await screen.findByText(/Hydrolases/)).toBeVisible();
+  });
+
+  it('should render', () => {
     const { asFragment } = rendered;
     expect(asFragment()).toMatchSnapshot();
   });
