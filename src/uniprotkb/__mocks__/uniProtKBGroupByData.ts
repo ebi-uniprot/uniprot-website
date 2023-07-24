a2926dc58bfbc9f7b3287f04fb310f112de1f540
Daniel Rice <dlrice@ebi.ac.uk>
2023-07-24 11:05:07 +0100

    Update uniProtKBGroupByData mock data.

--- src/uniprotkb/__mocks__/uniProtKBGroupByData.ts
+++ src/uniprotkb/__mocks__/uniProtKBGroupByData.ts
@@ -2,7 +2,7 @@ import { GroupByAPIModel } from '../components/results/UniProtKBGroupBy';
 
 // Source: https://rest.uniprot.org/uniprotkb/groups/taxonomy?query=%28%2A%29
 // Retrieved: 2023-05-18
-export const noParent: GroupByAPIModel = {
+export const taxonomyRoot: GroupByAPIModel = {
   ancestors: [],
   groups: [
     {
@@ -30,6 +30,35 @@ export const noParent: GroupByAPIModel = {
       count: 5392244,
     },
   ],
+  parent: {
+    count: 248842690,
+  },
 };
 
-// TODO: get mock data with parent
+// Source: https://rest.uniprot.org/uniprotkb/groups/ec?parent=3.-.-.-&query=%28shadab%29
+// Retrieved: 2023-07-24
+export const ecNonRoot = {
+  ancestors: [
+    {
+      id: '3.2.-.-',
+      label: 'Glycosylases',
+    },
+    {
+      id: '3.2.1.-',
+      label:
+        'Glycosidases, i.e. enzymes hydrolyzing O- and S-glycosyl compounds',
+    },
+  ],
+  groups: [
+    {
+      id: '3.2.1.18',
+      label: 'exo-alpha-sialidase',
+      expandable: false,
+      count: 37,
+    },
+  ],
+  parent: {
+    label: 'Hydrolases',
+    count: 37,
+  },
+};
