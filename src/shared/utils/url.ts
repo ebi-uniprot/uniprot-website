1108861f9dcf8b4753ce45cd2af194b5a97fb872
Daniel Rice <dlrice@ebi.ac.uk>
2023-08-05 18:15:26 +0100

    Cast found Location in getLocationForPathname.

--- src/shared/utils/url.ts
+++ src/shared/utils/url.ts
@@ -1,12 +1,12 @@
 import { matchPath } from 'react-router-dom';
 
-import { LocationToPath } from '../../app/config/urls';
+import { Location, LocationToPath } from '../../app/config/urls';
 
 export const getLocationForPathname = (pathname: string) => {
   const found = Object.entries(LocationToPath).find(([, path]) =>
     matchPath(pathname, { path, exact: path === '/' })
   );
-  return found?.[0];
+  return found?.[0] as Location | undefined;
 };
 
 type QueryStringParamsRecord = Record<
