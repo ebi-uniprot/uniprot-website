a8529c7144114b362b7dc2caa84384454018c658
Daniel Rice <dlrice@ebi.ac.uk>
2023-08-08 14:39:55 +0100

    Add json & excel from/to file formats for ID mapping.

--- src/tools/id-mapping/config/download.ts
+++ src/tools/id-mapping/config/download.ts
@@ -1,7 +1,10 @@
 import { FileFormat } from '../../../shared/types/resultsDownload';
 
+// Used when the number of results exceeds the enrichment threshold
 export const fileFormatsResultsDownload = [
-  FileFormat.tsv,
-  FileFormat.excel,
-  FileFormat.json,
+  FileFormat.tsvIdMappingFromTo, // stream
+  FileFormat.tsv, // async
+  FileFormat.excelIdMappingFromTo, // stream
+  FileFormat.jsonIdMappingFromTo, // stream
+  FileFormat.json, // async
 ];
