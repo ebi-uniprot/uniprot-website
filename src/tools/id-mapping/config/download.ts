05b77bfd78b03ee9f009f514192cbd91ba183042
Daniel Rice <dlrice@ebi.ac.uk>
2023-08-10 07:07:43 +0100

    Remove comment.

--- src/tools/id-mapping/config/download.ts
+++ src/tools/id-mapping/config/download.ts
@@ -1,6 +1,5 @@
 import { FileFormat } from '../../../shared/types/resultsDownload';
 
-// Used when the number of results exceeds the enrichment threshold
 export const fileFormatsResultsDownload = [
   FileFormat.tsvIdMappingFromTo, // stream
   FileFormat.tsv, // async
