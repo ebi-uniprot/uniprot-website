8240886e85057afbe49d8964878768bd21128f1b
Daniel Rice <dlrice@ebi.ac.uk>
2023-08-14 11:20:24 +0100

    Finish creation of ComponentsDownload.

--- src/proteomes/components/entry/ComponentsDownload.tsx
+++ src/proteomes/components/entry/ComponentsDownload.tsx
@@ -1,27 +1,16 @@
 import { useState, FC, ChangeEvent } from 'react';
-import { generatePath, Link, useLocation } from 'react-router-dom';
-import {
-  Button,
-  DownloadIcon,
-  ExternalLink,
-  LongNumber,
-  Message,
-} from 'franklin-sites';
+import { Button, LongNumber } from 'franklin-sites';
 import cn from 'classnames';
 
 // TODO: fix import order
-import { LocationToPath } from '../../../app/config/urls';
 import ColumnSelect from '../../../shared/components/column-select/ColumnSelect';
-import DownloadAPIURL, {
-  DOWNLOAD_SIZE_LIMIT,
-} from '../../../shared/components/download/DownloadAPIURL';
+import DownloadAPIURL from '../../../shared/components/download/DownloadAPIURL';
 import DownloadPreview from '../../../shared/components/download/DownloadPreview';
 import {
   DownloadUrlOptions,
   getDownloadUrl,
 } from '../../../shared/config/apiUrls';
 import { Column, nsToPrimaryKeyColumns } from '../../../shared/config/columns';
-import { getUniprotkbFtpFilenameAndUrl } from '../../../shared/config/ftpUrls';
 import { fileFormatsWithColumns } from '../../../shared/config/resultsDownload';
 import useColumnNames from '../../../shared/hooks/useColumnNames';
 import { Namespace } from '../../../shared/types/namespaces';
@@ -30,12 +19,11 @@ import {
   DownloadPanelFormCloseReason,
   DownloadMethod,
 } from '../../../shared/utils/gtagEvents';
-import AsyncDownloadForm from '../../../tools/async-download/components/AsyncDownloadForm';
-import { MAX_PEPTIDE_FACETS_OR_DOWNLOAD } from '../../../tools/peptide-search/components/results/PeptideSearchResult';
 import { IsoformStatistics } from './ComponentsButtons';
 
 import sticky from '../../../shared/styles/sticky.module.scss';
 import styles from '../../../shared/components/download/styles/download.module.scss';
+import { getPreviewFileFormat } from '../../../shared/components/download/Download';
 
 type DownloadProps = {
   query: string;
@@ -46,7 +34,7 @@ type DownloadProps = {
   namespace: Namespace;
   onClose: (
     panelCloseReason: DownloadPanelFormCloseReason,
-    downloadMethod: DownloadMethod
+    downloadMethod?: DownloadMethod
   ) => void;
   fileFormats: FileFormat[];
   showReviewedOption: boolean;
@@ -90,7 +78,8 @@ const Download: FC<DownloadProps> = ({
       query,
     fileFormat,
     compressed,
-    selected: downloadSelect === 'selected' ? selectedEntries : [],
+    selected:
+      downloadSelect === 'selected' && !selectedQuery ? selectedEntries : [],
     selectedIdField,
     namespace,
   };
@@ -101,28 +90,25 @@ const Download: FC<DownloadProps> = ({
     downloadOptions.columns = selectedColumns;
   }
 
+  let fileFormatOptions = fileFormats;
   const nSelectedEntries = numberSelectedEntries || selectedEntries.length;
   let downloadCount;
   switch (downloadSelect) {
     case 'all':
       downloadCount = totalNumberResults;
       if (showReviewedOption) {
-        // downloadCount = isoformStats?.allWithIsoforms;
         downloadOptions.fileFormat = FileFormat.fastaCanonicalIsoform;
-        fileFormats = [FileFormat.fasta];
+        fileFormatOptions = [FileFormat.fasta];
       }
       break;
     case 'reviewed':
       // Once we have the counts, we should update the downloadCount accordingly
       downloadCount = isoformStats?.reviewed || 0;
       if (includeIsoform) {
-        // downloadCount = isoformStats?.reviewedWithIsoforms || 0;
         downloadOptions.fileFormat = FileFormat.fastaCanonicalIsoform;
-        fileFormats = [FileFormat.fasta];
+        fileFormatOptions = [FileFormat.fasta];
       } else {
-        // downloadCount = isoformStats?.reviewed || 0;
         downloadOptions.fileFormat = FileFormat.fastaCanonical;
-        fileFormats = proteomesFileFormats;
       }
       break;
     case 'selected':
@@ -142,9 +128,8 @@ const Download: FC<DownloadProps> = ({
     fileFormat: previewFileFormat,
     compressed: false,
     size: nPreview,
-    base,
   };
-  if (previewOptions && namespace === Namespace.unisave) {
+  if (previewOptions) {
     // get only the first 10 entries instead of using the size parameters
     previewOptions.selected = previewOptions.selected.slice(0, 10);
   }
@@ -160,80 +145,20 @@ const Download: FC<DownloadProps> = ({
 
   const handleIsoformSelect = (e: ChangeEvent<HTMLInputElement>) => {
     if (e?.target.checked) {
-      fileFormats = [FileFormat.fasta];
+      fileFormatOptions = [FileFormat.fasta];
       setIncludeIsoform(true);
     } else {
-      fileFormats = proteomesFileFormats;
       setIncludeIsoform(false);
     }
   };
 
-  const isUniprotkb = namespace === Namespace.uniprotkb;
-  const isEmbeddings = fileFormat === FileFormat.embeddings;
-  const tooLargeForEmbeddings =
-    isEmbeddings && downloadCount > DOWNLOAD_SIZE_LIMIT_EMBEDDINGS;
-  const isAsyncDownload = (isLarge || isEmbeddings) && isUniprotkb;
-  const ftpFilenameAndUrl =
-    namespace === Namespace.uniprotkb &&
-    jobResultsLocation !== Location.IDMappingResult
-      ? getUniprotkbFtpFilenameAndUrl(downloadUrl, fileFormat)
-      : null;
-
-  // Peptide search download for matches exceeding the threshold
-  const redirectToIDMapping =
-    jobResultsLocation === Location.PeptideSearchResult &&
-    downloadCount > MAX_PEPTIDE_FACETS_OR_DOWNLOAD;
-
   let extraContentNode: JSX.Element | undefined;
-  if ((extraContent === 'ftp' || extraContent === 'url') && ftpFilenameAndUrl) {
-    extraContentNode = (
-      <>
-        <h4 data-article-id="downloads" className={styles['ftp-header']}>
-          File Available On FTP Server
-        </h4>
-        This file is available {!isEmbeddings && 'compressed'} within the{' '}
-        <Link
-          to={generatePath(LocationToPath[Location.HelpEntry], {
-            accession: 'downloads',
-          })}
-        >
-          UniProtKB directory
-        </Link>{' '}
-        of the UniProt FTP server:
-        <div className={styles['ftp-url']}>
-          <ExternalLink
-            url={ftpFilenameAndUrl.url}
-            noIcon
-            onClick={() => onClose('download', 'ftp')}
-          >
-            <DownloadIcon width="1em" />
-            {ftpFilenameAndUrl.filename}
-          </ExternalLink>
-        </div>
-      </>
-    );
-  } else if (
-    extraContent === 'url' ||
-    // Hopefully temporary, this is because of restrictions on embeddings
-    (extraContent === 'generate' && tooLargeForEmbeddings)
-  ) {
+  if (extraContent === 'url') {
     extraContentNode = (
       <DownloadAPIURL
         // Remove the download attribute as it's unnecessary for API access
         apiURL={downloadUrl.replace('download=true&', '')}
-        ftpURL={ftpFilenameAndUrl?.url}
         onCopy={() => onClose('copy', 'api-url')}
-        count={downloadCount}
-        disableAll={isEmbeddings || redirectToIDMapping}
-      />
-    );
-  } else if (extraContent === 'generate') {
-    extraContentNode = (
-      <AsyncDownloadForm
-        downloadUrlOptions={downloadOptions}
-        count={downloadCount}
-        initialFormValues={defaultFormValues}
-        onClose={() => onClose('submit', 'async')}
       />
     );
   } else if (extraContent === 'preview') {
@@ -245,9 +170,6 @@ const Download: FC<DownloadProps> = ({
     );
   }
 
-  const downloadHref =
-    isAsyncDownload || ftpFilenameAndUrl ? undefined : downloadUrl;
-
   return (
     <>
       <label htmlFor="data-selection-false">
@@ -258,7 +180,7 @@ const Download: FC<DownloadProps> = ({
           value="false"
           checked={downloadSelect === 'selected'}
           onChange={handleDownloadAllChange}
-          disabled={nSelectedEntries === 0 || redirectToIDMapping}
+          disabled={nSelectedEntries === 0}
         />
         Download selected (<LongNumber>{nSelectedEntries}</LongNumber>)
       </label>
@@ -272,7 +194,6 @@ const Download: FC<DownloadProps> = ({
               value="false"
               checked={downloadSelect === 'reviewed'}
               onChange={handleDownloadAllChange}
-              disabled={redirectToIDMapping}
             />
             Download only reviewed (Swiss-Prot) canonical proteins (
             {/* <LongNumber>{includeIsoform ? isoformStats?.reviewedWithIsoforms : isoformStats?.reviewed}</LongNumber>) */}
@@ -298,7 +219,6 @@ const Download: FC<DownloadProps> = ({
           value="true"
           checked={downloadSelect === 'all'}
           onChange={handleDownloadAllChange}
-          disabled={redirectToIDMapping}
         />
         Download all{' '}
         {showReviewedOption
@@ -316,9 +236,8 @@ const Download: FC<DownloadProps> = ({
             data-testid="file-format-select"
             value={fileFormat}
             onChange={(e) => setFileFormat(e.target.value as FileFormat)}
-            disabled={redirectToIDMapping}
           >
-            {fileFormats.map((format) => (
+            {fileFormatOptions.map((format) => (
               <option value={format} key={format}>
                 {format}
               </option>
@@ -338,7 +257,6 @@ const Download: FC<DownloadProps> = ({
               value="true"
               checked={compressed}
               onChange={handleCompressedChange}
-              disabled={redirectToIDMapping}
             />
             Yes
           </label>
@@ -350,35 +268,11 @@ const Download: FC<DownloadProps> = ({
               value="false"
               checked={!compressed}
               onChange={handleCompressedChange}
-              disabled={redirectToIDMapping}
             />
             No
           </label>
         </fieldset>
       )}
-      {/* Peptide search download for matches exceeding the threshold */}
-      {redirectToIDMapping && (
-        <Message level="warning">
-          To download peptide search results of more than{' '}
-          <LongNumber>{MAX_PEPTIDE_FACETS_OR_DOWNLOAD}</LongNumber> matches,
-          please use the{' '}
-          <Link
-            to={{
-              pathname: LocationToPath[Location.IDMapping],
-              state: {
-                parameters: {
-                  ids: accessions,
-                  name: `Peptide search matches`,
-                },
-              },
-            }}
-          >
-            ID Mapping
-          </Link>{' '}
-          service.
-        </Message>
-      )}
-
       {hasColumns && (
         <>
           <legend>Customize columns</legend>
@@ -400,11 +294,7 @@ const Download: FC<DownloadProps> = ({
         <Button variant="tertiary" onClick={() => setExtraContent('url')}>
           Generate URL for API
         </Button>
-        <Button
-          variant="tertiary"
-          onClick={() => setExtraContent('preview')}
-          disabled={redirectToIDMapping}
-        >
+        <Button variant="tertiary" onClick={() => setExtraContent('preview')}>
           Preview{' '}
           {namespace === Namespace.unisave && !selectedEntries.length
             ? 'file'
@@ -415,24 +305,12 @@ const Download: FC<DownloadProps> = ({
         </Button>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a
-          href={downloadHref}
+          href={downloadUrl}
           className={cn('button', 'primary')}
-          title={
-            isAsyncDownload
-              ? 'Download with a File Generation job'
-              : 'Download file'
-          }
+          title="Download file"
           target="_blank"
           rel="noreferrer"
-          onClick={() => {
-            if (ftpFilenameAndUrl) {
-              setExtraContent('ftp');
-            } else if (isAsyncDownload) {
-              setExtraContent('generate');
-            } else {
-              onClose('download', 'sync');
-            }
-          }}
+          onClick={() => onClose('download', 'sync')}
         >
           Download
         </a>
