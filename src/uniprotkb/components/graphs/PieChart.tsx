3cca63d03284dd9d75bd1dc0ba3e96604dd77b68
Aurélien Luciani <aurel-l@users.noreply.github.com>
2023-08-23 09:07:45 +0200

    formatting

--- src/uniprotkb/components/graphs/PieChart.tsx
+++ src/uniprotkb/components/graphs/PieChart.tsx
@@ -55,106 +55,101 @@ type CurrentMap = Map<
   { datum: PieArcDatum<StatisticsGraphItem>; opacity: number }
 >;
 
+// scope the current map (for cases of multiple instances the graph rendered)
 const getRenderPieChart =
-  // scope the current map (for cases of multiple instances the graph rendered)
+  (current: CurrentMap = new Map()) =>
+  (
+    svgElement: SVGSVGElement,
+    data: StatisticsGraphItem[],
+    colorScheme: string[][]
+  ): void => {
+    // Create the color scale.
+    const color = scaleOrdinal<string, string>()
+      .domain(data.map((d) => d.name))
+      .range(colorScheme[data.length]);
 
+    // Get the SVG container.
+    const svg = select(svgElement);
 
-    (current: CurrentMap = new Map()) =>
-    (
-      svgElement: SVGSVGElement,
-      data: StatisticsGraphItem[],
-      colorScheme: string[][]
-    ): void => {
-      // Create the color scale.
-      const color = scaleOrdinal<string, string>()
-        .domain(data.map((d) => d.name))
-        .range(colorScheme[data.length]);
+    const pieData = pie(data);
 
-      // Get the SVG container.
-      const svg = select(svgElement);
+    svg
+      .selectAll<SVGGElement, PieArcDatum<StatisticsGraphItem>>(
+        `.${styles.slice}`
+      )
+      .data(
+        pieData,
+        (d, i, domArray) => d?.data.name || domArray[i].dataset.key || i
+      )
+      .style('fill', (d) => color(d.data.name))
+      .transition()
+      .duration(1_000)
+      .tween('tween', (d, i, domArray) => {
+        const group = domArray[i];
+        const slice = group.querySelector<SVGPathElement>('path');
+        const line = group.querySelector<SVGLineElement>('polyline');
+        const label =
+          group.querySelector<SVGForeignObjectElement>('foreignObject');
 
-      const pieData = pie(data);
+        const { width = 0, height = 0 } =
+          label?.firstElementChild?.getBoundingClientRect() || {};
 
-      svg
-        .selectAll<SVGGElement, PieArcDatum<StatisticsGraphItem>>(
-          `.${styles.slice}`
-        )
-        .data(
-          pieData,
-          (d, i, domArray) => d?.data.name || domArray[i].dataset.key || i
-        )
-        .style('fill', (d) => color(d.data.name))
-        .transition()
-        .duration(1_000)
-        .tween('tween', (d, i, domArray) => {
-          const group = domArray[i];
-          const slice = group.querySelector<SVGPathElement>('path');
-          const line = group.querySelector<SVGLineElement>('polyline');
-          const label =
-            group.querySelector<SVGForeignObjectElement>('foreignObject');
+        label?.setAttribute('width', `${width}`);
+        // x1.5 to take into account the typography descenders (e.g. in "y")
+        label?.setAttribute('height', `${height * 1.5}`);
 
-          const { width = 0, height = 0 } =
-            label?.firstElementChild?.getBoundingClientRect() || {};
+        const isOnTheLeft = midAngle(d) < Math.PI;
+        const cur = current.get(d.data.name) || {
+          datum: {
+            startAngle: isOnTheLeft ? 0 : 2 * Math.PI,
+            endAngle: isOnTheLeft ? 0 : 2 * Math.PI,
+          },
+          opacity: 0,
+        };
+        const interpolate = d3interpolate(cur.datum, d);
+        const interpolateOpacity = d3interpolate(cur.opacity, d.value ? 1 : 0);
 
-          label?.setAttribute('width', `${width}`);
-          // x1.5 to take into account the typography descenders (e.g. in "y")
-          label?.setAttribute('height', `${height * 1.5}`);
-
-          const isOnTheLeft = midAngle(d) < Math.PI;
-          const cur = current.get(d.data.name) || {
-            datum: {
-              startAngle: isOnTheLeft ? 0 : 2 * Math.PI,
-              endAngle: isOnTheLeft ? 0 : 2 * Math.PI,
-            },
-            opacity: 0,
-          };
-          const interpolate = d3interpolate(cur.datum, d);
-          const interpolateOpacity = d3interpolate(
-            cur.opacity,
-            d.value ? 1 : 0
+        // At each tick, change the DOM
+        return (t) => {
+          const tweened = interpolate(t);
+          const tweenedOpacity = interpolateOpacity(t);
+          if (!tweened) {
+            return;
+          }
+          current.set(d.data.name, {
+            datum: tweened,
+            opacity: tweenedOpacity,
+          });
+          const isOnTheLeft = midAngle(tweened) < Math.PI;
+          // slices
+          slice?.setAttribute('d', arc(tweened) || '');
+          // lines
+          const linePos = outerArc.centroid(tweened);
+          linePos[0] = radius * 0.95 * (isOnTheLeft ? 1 : -1);
+          const pointsArray = [
+            midArc.centroid(tweened),
+            outerArc.centroid(tweened),
+            linePos,
+          ];
+          line?.setAttribute(
+            'points',
+            pointsArray.map((point) => point.join(',')).join(' ')
           );
-
-          // At each tick, change the DOM
-          return (t) => {
-            const tweened = interpolate(t);
-            const tweenedOpacity = interpolateOpacity(t);
-            if (!tweened) {
-              return;
-            }
-            current.set(d.data.name, {
-              datum: tweened,
-              opacity: tweenedOpacity,
-            });
-            const isOnTheLeft = midAngle(tweened) < Math.PI;
-            // slices
-            slice?.setAttribute('d', arc(tweened) || '');
-            // lines
-            const linePos = outerArc.centroid(tweened);
-            linePos[0] = radius * 0.95 * (isOnTheLeft ? 1 : -1);
-            const pointsArray = [
-              midArc.centroid(tweened),
-              outerArc.centroid(tweened),
-              linePos,
-            ];
-            line?.setAttribute(
-              'points',
-              pointsArray.map((point) => point.join(',')).join(' ')
-            );
-            line?.setAttribute('opacity', `${tweenedOpacity}`);
-            // labels
-            const labelPos = outerArc.centroid(tweened);
-            labelPos[0] =
-              radius * (isOnTheLeft ? 1 : -1) - (isOnTheLeft ? 0 : width);
-            labelPos[1] -= height / 1.5;
-            label?.setAttribute(
-              'transform',
-              `translate(${labelPos[0]}, ${labelPos[1]})`
-            );
-            // whole group
-            group.setAttribute('opacity', `${tweenedOpacity}`);
-          };
-        });
-    };
+          line?.setAttribute('opacity', `${tweenedOpacity}`);
+          // labels
+          const labelPos = outerArc.centroid(tweened);
+          labelPos[0] =
+            radius * (isOnTheLeft ? 1 : -1) - (isOnTheLeft ? 0 : width);
+          labelPos[1] -= height / 1.5;
+          label?.setAttribute(
+            'transform',
+            `translate(${labelPos[0]}, ${labelPos[1]})`
+          );
+          // whole group
+          group.setAttribute('opacity', `${tweenedOpacity}`);
+        };
+      });
+  };
 
 type StatisticsChartProps = {
   data?: StatisticsGraphItem[];
