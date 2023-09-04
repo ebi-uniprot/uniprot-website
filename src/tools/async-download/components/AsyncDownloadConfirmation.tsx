f024f97fb9ed81549bdefe2958b7f20015517221
Daniel Rice <dlrice@ebi.ac.uk>
2023-09-04 15:43:59 +0100

    First attempt at using InfoList for AsyncDownloadConfirmation. WIP

--- src/tools/async-download/components/AsyncDownloadConfirmation.tsx
+++ src/tools/async-download/components/AsyncDownloadConfirmation.tsx
@@ -1,12 +1,18 @@
+import { CodeBlock, InfoList } from 'franklin-sites';
+
 import { AsyncDownloadFormValues } from '../config/asyncDownloadFormData';
 
 type Props = {
   formValues: AsyncDownloadFormValues;
 };
 
-const AsyncDownloadConfirmation = ({ formValues }: Props) => {
-  console.log(formValues);
-  return <>{JSON.stringify(formValues)}</>;
-};
+const AsyncDownloadConfirmation = ({ formValues }: Props) => (
+  <InfoList
+    infoData={Object.entries(formValues).map(([k, v]) => ({
+      title: k,
+      content: <CodeBlock lightMode>{JSON.stringify(v)}</CodeBlock>,
+    }))}
+  />
+);
 
 export default AsyncDownloadConfirmation;
