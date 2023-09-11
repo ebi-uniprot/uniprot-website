9348a1868e930a94f58a0df23108a38f0b4348ca
Swaathik <swaathik@gmail.com>
2023-09-11 15:43:19 +0100

    Update latest news article

--- src/app/components/home-page/LatestNews.tsx
+++ src/app/components/home-page/LatestNews.tsx
@@ -132,6 +132,29 @@ const LatestNews = () => {
               <h3 className="tiny">
                 <Link
                   to={generatePath(LocationToPath[Location.ReleaseNotesEntry], {
+                    accession: '2023-09-13-release',
+                  })}
+                >
+                  UniProt release 2023_04
+                </Link>
+              </h3>
+              <p
+                className={cn(
+                  styles['latest-news__abstract'],
+                  styles['latest-news__abstract--2-lines']
+                )}
+              >
+                Some like it hot | Change of the cross-references to TAIR |
+                Change of the cross-references to TIGRFAMs: replaced by NCBIfam
+                | Change of evidence code for the ProtNLM method
+              </p>
+            </article>
+          </li>
+          <li>
+            <article>
+              <h3 className="tiny">
+                <Link
+                  to={generatePath(LocationToPath[Location.ReleaseNotesEntry], {
                     accession: '2023-06-28-release',
                   })}
                 >
