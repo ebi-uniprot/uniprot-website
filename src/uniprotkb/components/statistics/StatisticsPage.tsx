0f8dd615fec91dbd091ec7aa7b203080fc558892
Aurélien Luciani <aurel-l@users.noreply.github.com>
2023-09-12 20:45:46 +0100

    robots noindex for statistics page for now

--- src/uniprotkb/components/statistics/StatisticsPage.tsx
+++ src/uniprotkb/components/statistics/StatisticsPage.tsx
@@ -790,7 +790,10 @@ const StatisticsPage = () => {
       className={styles['statistics-page']}
       noOverflow
     >
-      <HTMLHead title={['UniProtKB', 'Statistics']} />
+      <HTMLHead title={['UniProtKB', 'Statistics']}>
+        {/* Remove when this page is finished */}
+        <meta name="robots" content="noindex" />
+      </HTMLHead>
       <h1>UniProtKB statistics</h1>
       <Card id="introduction">
         <h2>Introduction</h2>
