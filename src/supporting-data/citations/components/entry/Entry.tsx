cedbcb91ab3d64cd028c11b5abacc4745937e06e
Swaathik <swaathik@gmail.com>
2023-09-27 14:56:43 +0100

    replace in citations entry

--- src/supporting-data/citations/components/entry/Entry.tsx
+++ src/supporting-data/citations/components/entry/Entry.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Loader, Card } from 'franklin-sites';
 import { RouteChildrenProps } from 'react-router-dom';
 import { SetOptional } from 'type-fest';
@@ -5,9 +6,10 @@ import { SetOptional } from 'type-fest';
 import HTMLHead from '../../../../shared/components/HTMLHead';
 import { SingleColumnLayout } from '../../../../shared/components/layouts/SingleColumnLayout';
 import ErrorHandler from '../../../../shared/components/error-pages/ErrorHandler';
-import EntryDownloadOld from '../../../../shared/components/entry/EntryDownloadOld';
 import LiteratureCitation from '../LiteratureCitation';
 import RelatedResults from '../../../../shared/components/results/RelatedResults';
+import EntryDownloadPanel from '../../../../shared/components/entry/EntryDownloadPanel';
+import EntryDownloadButton from '../../../../shared/components/entry/EntryDownloadButton';
 
 import useDataApiWithStale from '../../../../shared/hooks/useDataApiWithStale';
 
@@ -22,6 +24,8 @@ import { CitationsAPIModel } from '../../adapters/citationsConverter';
 import entryPageStyles from '../../../shared/styles/entry-page.module.scss';
 
 const CitationsEntry = (props: RouteChildrenProps<{ accession: string }>) => {
+  const [displayDownloadPanel, setDisplayDownloadPanel] = useState(false);
+
   const accession = props.match?.params.accession;
 
   const { data, loading, error, status, progress } = useDataApiWithStale<
@@ -45,6 +49,9 @@ const CitationsEntry = (props: RouteChildrenProps<{ accession: string }>) => {
 
   const relatedQuery = `(lit_citation_id:${accession}) OR (computational_pubmed_id:${accession}) OR (community_pubmed_id:${accession})`;
 
+  const handleToggleDownload = () =>
+    setDisplayDownloadPanel(!displayDownloadPanel);
+
   return (
     <SingleColumnLayout>
       <HTMLHead
@@ -57,8 +64,11 @@ const CitationsEntry = (props: RouteChildrenProps<{ accession: string }>) => {
       </HTMLHead>
       <h1>{searchableNamespaceLabels[Namespace.citations]}</h1>
       <Card className={entryPageStyles.card}>
+        {displayDownloadPanel && (
+          <EntryDownloadPanel handleToggle={handleToggleDownload} />
+        )}
         <div className="button-group">
-          <EntryDownloadOld />
+          <EntryDownloadButton handleToggle={handleToggleDownload} />
         </div>
         <LiteratureCitation data={data} displayAll headingLevel="h2" />
       </Card>
