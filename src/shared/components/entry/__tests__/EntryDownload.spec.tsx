72dfb8f8e90dcc9ddcc3c8276329ce9f5af215ea
Swaathik <swaathik@gmail.com>
2023-09-27 17:09:10 +0100

    Remove old dropdown and update test

--- src/shared/components/entry/__tests__/EntryDownload.spec.tsx
+++ src/shared/components/entry/__tests__/EntryDownload.spec.tsx
@@ -1,29 +1,38 @@
 import { screen, fireEvent } from '@testing-library/react';
 
-import EntryDownloadOld from '../EntryDownloadOld';
+import EntryDownload from '../EntryDownload';
+
+import { FileFormat } from '../../../types/resultsDownload';
 
 import customRender from '../../../__test-helpers__/customRender';
 
 describe('EntryDownload', () => {
+  let onCloseMock: jest.Mock;
+
+  beforeEach(() => {
+    onCloseMock = jest.fn();
+  });
+
   it('should link to uniparc TSV download endpoint', () => {
-    customRender(<EntryDownloadOld />, {
+    customRender(<EntryDownload nResults={1000} onClose={onCloseMock} />, {
       route: '/uniparc/UPI0000000001/entry',
     });
-    const dropdownButton = screen.getByText('Download');
-    fireEvent.click(dropdownButton);
-    const tsv = screen.getByRole<HTMLAnchorElement>('link', { name: 'TSV' });
-    expect(tsv.href).toEqual(
+    const formatSelect = screen.getByTestId('file-format-select');
+    fireEvent.change(formatSelect, { target: { value: FileFormat.tsv } });
+    const downloadLink = screen.getByTitle<HTMLAnchorElement>('Download file');
+    expect(downloadLink.href).toEqual(
       expect.stringContaining('/uniparc/UPI0000000001/databases?format=tsv')
     );
   });
+
   it('should link to uniref list download endpoint', () => {
-    customRender(<EntryDownloadOld />, {
+    customRender(<EntryDownload nResults={1000} onClose={onCloseMock} />, {
       route: '/uniref/UniRef100_A0A009E088',
     });
-    const dropdownButton = screen.getByText('Download');
-    fireEvent.click(dropdownButton);
-    const tsv = screen.getByRole<HTMLAnchorElement>('link', { name: 'List' });
-    expect(tsv.href).toEqual(
+    const formatSelect = screen.getByTestId('file-format-select');
+    fireEvent.change(formatSelect, { target: { value: FileFormat.list } });
+    const downloadLink = screen.getByTitle<HTMLAnchorElement>('Download file');
+    expect(downloadLink.href).toEqual(
       expect.stringContaining(
         '/uniref/UniRef100_A0A009E088/members?format=list'
       )
