41f63ccd138e46aafce9e94ee1276a9cdc62b849
Daniel Rice <dlrice@ebi.ac.uk>
2023-09-28 16:39:23 +0100

    Update test.

--- src/uniprotkb/components/statistics/__tests__/SequenceLengthHistogram.spec.tsx
+++ src/uniprotkb/components/statistics/__tests__/SequenceLengthHistogram.spec.tsx
@@ -1,57 +1,39 @@
-import { sumBy } from 'lodash-es';
+import { sum } from 'lodash-es';
 
 import { bin } from '../SequenceLengthHistogram';
 
-const sequenceLengthCounts = [
-  {
-    sequenceLength: 1,
-    count: 1,
-  },
-  {
-    sequenceLength: 2,
-    count: 1,
-  },
-  {
-    sequenceLength: 3,
-    count: 1,
-  },
-  {
-    sequenceLength: 4,
-    count: 1,
-  },
-  {
-    sequenceLength: 5,
-    count: 1,
-  },
-  {
-    sequenceLength: 6,
-    count: 1,
-  },
-];
+const sequenceLengthCounts = new Map([
+  [1, 1],
+  [2, 1],
+  [3, 1],
+  [4, 1],
+  [5, 1],
+  [6, 1],
+]);
 
 describe('rebin', () => {
   it('should rebin into bins of size 1', () => {
     const binned = bin(sequenceLengthCounts, 1);
     expect(binned).toEqual(sequenceLengthCounts);
-    expect(sumBy(binned, 'count')).toEqual(sequenceLengthCounts.length);
+    expect(sum(Array.from(binned.values()))).toEqual(sequenceLengthCounts.size);
   });
   it('should rebin into bins of size 2', () => {
     const binned = bin(sequenceLengthCounts, 2);
-    expect(binned).toEqual([
-      { sequenceLength: 0, count: 1 },
-      { sequenceLength: 2, count: 2 },
-      { sequenceLength: 4, count: 2 },
-      { sequenceLength: 6, count: 1 },
+    expect(Array.from(binned)).toEqual([
+      [0, 1],
+      [2, 2],
+      [4, 2],
+      [6, 1],
     ]);
-    expect(sumBy(binned, 'count')).toEqual(sequenceLengthCounts.length);
+    expect(sum(Array.from(binned.values()))).toEqual(sequenceLengthCounts.size);
   });
   it('should rebin into bins of size 3', () => {
     const binned = bin(sequenceLengthCounts, 3);
-    expect(binned).toEqual([
-      { sequenceLength: 0, count: 2 },
-      { sequenceLength: 3, count: 3 },
-      { sequenceLength: 6, count: 1 },
+    expect(Array.from(binned)).toEqual([
+      [0, 2],
+      [3, 3],
+      [6, 1],
     ]);
-    expect(sumBy(binned, 'count')).toEqual(sequenceLengthCounts.length);
+    expect(sum(Array.from(binned.values()))).toEqual(sequenceLengthCounts.size);
   });
 });
