23dae88ee89726cfb5beef389d866d1fe5ed40b2
Daniel Rice <dlrice@ebi.ac.uk>
2023-10-19 14:30:53 +0100

    Refactor to push more rendering into react.

--- src/uniprotkb/components/statistics/AminoAcidBarPlot.tsx
+++ src/uniprotkb/components/statistics/AminoAcidBarPlot.tsx
@@ -51,128 +51,121 @@ const aaToProperty: Record<string, AAProperty> = {
   Trp: 'aromatic',
 };
 
+const size = 20;
+const xOffset = 105;
+const yOffset = 5;
+
 type Props = {
   category: StatisticsCategory;
 };
 const AminoAcidBarPlot = ({ category }: Props) => {
   const svgRef = useRef<SVGSVGElement>(null);
 
-  const aaCounts = category.items
-    .map(({ label, count }) => [label, count])
-    .filter((d): d is [string, number] => Boolean(d[0]));
-  const sumCount = sum(aaCounts.map(([, count]) => count));
-  const aaPercentages: [string, number][] = aaCounts.map(([aa, count]) => [
-    aa,
-    100 * (count / sumCount),
-  ]);
-  const aaPercentagesSorted = aaPercentages.sort(
-    ([, aPercentage], [, bPercentage]) => bPercentage - aPercentage
-  );
-  const maxPercentage = aaPercentagesSorted[0][1];
-  const renderHistogram = useCallback(() => {
+  const renderHistogram = useCallback((items: StatisticsCategory['items']) => {
+    const aaCounts = items
+      .map(({ label, count }) => [label, count])
+      .filter((d): d is [string, number] => Boolean(d[0]));
+    const sumCount = sum(aaCounts.map(([, count]) => count));
+    const aaPercentages: [string, number][] = aaCounts.map(([aa, count]) => [
+      aa,
+      100 * (count / sumCount),
+    ]);
+    const aaPercentagesSorted = aaPercentages.sort(
+      ([, aPercentage], [, bPercentage]) => bPercentage - aPercentage
+    );
+    const maxPercentage = aaPercentagesSorted[0][1];
+
     if (!aaPercentagesSorted.length) {
       return;
     }
 
-    const svg = select(svgRef.current);
-
-    // Remove previous drawings
-    svg.selectAll('g').remove();
-
-    const chart = svg
-      .append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
+    const chart = select(svgRef.current).select('g');
 
     // x-axis
     const xScale = scaleBand()
       .domain(aaPercentagesSorted.map(([aa]) => aa)) // units: amino acid
       .range([0, width]) // units: pixels
       .padding(0.2);
-    chart
-      .append('g')
-      .attr('transform', `translate(0, ${height})`)
-      .call(axisBottom(xScale))
-      .selectAll('text')
-      .style('text-anchor', 'end')
-      .style('font-size', '1.4em')
-      .attr('dx', '-0.5em')
-      .attr('dy', '0.2em')
-      .attr('transform', 'rotate(-45)');
-    chart
-      .append('text')
-      .attr('text-anchor', 'middle')
-      .attr('x', width / 2)
-      .attr('y', height + margin.bottom)
-      .text('Amino acid');
+    chart.select<SVGGElement>('.x-axis').call(axisBottom(xScale));
 
     // y-axis
     const yScale = scaleLinear()
       .domain([0, maxPercentage]) // units: percentage
       .range([height, 0]); // units: pixels
-    chart
-      .append('g')
-      .call(axisLeft(yScale))
-      .selectAll('text')
-      .style('font-size', '1.4em');
-    chart
-      .append('text')
-      .attr('text-anchor', 'middle')
-      .attr('x', -height / 2)
-      .attr('y', -0.6 * margin.left)
-      .attr('transform', 'rotate(-90)')
-      .text('% of sequences');
+    chart.select<SVGGElement>('.y-axis').call(axisLeft(yScale));
+
+    // Remove previous bars
+    chart.selectAll('.bars').remove();
 
     chart
-      .selectAll('bar-plot')
+      .selectAll('.bars')
       .data(aaPercentagesSorted)
       .enter()
       .append('rect')
+      .classed('bars', true)
       .attr('x', (d) => xScale(d[0]) || 0)
       .attr('y', (d) => yScale(d[1]) || 0)
       .attr('width', xScale.bandwidth())
       .attr('height', (d) => height - (yScale(d[1]) || 0))
       .attr('fill', (d) => propertyToColor[aaToProperty[d[0]]])
       .attr('stroke', 'black');
-
-    const size = 20;
-    const xOffset = 105;
-    const yOffset = 5;
-    chart
-      .selectAll('legend-colors')
-      .data(Object.values(propertyToColor))
-      .enter()
-      .append('rect')
-      .attr('x', width - xOffset)
-      .attr('y', (d, i) => i * (size + 5) + yOffset)
-      .attr('width', size)
-      .attr('height', size)
-      .style('fill', (d) => d)
-      .style('stroke', 'black');
-
-    chart
-      .selectAll('legend-labels')
-      .data(Object.keys(propertyToColor))
-      .enter()
-      .append('text')
-      .attr('x', width - xOffset + 1.2 * size)
-      .attr('y', (d, i) => i * (size + 5) + size / 2 + yOffset)
-      .text((d) => d)
-      .attr('text-anchor', 'left')
-      .style('alignment-baseline', 'middle');
-  }, [aaPercentagesSorted, maxPercentage]);
+  }, []);
 
   useEffect(() => {
     if (svgRef.current) {
-      renderHistogram();
+      renderHistogram(category.items);
     }
-  }, [renderHistogram]);
+  }, [category.items, renderHistogram]);
 
   return (
     <svg
       ref={svgRef}
       width={width + margin.left + margin.right}
       height={height + margin.top + margin.bottom}
-    />
+    >
+      <g transform={`translate(${margin.left}, ${margin.top})`}>
+        <g
+          className="x-axis"
+          transform={`translate(0, ${height})`}
+          textAnchor="end"
+          fontSize="1.4em"
+          dx="-0.5em"
+          dy="0.2em"
+        />
+        <text textAnchor="middle" x={width / 2} y={height + margin.bottom}>
+          Amino acid
+        </text>
+        <g className="y-axis" fontSize="1.4em" />
+        <text
+          textAnchor="middle"
+          x={-height / 2}
+          y={-0.6 * margin.left}
+          transform="rotate(-90)"
+        >
+          % of sequences
+        </text>
+        {Object.entries(propertyToColor).map(([property, color], i) => (
+          <g key={property}>
+            <rect
+              x={width - xOffset}
+              y={i * (size + 5) + yOffset}
+              width={size}
+              height={size}
+              fill={color}
+              stroke="black"
+            />
+            <text
+              x={width - xOffset + 1.2 * size}
+              y={i * (size + 5) + size / 2 + yOffset}
+              textAnchor="left"
+              style={{ alignmentBaseline: 'middle' }}
+            >
+              {property}
+            </text>
+          </g>
+        ))}
+      </g>
+    </svg>
   );
 };
 
