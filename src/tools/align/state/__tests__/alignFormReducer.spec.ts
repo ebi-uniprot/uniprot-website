90b0671bd2fa65a62d1a9d12aa4cc70eb4f81eac
Daniel Rice <dlrice@ebi.ac.uk>
2023-12-22 11:09:12 +0000

    Add alignFormReducer tests.

---
+++ src/tools/align/state/__tests__/alignFormReducer.spec.ts
@@ -0,0 +1,34 @@
+import {
+  AlignFormAction,
+  getAlignFormDataReducer,
+  getAlignFormInitialState,
+} from '../alignFormReducer';
+import * as actions from '../alignFormActions';
+import defaultFormValues, { AlignFields } from '../../config/AlignFormData';
+
+describe('alignFormReducer', () => {
+  it('should reset with new form values when provided', () => {
+    const reducer = getAlignFormDataReducer(defaultFormValues);
+    const initialState = getAlignFormInitialState(defaultFormValues);
+    let action: AlignFormAction = {
+      type: actions.RESET,
+      payload: {
+        ...initialState.formValues,
+        [AlignFields.name]: {
+          fieldName: 'name',
+          selected: 'foo',
+        },
+      },
+    };
+    let state = reducer(initialState, action);
+    expect(state.formValues.Name.selected).toEqual(
+      action.payload?.Name.selected
+    );
+    action = {
+      type: actions.RESET,
+      payload: undefined,
+    };
+    state = reducer(state, action);
+    expect(state).toEqual(initialState);
+  });
+});
