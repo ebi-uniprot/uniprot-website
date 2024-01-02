73243ee6f81a59db67ea0453d2ac4282f91ea2cf
Daniel Rice <dlrice@ebi.ac.uk>
2024-01-02 11:02:47 +0000

    Exclude Precursor from Fragment column.

--- src/uniprotkb/config/UniProtKBColumnConfiguration.tsx
+++ src/uniprotkb/config/UniProtKBColumnConfiguration.tsx
@@ -465,7 +465,8 @@ UniProtKBColumnConfiguration.set(UniProtKBColumn.fragment, {
   render: (data) => {
     const { flag } = data[EntrySection.Sequence];
     const isFragment = flag && fragmentFlags.has(flag);
-    return isFragment && flag;
+    // Split to not include precursor flag
+    return isFragment && flag.split(',')[0];
   },
 });
 
