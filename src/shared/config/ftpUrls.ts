6f168d85580aa4ad9d2de046434a54b85cbbd422
Daniel Rice <dlrice@ebi.ac.uk>
2024-01-04 12:41:45 +0000

    Genearlize ftp url detection more.

--- src/shared/config/ftpUrls.ts
+++ src/shared/config/ftpUrls.ts
@@ -30,9 +30,17 @@ const ftpUrls = {
       `/current_release/knowledgebase/pan_proteomes/${id}.fasta.gz`
     ),
   embeddings: joinUrl(ftpUniProt, 'current_release/knowledgebase/embeddings'),
+  uniref: joinUrl(ftpUniProt, 'uniref'),
+  uniparc: joinUrl(ftpUniProt, 'uniparc'),
 };
 
+const namespaceToFtpUrlBase = new Map([
+  [Namespace.uniprotkb, ftpUrls.uniprotkb],
+  [Namespace.uniref, ftpUrls.uniref],
+]);
+
 const restFormatToFtpFormat = new Map([
+  [FileFormat.fastaRepresentative, 'fasta'],
   [FileFormat.fastaCanonical, 'fasta'],
   [FileFormat.text, 'dat'],
   [FileFormat.xml, 'xml'],
@@ -48,20 +56,33 @@ const namespaceToAvailableFormats = new Map([
       FileFormat.embeddings,
     ]),
   ],
-  [Namespace.uniref, new Set([FileFormat.fastaCanonical, FileFormat.xml])],
+  [Namespace.uniref, new Set([FileFormat.fastaRepresentative, FileFormat.xml])],
 ]);
 
-const restQueryToFtpFilenames = new Map([
-  ['reviewed:true', ['uniprot_sprot']],
-  ['reviewed:false', ['uniprot_trembl']],
-  ['*', ['uniprot_sprot', 'uniprot_trembl']],
-  ['proteome:up000006548', ['UP000006548_3702/per-protein.h5']],
-  ['proteome:up000001940', ['UP000001940_6239/per-protein.h5']],
-  ['proteome:up000000625', ['UP000000625_83333/per-protein.h5']],
-  ['proteome:up000005640', ['UP000005640_9606/per-protein.h5']],
-  ['proteome:up000000589', ['UP000000589_10090/per-protein.h5']],
-  ['proteome:up000002494', ['UP000002494_10116/per-protein.h5']],
-  ['proteome:up000464024', ['UP000464024_2697049/per-protein.h5']],
+const namespaceToRestQueryToFtpFilenames = new Map([
+  [
+    Namespace.uniref,
+    new Map([
+      ['identity:1.0', ['uniref100/uniref100']],
+      ['identity:0.9', ['uniref90/uniref90']],
+      ['identity:0.5', ['uniref50/uniref50']],
+    ]),
+  ],
+  [
+    Namespace.uniprotkb,
+    new Map([
+      ['reviewed:true', ['uniprot_sprot']],
+      ['reviewed:false', ['uniprot_trembl']],
+      ['*', ['uniprot_sprot', 'uniprot_trembl']],
+      ['proteome:up000006548', ['UP000006548_3702/per-protein.h5']],
+      ['proteome:up000001940', ['UP000001940_6239/per-protein.h5']],
+      ['proteome:up000000625', ['UP000000625_83333/per-protein.h5']],
+      ['proteome:up000005640', ['UP000005640_9606/per-protein.h5']],
+      ['proteome:up000000589', ['UP000000589_10090/per-protein.h5']],
+      ['proteome:up000002494', ['UP000002494_10116/per-protein.h5']],
+      ['proteome:up000464024', ['UP000464024_2697049/per-protein.h5']],
+    ]),
+  ],
 ]);
 
 // This goes from an array of regexs with named capture groups to a
@@ -69,6 +90,13 @@ const restQueryToFtpFilenames = new Map([
 // eg $<bool> with the matched value.
 const reToSimple = new Map([
   [[/^\(*\*\)*$/], '*'],
+  [
+    [
+      /^\(*identity:(?<identity>0.5|0.9|1.0)\)*(?:\s+and\s+\(*\*\)*)?$/,
+      /^(?:\(*\*\)*\s+and\s+)?\(*identity:(?<identity>0.5|0.9|1.0)\)*$/,
+    ],
+    'identity:$<identity>',
+  ],
   [
     [
       /^\(*reviewed:(?<bool>false|true)\)*(?:\s+and\s+\(*\*\)*)?$/,
@@ -113,6 +141,10 @@ export const getUniprotkbFtpFilenamesAndUrls = (
   if (!namespaceToAvailableFormats.get(namespace)?.has(format)) {
     return null;
   }
+  const ftpUrlBase = namespaceToFtpUrlBase.get(namespace);
+  if (!ftpUrlBase) {
+    return null;
+  }
   const sp = new URLSearchParams(downloadUrl);
   const query = sp.get('query');
   if (!query) {
@@ -122,7 +154,9 @@ export const getUniprotkbFtpFilenamesAndUrls = (
   if (!simplifiedQuery) {
     return null;
   }
-  const ftpFilenames = restQueryToFtpFilenames.get(simplifiedQuery);
+  const ftpFilenames = namespaceToRestQueryToFtpFilenames
+    .get(namespace)
+    ?.get(simplifiedQuery);
   if (!ftpFilenames) {
     return null;
   }
@@ -144,7 +178,7 @@ export const getUniprotkbFtpFilenamesAndUrls = (
 
   return ftpFilenames.map((ftpFilename) => {
     const filename = `${ftpFilename}.${ftpFormat}.gz`;
-    const url = joinUrl(ftpUrls.uniprotkb, filename);
+    const url = joinUrl(ftpUrlBase, filename);
     return { filename, url };
   });
 };
