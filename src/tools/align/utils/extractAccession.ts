c576491b014878ae16e5dd23cbcb60c42ea69dc0
Daniel Rice <dlrice@ebi.ac.uk>
2024-01-05 16:34:07 +0000

    Expand extractAccession to include uniref, uniparc as well as returning namespace.

--- src/tools/align/utils/extractAccession.ts
+++ src/tools/align/utils/extractAccession.ts
@@ -1,18 +1,41 @@
-import { reUniProtKBAccessionWithIsoform } from '../../../uniprotkb/utils/regexes';
+import { Namespace } from '../../../shared/types/namespaces';
+import {
+  reUniParc,
+  reUniProtKBAccessionWithIsoform,
+  reUniRefAccession,
+} from '../../../uniprotkb/utils/regexes';
 
-const re = new RegExp(
-  `(?:^|\\W)(${reUniProtKBAccessionWithIsoform.source})(?:\\W|$)`,
-  'i'
-);
+const namespaceToReAccession = [
+  {
+    namespace: Namespace.uniprotkb,
+    re: reUniProtKBAccessionWithIsoform,
+    toUpperCase: true,
+  },
+  { namespace: Namespace.uniref, re: reUniRefAccession },
+  { namespace: Namespace.uniparc, re: reUniParc, toUpperCase: true },
+].map((o) => ({
+  ...o,
+  re: new RegExp(`(?:^|\\W)(${o.re.source})(?:\\W|$)`, 'i'),
+}));
 
-const extractAccession = (string?: string): string | undefined => {
+const extractAccession = (
+  string?: string
+): { namespace: Namespace; accession: string } | null => {
   if (!string) {
-    return undefined;
+    return null;
   }
-  const match = string.match(re);
-  // index 0: full matching string
-  // index 1: the actual accession <--
-  return match?.[1].toUpperCase() ?? undefined;
+  for (const { namespace, re, toUpperCase } of namespaceToReAccession) {
+    // index 0: full matching string
+    // index 1: the actual accession <--
+    const group = string.match(re)?.[1];
+    if (group) {
+      return {
+        namespace,
+        accession: toUpperCase ? group.toUpperCase() : group,
+      };
+    }
+  }
+  return null;
 };
 
 export default extractAccession;
