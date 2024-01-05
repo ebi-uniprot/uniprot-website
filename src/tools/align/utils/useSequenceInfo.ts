4dc2f809506558a9b76f8d37c69a534add35b4ef
Daniel Rice <dlrice@ebi.ac.uk>
2024-01-05 16:36:51 +0000

    useSequenceInfo: discern uniprotkb, uniref and uniparc ids and request corresponding data.

--- src/tools/align/utils/useSequenceInfo.ts
+++ src/tools/align/utils/useSequenceInfo.ts
@@ -11,9 +11,13 @@ import { getAccessionsURL } from '../../../shared/config/apiUrls';
 import { FeatureDatum } from '../../../uniprotkb/components/protein-data-views/UniProtKBFeaturesView';
 import { UniProtkbAPIModel } from '../../../uniprotkb/adapters/uniProtkbConverter';
 import { SearchResults } from '../../../shared/types/results';
+import { Namespace } from '../../../shared/types/namespaces';
+import { UniRefAPIModel } from '../../../uniref/adapters/uniRefConverter';
+import { UniParcAPIModel } from '../../../uniparc/adapters/uniParcConverter';
 
 export type ParsedSequenceAndFeatures = SequenceObject & {
   accession: string;
+  namespace: Namespace;
   features?: FeatureDatum[];
 };
 
@@ -22,63 +26,127 @@ export type SequenceInfo = {
   data: Map<ParsedSequenceAndFeatures['accession'], ParsedSequenceAndFeatures>;
 };
 
-const hasAccession = (
-  value: SequenceObject | ParsedSequenceAndFeatures
-): value is ParsedSequenceAndFeatures =>
-  (value as ParsedSequenceAndFeatures).accession !== undefined;
+const processSequences = (rawSequences = '') => {
+  const processedSequences = [];
+  for (const processedSequence of sequenceProcessor(rawSequences)) {
+    const extracted = extractAccession(processedSequence.name);
+    if (extracted?.accession) {
+      processedSequences.push({
+        ...processedSequence,
+        ...extracted,
+      });
+    }
+  }
+  return processedSequences;
+};
 
-// TODO: handle non-UniProtKB entries too!
-// Need to separate the entries, call the 3 accessions endpoints, and merge data
-// https://www.ebi.ac.uk/panda/jira/browse/TRM-26756
+const getEndpoint = (
+  processedArray: ParsedSequenceAndFeatures[],
+  namespace: Namespace
+) =>
+  getAccessionsURL(
+    processedArray
+      .filter((p) => p.namespace === namespace)
+      .map((p) => p.accession),
+    { namespace, facets: null }
+  );
+
 const useSequenceInfo = (rawSequences?: string): SequenceInfo => {
   const processedArray: ParsedSequenceAndFeatures[] = useMemo(
-    () =>
-      sequenceProcessor(rawSequences || '')
-        .map((processed) => ({
-          ...processed,
-          accession: extractAccession(processed.name),
-        }))
-        // ensures we managed to extract an accession from header, otherwise discard
-        .filter(hasAccession),
+    () => processSequences(rawSequences),
     [rawSequences]
   );
 
-  const endpoint = getAccessionsURL(
-    processedArray.map((processed) => processed.accession),
-    { facets: null }
-  );
-  const { data, loading, error } =
-    useDataApi<SearchResults<UniProtkbAPIModel>>(endpoint);
+  const uniprotkbEndpoint = getEndpoint(processedArray, Namespace.uniprotkb);
+  const unirefEndpoint = getEndpoint(processedArray, Namespace.uniref);
+  const uniparcEndpoint = getEndpoint(processedArray, Namespace.uniparc);
+
+  const uniprotkbResults =
+    useDataApi<SearchResults<UniProtkbAPIModel>>(uniprotkbEndpoint);
+  const unirefResults =
+    useDataApi<SearchResults<UniRefAPIModel>>(unirefEndpoint);
+  const uniparcResults =
+    useDataApi<SearchResults<UniParcAPIModel>>(uniparcEndpoint);
 
   const outputData = useMemo(() => {
-    const dataPerAccession = new Map(
-      data?.results.map((object) => [object.primaryAccession, object]) ?? []
-    );
-
+    const idToSequenceAndFeatures = new Map<
+      string,
+      {
+        sequence: string;
+        features?:
+          | UniProtkbAPIModel['features']
+          | UniParcAPIModel['sequenceFeatures'];
+      }
+    >();
+    for (const result of uniprotkbResults?.data?.results || []) {
+      idToSequenceAndFeatures.set(result.primaryAccession, {
+        sequence: result.sequence.value,
+        features: result.features,
+      });
+    }
+    for (const result of unirefResults?.data?.results || []) {
+      idToSequenceAndFeatures.set(result.id, {
+        sequence: result.representativeMember.sequence.value,
+      });
+    }
+    for (const result of uniparcResults?.data?.results || []) {
+      idToSequenceAndFeatures.set(result.uniParcId, {
+        sequence: result.sequence.value,
+        // features: result.sequenceFeatures,
+      });
+    }
     const pa = processedArray
       // ensures the sequences are identical between submitted and UniProt's DB
       .filter(
         (processed) =>
           processed.sequence ===
-          dataPerAccession.get(processed.accession)?.sequence.value
+          idToSequenceAndFeatures.get(processed.accession)?.sequence
       )
       // enrich with the feature data
       .map((processed) => ({
         ...processed,
-        features: dataPerAccession.get(processed.accession)?.features,
+        features: idToSequenceAndFeatures.get(processed.accession)?.features,
       }));
     return new Map(pa.map((processed) => [processed.accession, processed]));
-  }, [data, processedArray]);
+  }, [
+    processedArray,
+    uniprotkbResults?.data?.results,
+    unirefResults?.data?.results,
+    uniparcResults?.data?.results,
+  ]);
 
   const output = useMemo(
     () => ({
       data: outputData,
-      loading: loading || !rawSequences || (endpoint && !data && !error),
+      loading:
+        !rawSequences ||
+        uniprotkbResults.loading ||
+        unirefResults.loading ||
+        uniparcResults.loading ||
+        (uniprotkbEndpoint &&
+          !uniprotkbResults.data &&
+          !uniprotkbResults.error) ||
+        (unirefEndpoint && !unirefResults.data && !unirefResults.error) ||
+        (uniparcEndpoint && !uniparcResults.data && !uniparcResults.error),
     }),
-    [outputData, loading, rawSequences, endpoint, data, error]
+    [
+      outputData,
+      rawSequences,
+      uniprotkbResults.loading,
+      uniprotkbResults.data,
+      uniprotkbResults.error,
+      unirefResults.loading,
+      unirefResults.data,
+      unirefResults.error,
+      uniparcResults.loading,
+      uniparcResults.data,
+      uniparcResults.error,
+      uniprotkbEndpoint,
+      unirefEndpoint,
+      uniparcEndpoint,
+    ]
   );
 
-  // eslint-disable-next-line consistent-return
   return output as SequenceInfo;
 };
 
