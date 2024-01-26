34a854a306b5d3efb1df8189407fc57cf3f7f91f
Swaathik <swaathik@gmail.com>
2024-01-26 11:52:40 +0000

    Merge the data by proteome ID

--- src/uniprotkb/components/protein-data-views/ProteomesView.tsx
+++ src/uniprotkb/components/protein-data-views/ProteomesView.tsx
@@ -11,29 +11,44 @@ type Props = {
   data?: Xref[];
   isCompact?: boolean;
 };
-const ProteomesView = ({ data, isCompact = false }: Props) => (
-  <ExpandableList descriptionString="proteomes" displayNumberOfHiddenItems>
-    {data?.map((proteome) => (
-      <InfoList
-        key={`${proteome.id}-${proteome.properties?.Component}`}
-        isCompact={isCompact}
-        infoData={[
-          {
-            title: 'Identifier',
-            content: proteome.id && (
-              <Link to={getEntryPath(Namespace.proteomes, proteome.id)}>
-                {proteome.id}
-              </Link>
-            ),
-          },
-          {
-            title: <span data-article-id="proteome_component">Component</span>,
-            content: proteome.properties?.Component,
-          },
-        ]}
-      />
-    ))}
-  </ExpandableList>
-);
+const ProteomesView = ({ data, isCompact = false }: Props) => {
+  const mergedData: { [key: string]: Array<string | undefined> } = {};
+
+  if (data) {
+    data.forEach((proteome) => {
+      const { id, properties } = proteome;
+      if (id && properties?.Component) {
+        mergedData[id] = mergedData[id]
+          ? [...mergedData[id], properties.Component]
+          : [properties.Component];
+      }
+    });
+  }
+
+  return (
+    <ExpandableList descriptionString="proteomes" displayNumberOfHiddenItems>
+      {Object.entries(mergedData).map(([id, components]) => (
+        <InfoList
+          key={id}
+          isCompact={isCompact}
+          infoData={[
+            {
+              title: 'Identifier',
+              content: id && (
+                <Link to={getEntryPath(Namespace.proteomes, id)}>{id}</Link>
+              ),
+            },
+            {
+              title: (
+                <span data-article-id="proteome_component">Component</span>
+              ),
+              content: components?.join(', '),
+            },
+          ]}
+        />
+      ))}
+    </ExpandableList>
+  );
+};
 
 export default ProteomesView;
