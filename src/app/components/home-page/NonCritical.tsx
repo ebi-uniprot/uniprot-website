49e8976bb66bff9db71fd2d87770fdacbf9394a7
Aurélien Luciani <aurel-l@users.noreply.github.com>
2024-05-20 07:09:43 +0100

    add EBI survey to homepage

--- src/app/components/home-page/NonCritical.tsx
+++ src/app/components/home-page/NonCritical.tsx
@@ -1,4 +1,6 @@
 import { memo } from 'react';
+import { Message } from 'franklin-sites';
+import cn from 'classnames';
 
 // Sections
 import CoreData from './CoreData';
@@ -8,18 +10,41 @@ import AnalysisTools from './AnalysisTools';
 import NeedHelp from './NeedHelp';
 import UniProtData from './UniProtData';
 
+import ExternalLink from '../../../shared/components/ExternalLink';
+
+import styles from './styles/non-critical.module.scss';
+
 const HomePageNonCritical = () => (
   <>
+    {' '}
+    <div
+      className={cn(
+        'uniprot-grid',
+        'uniprot-grid--centered',
+        styles['home-page-section']
+      )}
+    >
+      <Message level="info" className="uniprot-grid-cell--span-12">
+        <small>
+          Do data resources managed by EMBL-EBI and our collaborators make a
+          difference to your work?
+          <br />
+          Please take 10 minutes to fill in our annual user survey, and help us
+          make the case for why sustaining open data resources is critical for
+          life sciences research.
+          <br />
+          Survey link:{' '}
+          <ExternalLink url="https://www.surveymonkey.com/r/HJKYKTT?channel=uniprot">
+            https://www.surveymonkey.com/r/HJKYKTT
+          </ExternalLink>
+        </small>
+      </Message>
+    </div>
     <CoreData />
-
     <SupportingData />
-
     <LatestNews />
-
     <AnalysisTools />
-
     <NeedHelp />
-
     <UniProtData />
   </>
 );
