c49273e9808287b7603b5b31426debfbdbee8d65
Daniel Rice <dlrice@ebi.ac.uk>
2024-05-31 11:43:32 +0100

    Tooltip wip.

--- src/shared/utils/tooltip.ts
+++ src/shared/utils/tooltip.ts
@@ -9,9 +9,31 @@ import {
 
 import styles from './styles/tooltip.module.scss';
 
+/*
+Target based
+	DOM target
+	Content
+	A list of activators
+
+Event based
+	Click position
+	Content markdown
+
+Event based
+	DOM Target
+	Content markdown
+
+
+thead event listener for targets with data-column-id attribute
+1. use id to get column tooltip from column config
+2. trigger display of the tooltip with the event target
+
+
+*/
+
 type StaticSide = 'bottom' | 'left' | 'top' | 'right';
 
-const addTooltip = (target: Element, content: string, triggers: Element[]) => {
+const addTooltip = (target: Element, content: string, triggers?: Element[]) => {
   const tooltip = document.createElement('div');
   tooltip.setAttribute('role', 'tooltip');
   tooltip.className = styles.tooltip;
@@ -69,14 +91,106 @@ const addTooltip = (target: Element, content: string, triggers: Element[]) => {
     ['focus', showTooltip],
     ['mouseleave', hideTooltip],
     ['blur', hideTooltip],
-    ['click', hideTooltip],
   ];
-  const allTriggers = [target, ...triggers];
+  const allTriggers = [target, ...(triggers || [])];
   eventsAndListeners.forEach(([event, listener]) => {
     allTriggers.forEach((trigger) => {
       trigger.addEventListener(event, listener);
     });
   });
+
+  // return fn to remove event listeners and remove from view and remove click behavior
 };
 
 export default addTooltip;
+
+export const showTooltip = (
+  x: number,
+  y: number,
+  content: string,
+  target?: Element
+) => {
+  const tooltip = document.createElement('div');
+  tooltip.setAttribute('role', 'tooltip');
+  tooltip.className = styles.tooltip;
+  tooltip.innerHTML = content;
+  const arrowElement = document.createElement('div');
+  arrowElement.className = styles.arrow;
+  tooltip.appendChild(arrowElement);
+  const reference = target || {
+    getBoundingClientRect() {
+      return {
+        x: 0,
+        y: 0,
+        top: y,
+        left: x,
+        bottom: 0,
+        right: 0,
+        width: 0,
+        height: 0,
+      };
+    },
+  };
+
+  const update = () => {
+    computePosition(reference, tooltip, {
+      placement: 'top',
+      middleware: [
+        offset(10),
+        flip(),
+        shift({ padding: 10 }),
+        arrow({ element: arrowElement }),
+      ],
+    }).then(({ x: tooltipX, y: tooltipY, placement, middlewareData }) => {
+      Object.assign(tooltip.style, {
+        left: `${tooltipX}px`,
+        top: `${tooltipY}px`,
+      });
+      const { x: arrowX, y: arrowY } = middlewareData.arrow || {};
+      const staticSide = {
+        top: 'bottom',
+        right: 'left',
+        bottom: 'top',
+        left: 'right',
+      }[placement.split('-')[0]] as StaticSide;
+
+      Object.assign(arrowElement.style, {
+        left: arrowX != null ? `${arrowX}px` : '',
+        top: arrowY != null ? `${arrowY}px` : '',
+        right: '',
+        bottom: '',
+        [staticSide]: '-4px',
+      });
+    });
+  };
+
+  document.body.append(tooltip);
+
+  // option "once" to remove as soon as it has exited
+
+  // Does this really work when scrolling in feature viewer? If not, just hide on scroll.
+  const cleanup = autoUpdate(reference, tooltip, update);
+
+  console.log(target);
+  const hideTooltip = () => {
+    console.log('in hideTooltip');
+    tooltip.remove();
+    cleanup?.();
+  };
+
+  reference.addEventListener('mouseleave', hideTooltip, { once: true });
+
+  // const eventsAndListeners: [string, () => void][] = [
+  //   ['mouseleave', hideTooltip],
+  //   ['blur', hideTooltip],
+  //   ['click', hideTooltip],
+  // ];
+  // const allTriggers = [target, ...(triggers || [])];
+  // eventsAndListeners.forEach(([event, listener]) => {
+  //   allTriggers.forEach((trigger) => {
+  //     trigger.addEventListener(event, listener);
+  //   });
+  // });
+};
+
+//
