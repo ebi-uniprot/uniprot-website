a7bdb6d85717df7db6ef39b7eb132620a3a00e28
Daniel Rice <dlrice@ebi.ac.uk>
2024-06-24 17:07:18 +0100

    Add SubEntrySequenceSection.

---
+++ src/uniparc/components/entry/SubEntrySequenceSection.tsx
@@ -0,0 +1,25 @@
+import { Card } from 'franklin-sites';
+
+import SimpleSequence from '../../../shared/components/simple-sequence/SimpleSequence';
+
+import { hasContent } from '../../../shared/utils/utils';
+import { getEntrySectionNameAndId } from '../../utils/entrySection';
+
+import EntrySection from '../../types/entrySection';
+import { Sequence as SequenceType } from '../../../shared/types/sequence';
+
+const SubEntrySequenceSection = ({ data }: { data: SequenceType }) => {
+  if (!hasContent(data)) {
+    return null;
+  }
+
+  return (
+    <Card
+      header={<h2>{getEntrySectionNameAndId(EntrySection.Sequence).name}</h2>}
+    >
+      <SimpleSequence sequence={data} />
+    </Card>
+  );
+};
+
+export default SubEntrySequenceSection;
