2813d60425c13587ec2832be1c629f1ea71ac0b8
Daniel Rice <dlrice@ebi.ac.uk>
2024-07-01 14:47:28 +0100

    Add getSource util fn.

--- src/uniparc/utils/subEntry.ts
+++ src/uniparc/utils/subEntry.ts
@@ -1,6 +1,7 @@
 import { generatePath } from 'react-router-dom';
 import { Location, LocationToPath } from '../../app/config/urls';
 import { UniParcXRef } from '../adapters/uniParcConverter';
+import { TaxonomyDatum } from '../../supporting-data/taxonomy/adapters/taxonomyConverter';
 
 export const getXRefsForId = (id: string, xrefs?: UniParcXRef[]) =>
   xrefs?.find((xref) => xref.id === id);
@@ -15,3 +16,33 @@ export const getSubEntryPath = (
     subEntryId,
     subPage,
   });
+
+const reSourceDatabase = /^refseq|ensembl|embl/i;
+
+export const isSourceDatabase = (database: string) =>
+  reSourceDatabase.test(database);
+
+export const getSource = (
+  organism: TaxonomyDatum,
+  geneName?: string,
+  xrefs?: UniParcXRef[]
+) => {
+  const found = xrefs?.filter(
+    (xref) =>
+      xref.organism?.taxonId &&
+      organism.taxonId &&
+      xref.database &&
+      xref.organism?.taxonId === organism.taxonId &&
+      isSourceDatabase(xref.database)
+  );
+  if (!found || !found.length) {
+    return null;
+  }
+  if (found.length === 1) {
+    return found[0];
+  }
+  if (found.length > 1 && geneName) {
+    return found.find((xref) => xref.geneName === geneName);
+  }
+  return null;
+};
