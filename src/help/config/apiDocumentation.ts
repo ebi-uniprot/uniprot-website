8df3a3432a1f0cfa4b19ef4a454e4cca97a1ba24
Daniel Rice <dlrice@ebi.ac.uk>
2024-07-19 07:46:17 +0100

    Add supporting data api docs endpoint.

--- src/help/config/apiDocumentation.ts
+++ src/help/config/apiDocumentation.ts
@@ -5,6 +5,7 @@ export const apiDocsDefinitionToString = new Map([
   [ApiDocsDefinition.uniref, 'UniRef'],
   [ApiDocsDefinition.uniparc, 'UniParc'],
   [ApiDocsDefinition.proteome, 'Proteomes'],
+  [ApiDocsDefinition.support_data, 'Supporting Data'],
   [ApiDocsDefinition.aa, 'AA'],
   [ApiDocsDefinition.idmapping, 'ID Mapping'],
   [ApiDocsDefinition.async, 'Async Download'],
