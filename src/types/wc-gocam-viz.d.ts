33db87f2e375416ce5565e8c84b3c4cd8ad1ad08
Daniel Rice <dlrice@ebi.ac.uk>
2024-07-22 14:52:15 +0100

    Add wc-gocam-viz module types.

---
+++ src/types/wc-gocam-viz.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '@geneontology/wc-gocam-viz';
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'wc-gocam-viz': any;
+  }
+}
