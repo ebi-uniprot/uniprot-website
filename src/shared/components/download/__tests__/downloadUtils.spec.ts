659045dc1b2c8846730546281cf1772e17648930
Aurélien Luciani <aurel-l@users.noreply.github.com>
2024-07-25 09:56:55 +0100

    Revert "temporary change to ftp url"
    This reverts commit 9a436368358ba8993404ed2dd1f2aef79ffe478a.

--- src/shared/components/download/__tests__/downloadUtils.spec.ts
+++ src/shared/components/download/__tests__/downloadUtils.spec.ts
@@ -286,7 +286,7 @@ describe('Download Utils', () => {
     expect(getFtpFilenamesAndUrls(state, props, location, job)).toEqual([
       {
         filename: 'uniprot_sprot.fasta.gz',
-        url: 'https://ftp.ebi.ac.uk/pub/databases/uniprot/knowledgebase/complete/uniprot_sprot.fasta.gz',
+        url: 'https://ftp.uniprot.org/pub/databases/uniprot/knowledgebase/complete/uniprot_sprot.fasta.gz',
       },
     ]);
     expect(getColumnsNamespace(props, job)).toEqual(Namespace.uniprotkb);
@@ -713,7 +713,7 @@ describe('Download Utils', () => {
     expect(getFtpFilenamesAndUrls(state, props, location, job)).toEqual([
       {
         filename: 'uniprot_sprot/per-protein.h5',
-        url: 'https://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/embeddings/uniprot_sprot/per-protein.h5',
+        url: 'https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/embeddings/uniprot_sprot/per-protein.h5',
       },
     ]);
     expect(getColumnsNamespace(props, job)).toEqual(Namespace.uniprotkb);
