04e72ba83d5e7acd346df4af3b6309d87f9fda13
Swaathik <swaathik@gmail.com>
2024-11-26 15:08:43 +0000

    Update latest news section

--- src/app/components/home-page/LatestNews.tsx
+++ src/app/components/home-page/LatestNews.tsx
@@ -132,6 +132,28 @@ const LatestNews = () => {
               <h3 className="tiny">
                 <Link
                   to={generatePath(LocationToPath[Location.ReleaseNotesEntry], {
+                    accession: '2024-11-27-release',
+                  })}
+                >
+                  UniProt release 2024_06
+                </Link>
+              </h3>
+              <p
+                className={cn(
+                  styles['latest-news__abstract'],
+                  styles['latest-news__abstract--2-lines']
+                )}
+              >
+                What happens when ribosomes crash | Cross-references to FunFam |
+                Cross-references to AntiFam
+              </p>
+            </article>
+          </li>
+          <li>
+            <article>
+              <h3 className="tiny">
+                <Link
+                  to={generatePath(LocationToPath[Location.ReleaseNotesEntry], {
                     accession: '2024-10-02-release',
                   })}
                 >
