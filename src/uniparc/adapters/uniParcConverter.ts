975c76710bb10ab149c253a73994f9a529f993df
Daniel Rice <dlrice@ebi.ac.uk>
2024-11-29 17:28:48 +0000

    Add uniProtKBAccessions to UniParcAPIModel.

--- src/uniparc/adapters/uniParcConverter.ts
+++ src/uniparc/adapters/uniParcConverter.ts
@@ -90,6 +90,7 @@ export type UniParcAPIModel = {
   oldestCrossRefCreated?: string;
   sequenceFeatures?: SequenceFeature[];
   sequence: Sequence;
+  uniProtKBAccessions: string[];
   from?: string; // ID Mapping results
 };
 
