996f7066fd80c4e9893dc3a33c91f7530349afd6
Daniel Rice <dlrice@ebi.ac.uk>
2024-12-23 10:54:24 +0000

    Update InteractionSection tests.

--- src/uniprotkb/components/entry/__tests__/InteractionSection.spec.tsx
+++ src/uniprotkb/components/entry/__tests__/InteractionSection.spec.tsx
@@ -1,28 +1,20 @@
-import { fireEvent, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import InteractionSection from '../InteractionSection';
 import customRender from '../../../../shared/__test-helpers__/customRender';
 
 import data, { dataWithNoXref } from './__mocks__/interactionComplexesData';
 
-// TODO: unskip
-describe.skip('InteractionSection', () => {
+describe('InteractionSection', () => {
   it('show the viewer tab if there are any xrefs from complex portal', async () => {
     const { asFragment } = customRender(
       <InteractionSection data={data} primaryAccession="P05067" />
     );
     await screen.findByText('Complex viewer');
     expect(asFragment()).toMatchSnapshot();
-
-    const dropdownButton = screen.getByRole<HTMLButtonElement>('button');
-    expect(dropdownButton).toHaveTextContent(
-      'CPX-1062 Amyloid-beta protein 40/42 complex'
-    );
-    fireEvent.click(dropdownButton);
-    const selectButton = screen.getByRole('button', { name: /CPX-1069/ });
-    fireEvent.click(selectButton, { target: { innerText: 'CPX-1069' } });
-    expect(dropdownButton).toHaveTextContent(
-      'CPX-1069 Amyloid-beta protein 40 complex'
-    );
+    const defaultOption = screen.getByRole<HTMLOptionElement>('option', {
+      name: 'CPX-1062 Amyloid-beta protein 40/42 complex',
+    });
+    expect(defaultOption.selected).toBe(true);
   });
 
   it('Should not render the viewer tab if there are no xrefs from complex portal', async () => {
