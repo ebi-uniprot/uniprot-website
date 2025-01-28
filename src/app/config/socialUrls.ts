61b4863f511924c4b84361ca6778229136b32e36
Swaathik <swaathik@gmail.com>
2025-01-28 14:20:14 +0000

    Match x's icon dimensions with linkedin and update snapshots

--- src/app/config/socialUrls.ts
+++ src/app/config/socialUrls.ts
@@ -1,4 +1,4 @@
 export const linkedIn = 'https://www.linkedin.com/company/uniprot/';
-export const twitterX = 'https://twitter.com/uniprot';
+export const twitterX = 'https://x.com/uniprot';
 export const youtube = 'https://www.youtube.com/user/uniprotvideos';
 export const blogspot = 'https://insideuniprot.blogspot.com/';
