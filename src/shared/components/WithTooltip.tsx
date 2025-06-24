6c333670fcd16de9d50baa305ad0c9c2c16f42a2
Daniel Rice <dlrice@ebi.ac.uk>
2025-06-24 11:53:11 +0100

    Create WithTooltip component.

---
+++ src/shared/components/WithTooltip.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, useEffect, useRef } from 'react';
+
+import { addTooltip } from '../utils/tooltip';
+
+type WithTooltipProps = {
+  tooltip: string;
+  children: ReactNode;
+};
+
+const WithTooltip = ({ tooltip, children }: WithTooltipProps) => {
+  const wrapperRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    if (!wrapperRef.current) {
+      return;
+    }
+    const cleanup = addTooltip(wrapperRef.current, tooltip);
+    return cleanup;
+  }, [tooltip]);
+
+  return (
+    <span
+      ref={wrapperRef}
+      style={{ display: 'inline-block' }} // ensures the span can receive mouse events & sizing
+    >
+      {children}
+    </span>
+  );
+};
+
+export default WithTooltip;
