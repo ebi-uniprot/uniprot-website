2232b5620c155107bce6c4a8a533c97e5594d49a
Aurélien Luciani <aurel-l@users.noreply.github.com>
2025-06-25 14:50:55 +0100

    add message about upcoming changes to proteomes landing page

--- src/proteomes/components/landing-page/LandingPage.tsx
+++ src/proteomes/components/landing-page/LandingPage.tsx
@@ -1,7 +1,7 @@
 import cn from 'classnames';
 import {
-  ExternalLink,
   LongNumber,
+  Message,
   ReferenceProteomeIcon,
   TremblIcon,
 } from 'franklin-sites';
@@ -11,6 +11,7 @@ import joinUrl from 'url-join';
 
 import { Location, LocationToPath } from '../../../app/config/urls';
 import SpeciesIllustration from '../../../images/species_illustration.img.svg';
+import ExternalLink from '../../../shared/components/ExternalLink';
 import HTMLHead from '../../../shared/components/HTMLHead';
 import apiUrls from '../../../shared/config/apiUrls/apiUrls';
 import ftpUrls from '../../../shared/config/ftpUrls';
@@ -178,6 +179,16 @@ const LandingPage = () => {
             </Link>
           </p>
         </div>
+
+        <Message level="warning" className="uniprot-grid-cell--span-12">
+          We will be improving our pipelines for the selection of Reference
+          Proteomes over the next few months (September 2025–February 2026).
+          This might affect your data analysis,{' '}
+          <ExternalLink url="https://insideuniprot.blogspot.com/2025/06/capturing-diversity-of-life.html">
+            please read this short article
+          </ExternalLink>{' '}
+          if you want to know more.
+        </Message>
 
         {/* Statistics */}
         <section className="uniprot-grid-cell--small-span-12 uniprot-grid-cell--medium-span-9">
