bedd12d65c68a493e216d953a423cbc51c68fed4
Daniel Rice <dlrice@ebi.ac.uk>
2025-07-04 17:32:41 +0100

    Styling tweak; add help links.

--- src/uniprotkb/components/entry/similar-proteins/SimilarProteinsSection.tsx
+++ src/uniprotkb/components/entry/similar-proteins/SimilarProteinsSection.tsx
@@ -2,6 +2,7 @@ import { Card } from 'franklin-sites';
 import { lazy, memo } from 'react';
 
 import LazyComponent from '../../../../shared/components/LazyComponent';
+import helper from '../../../../shared/styles/helper.module.scss';
 import { UniProtkbUIModel } from '../../../adapters/uniProtkbConverter';
 import EntrySection from '../../../types/entrySection';
 import { getEntrySectionNameAndId } from '../../../utils/entrySection';
@@ -23,10 +24,18 @@ const SimilarProteinsSection = ({
       id={id}
       data-entry-section
     >
+      <h3 data-article-id="similar_proteins_section#uniref-clusters">
+        UniRef clusters
+      </h3>
       <LazyComponent>
         <SimilarProteins canonical={canonical} isoforms={isoforms} />
       </LazyComponent>
-      <h3 data-article-id="TODO">Orthologs & Paralogs</h3>
+      <h3
+        data-article-id="similar_proteins_section#orthology-and-paralogy"
+        className={helper['padding-top-med']}
+      >
+        Orthologs & paralogs
+      </h3>
       <LazyComponent>
         <AgrHomology xrefs={xrefs} />
       </LazyComponent>
