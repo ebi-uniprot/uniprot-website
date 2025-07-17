3277dc3d332e7dcf2de987d85af0f1ce4a602a90
Daniel Rice <dlrice@ebi.ac.uk>
2025-07-17 15:45:58 +0100

    Can actually just html with tooltips.

--- src/uniprotkb/components/entry/similar-proteins/AgrParalogy.tsx
+++ src/uniprotkb/components/entry/similar-proteins/AgrParalogy.tsx
@@ -106,14 +106,16 @@ export const columns: TableFromDataColumn<AgrParalogsResult>[] = [
     id: 'rank',
     label: (
       <WithTooltip
-        tooltip={`The ranking score is calculated using a weighted formula: Score = w₁ x (Alignment Length x Similarity %) + w₂ x (Alignment Length x Identity %) + w₃ x (Method Count) + w₄ x (Alignment Length).
-
-Where the weights are as follows:
-
-⋅ w₁ = 1.000: Weight for the absolute number of similar amino acids.
-⋅ w₂ = 1.000: Weight for the absolute number of identical amino acids.
-⋅ w₃ = 1.500: Weight for the method count.
-⋅ w₄ = 1.500: Weight for the alignment length.
+        tooltip={`The ranking score is calculated using a weighted formula: Score = w₁ x (Alignment Length x Similarity %) + w₂ x (Alignment Length x Identity %) + w₃ x (Method Count) + w₄ x (Alignment Length).<br>
+<br>
+Where the weights are as follows:<br>
+<br>
+<ul>
+  <li>w₁ = 1.000: Weight for the absolute number of similar amino acids.</li>
+  <li>w₂ = 1.000: Weight for the absolute number of identical amino acids.</li>
+  <li>w₃ = 1.500: Weight for the method count.</li>
+  <li>w₄ = 1.500: Weight for the alignment length.</li>
+</ul>
 
 Multiple entries can share the same rank.`}
       >
