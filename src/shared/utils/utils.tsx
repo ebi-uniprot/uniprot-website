c7b51d32edecf76afb7b26ad57e4cf0de600d2d0
Daniel Rice <dlrice@ebi.ac.uk>
2025-09-18 10:47:36 +0100

    Create generateAndDownloadTSV.

--- src/shared/utils/utils.tsx
+++ src/shared/utils/utils.tsx
@@ -139,3 +139,33 @@ export function excludeKeys<T>(
   const setKeys = new Set(keys);
   return Object.fromEntries(Object.entries(o).filter(([k]) => !setKeys.has(k)));
 }
+
+export const generateAndDownloadTSV = (
+  data?: Record<string, string>[],
+  filename = 'data.tsv'
+): void => {
+  if (!data?.length) {
+    return;
+  }
+
+  const header = Object.keys(data[0]);
+  const tsvRows = [
+    header.join('\t'),
+    ...data.map((row) => header.map((h) => row[h] ?? '').join('\t')),
+  ];
+  const tsvString = tsvRows.join('\n');
+
+  const blob = new Blob([tsvString], {
+    type: 'text/tab-separated-values;charset=utf-8;',
+  });
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', filename);
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+};
