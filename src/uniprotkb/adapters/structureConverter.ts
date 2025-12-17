a508064fd0fd4ba86ec15debb9d8045cdf2a1cf9
Swaathik <swaathik@gmail.com>
2025-12-17 22:50:52 +0000

    Construct isoform sequences

--- src/uniprotkb/adapters/structureConverter.ts
+++ src/uniprotkb/adapters/structureConverter.ts
@@ -2,6 +2,7 @@ import { groupBy } from 'lodash-es';
 
 import { Xref } from '../../shared/types/apiModel';
 import { UniProtKBColumn } from '../types/columnTypes';
+import { AlternativeProductsComment } from '../types/commentTypes';
 import EntrySection from '../types/entrySection';
 import { StructureFeatures } from '../types/featureType';
 import { DatabaseInfoMaps } from '../utils/database';
@@ -9,9 +10,15 @@ import { convertSection, UIModel } from './sectionConverter';
 import { UniProtkbAPIModel } from './uniProtkbConverter';
 
 type GroupedStructureInfo = { [key: string]: Xref[] };
+type IsoformSequences = {
+  isoformId: string;
+  sequence: string;
+  length: number;
+}[];
 
 export type StructureUIModel = {
   structures?: GroupedStructureInfo;
+  isoforms?: IsoformSequences;
 } & UIModel;
 
 export const structureFeaturesToColumns: Readonly<
@@ -34,12 +41,70 @@ const convertStructure = (
   const structureData: StructureUIModel = convertSection(
     data,
     databaseInfoMaps,
+    ['ALTERNATIVE PRODUCTS'],
     undefined,
-    undefined,
-    featuresCategories,
+    [...featuresCategories, 'Alternative sequence'],
     EntrySection.Structure,
     uniProtKBCrossReferences
   );
+
+  const canonicalSequence = data.sequence?.value || '';
+
+  let isoforms: IsoformSequences = [];
+  if (structureData.commentsData.size) {
+    const alternativeProducts = structureData.commentsData.get(
+      'ALTERNATIVE PRODUCTS'
+    ) as AlternativeProductsComment[] | undefined;
+    const variantSequences = structureData.featuresData.filter(
+      (feature) => feature.type === 'Alternative sequence'
+    );
+
+    isoforms =
+      alternativeProducts?.[0]?.isoforms?.map((isoform) => {
+        const isoformSequence: string[] = [];
+        let tailIndex = 0;
+        if (isoform.sequenceIds && variantSequences.length !== 0) {
+          isoform.sequenceIds.forEach((sequenceId) => {
+            const variantSeq = variantSequences.find(
+              (varSeq) => varSeq.featureId === sequenceId
+            );
+            if (variantSeq) {
+              isoformSequence.push(
+                canonicalSequence.slice(
+                  tailIndex,
+                  variantSeq.location.start.value - 1
+                )
+              );
+              if (
+                variantSeq.alternativeSequence?.originalSequence &&
+                variantSeq.alternativeSequence?.alternativeSequences?.length
+              ) {
+                variantSeq.alternativeSequence?.alternativeSequences.forEach(
+                  (altSeq) => isoformSequence.push(altSeq)
+                );
+              }
+              tailIndex = variantSeq.location.end.value;
+            }
+          });
+          if (tailIndex < canonicalSequence.length) {
+            isoformSequence.push(
+              canonicalSequence.slice(tailIndex, canonicalSequence.length)
+            );
+          }
+        }
+
+        return {
+          isoformId: isoform.isoformIds[0],
+          sequence: isoformSequence.join(''),
+          length: isoformSequence.join('').length,
+        };
+      }) ?? [];
+  }
+
+  if (isoforms.length > 0) {
+    structureData.isoforms = isoforms;
+  }
+
   // Extract xrefs to PDB
   if (uniProtKBCrossReferences) {
     const structureInfo = uniProtKBCrossReferences
