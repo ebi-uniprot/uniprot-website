d3fce0169e24341030f7734f47f8fd1719313911
Daniel Rice <dlrice@ebi.ac.uk>
2025-12-19 16:05:18 +0000

    Provide primaryAccession to similar proteins section as this is required by XRefView.

--- src/uniprotkb/components/entry/similar-proteins/SimilarProteinsSection.tsx
+++ src/uniprotkb/components/entry/similar-proteins/SimilarProteinsSection.tsx
@@ -3,9 +3,10 @@ import { lazy, memo } from 'react';
 
 import LazyComponent from '../../../../shared/components/LazyComponent';
 import helper from '../../../../shared/styles/helper.module.scss';
-import { UniProtkbUIModel } from '../../../adapters/uniProtkbConverter';
+import { SimilarProteinsUIModel } from '../../../adapters/similarProteinsConverter';
 import EntrySection from '../../../types/entrySection';
 import { getEntrySectionNameAndId } from '../../../utils/entrySection';
+import XRefView from '../../protein-data-views/XRefView';
 import AgrHomology from './AgrHomology';
 
 const SimilarProteins = lazy(
@@ -13,10 +14,11 @@ const SimilarProteins = lazy(
 );
 
 const SimilarProteinsSection = ({
+  primaryAccession,
   canonical,
   isoforms,
   xrefs,
-}: UniProtkbUIModel[EntrySection.SimilarProteins]) => {
+}: SimilarProteinsUIModel) => {
   const { name, id } = getEntrySectionNameAndId(EntrySection.SimilarProteins);
   return (
     <Card
@@ -39,6 +41,7 @@ const SimilarProteinsSection = ({
       <LazyComponent>
         <AgrHomology xrefs={xrefs} />
       </LazyComponent>
+      <XRefView xrefs={xrefs} primaryAccession={primaryAccession} />
     </Card>
   );
 };
