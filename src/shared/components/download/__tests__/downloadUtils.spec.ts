35833636718ebdf1c3006b36517bb0d6ebf2a0d4
Swaathik <swaathik@gmail.com>
2026-02-18 12:05:28 +0000

    Test query for embeddings download from basket

--- src/shared/components/download/__tests__/downloadUtils.spec.ts
+++ src/shared/components/download/__tests__/downloadUtils.spec.ts
@@ -802,6 +802,55 @@ describe('Download Utils', () => {
     expect(getExtraContent(state, props, location, job)).toEqual('generate');
     expect(getRedirectToIDMapping(state, props, job)).toEqual(false);
   });
+  test('embeddings download with async download from basket', () => {
+    const props: DownloadProps<JobTypes> = {
+      selectedEntries: [],
+      accessions: ['P05067[96-110]', 'P05067', 'A0JP26[1-581]'],
+      totalNumberResults: 3,
+      namespace: Namespace.uniprotkb,
+      notCustomisable: false,
+      inBasketMini: true,
+      accessionSubSequenceMap: new Map([
+        ['P05067[96-110]', 'P05067'],
+        ['P05067', 'P05067'],
+        ['A0JP26[1-581]', 'A0JP26'],
+      ]),
+      onClose: jest.fn(),
+    };
+    const location: HistoryLocation = {
+      pathname: '/uniprotkb/P05067/entry',
+      search: '',
+      hash: '',
+      key: '',
+      state: undefined,
+    };
+    const job: JobFromUrl = {
+      jobId: undefined,
+      jobResultsLocation: undefined,
+      jobResultsNamespace: undefined,
+    };
+    const state = getDownloadInitialState({
+      props,
+      job,
+      selectedColumns: defaultColumns,
+    });
+
+    // Manually set state
+    state.selectedFileFormat = FileFormat.embeddings;
+    state.extraContent = 'generate';
+    expect(getPreviewFileFormat(state)).toEqual(undefined);
+    expect(getDownloadCount(state, props)).toEqual(2);
+    expect(getDownloadOptions(state, props, location, job)).toEqual({
+      accessions: ['P05067', 'A0JP26'],
+      base: undefined,
+      compressed: true,
+      fileFormat: FileFormat.embeddings,
+      namespace: Namespace.uniprotkb,
+      query: 'accession:A0JP26 OR accession:P05067',
+      selected: [],
+      selectedIdField: 'accession',
+    });
+  });
 
   test('unisave', () => {
     const props: DownloadProps<JobTypes> = {
