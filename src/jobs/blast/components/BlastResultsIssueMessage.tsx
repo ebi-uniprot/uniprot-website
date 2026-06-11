596173f55d89be2b25db68a84fcc11785ba0d3b7
Daniel Rice <dlrice@ebi.ac.uk>
2026-06-11 07:59:27 +0100

    Fix typo.

--- src/jobs/blast/components/BlastResultsIssueMessage.tsx
+++ src/jobs/blast/components/BlastResultsIssueMessage.tsx
@@ -3,10 +3,10 @@ import { Message } from 'franklin-sites';
 const BlastResultsIssueMessage = () => (
   <Message level="failure">
     There is currently an issue presenting <span translate="no">BLAST</span>{' '}
-    results within the website. It is still submit jobs but the results will
-    only be available within the Text Output tab and for downloading. We are
-    working to resolve this issue as soon as possible. We apologize for any
-    inconvenience.
+    results within the website. It is still possible to submit jobs but the
+    results will only be available within the Text Output tab and for
+    downloading. We are working to resolve this issue as soon as possible. We
+    apologize for any inconvenience.
   </Message>
 );
 
